5c8bf27d44c9796810f414f0ba04c3a4ca4f97b1
Alfonso J. Ramos <theraot@gmail.com>
2013-07-04 03:40:44 -0500

    Added BuildDouble and overload for GetParts
    BuildDouble is not working correctly

--- Core/Theraot/Core/NumericHelper.cs
+++ Core/Theraot/Core/NumericHelper.cs
@@ -518,6 +518,76 @@ namespace Theraot.Core
         }
 
         [CLSCompliantAttribute(false)]
+        public static double BuildDouble(int sign, long mantissa, int exponent)
+        {
+            if (sign == 0)
+            {
+                return 0.0;
+            }
+            else
+            {
+                //return (sign > 0 ? 1 : - 1) * (double)mantissa * Math.Pow(2, exponent);
+                ulong _mantissa = (ulong)mantissa;
+                if ((_mantissa & 0xffe0000000000000L) != 0)
+                {
+                    throw new ArgumentOutOfRangeException("mantissa", "The mantissa is invalid");
+                }
+                else
+                {
+                    if (mantissa != 0)
+                    {
+                        while (true)
+                        {
+                            var tmp = _mantissa << 1;
+                            if ((tmp & (1L << 52)) == 0)
+                            {
+                                _mantissa = tmp;
+                                exponent--;
+                            }
+                            else
+                            {
+                                _mantissa = tmp;
+                                exponent--;
+                                break;
+                            }
+                        }
+                    }
+                    exponent += 1075;
+                    if (exponent == 1 & mantissa != 0)
+                    {
+                        
+                        exponent = 0;
+                    }
+                    else
+                    {
+                        /*_mantissa = _mantissa << 1;
+                        exponent--;*/
+                        _mantissa = _mantissa & 0xfffffffffffffL;
+                    }
+                    if ((exponent & 0x7ffL) != exponent)
+                    {
+                        throw new ArgumentOutOfRangeException("exponent", "The exponent is invalid");
+                    }
+                    else
+                    {
+                        if (_mantissa < 0 || (_mantissa & 0xfffffffffffffL) != _mantissa)
+                        {
+                            throw new ArgumentOutOfRangeException("mantissa", "The mantissa is invalid");
+                        }
+                        else
+                        {
+                            unchecked
+                            {
+                                long bits = (long)((ulong)_mantissa | (ulong)((ulong)exponent << 52));
+                                return BitConverter.Int64BitsToDouble(bits);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [CLSCompliantAttribute(false)]
         public static ulong BuildUlong(uint hi, uint lo)
         {
             return (ulong)hi << 32 | (ulong)lo;
@@ -584,6 +654,56 @@ namespace Theraot.Core
             {
                 lo = (uint)value;
                 hi = (uint)(value >> 32);
+            }
+        }
+
+        public static void GetParts(double value, out int sign, out long mantissa, out int exponent)
+        {
+            if (value == 0.0)
+            {
+                sign = 0;
+                mantissa = 0;
+                exponent = 1;
+            }
+            else
+            {
+                long bits = BitConverter.DoubleToInt64Bits(value);
+                sign = (bits < 0) ? -1 : 1;
+                exponent = (int) ((bits >> 52) & 0x7ffL);
+                if (exponent == 2047)
+                {
+                    throw new ArgumentException("The value is NaN, PositiveInfinity or NegativeInfinity");
+                }
+                else
+                {
+                    mantissa = bits & 0xfffffffffffffL;
+                    if (exponent == 0)
+                    {
+                        // Subnormal numbers; exponent is effectively one higher,
+                        // but there's no extra normalisation bit in the mantissa
+                        exponent = 1;
+                    }
+                    else
+                    {
+                        // Normal numbers; leave exponent as it is but add extra
+                        // bit to the front of the mantissa
+                        mantissa = mantissa | (1L << 52);
+                    }
+
+                    // Bias the exponent. It's actually biased by 1023, but we're
+                    // treating the mantissa as m.0 rather than 0.m, so we need
+                    // to subtract another 52 from it.
+                    exponent -= 1075;
+
+                    if (mantissa != 0)
+                    {
+                        while ((mantissa & 1) == 0)
+                        {
+                            mantissa >>= 1;
+                            exponent++;
+                        }
+                    }
+                }
             }
         }
 
