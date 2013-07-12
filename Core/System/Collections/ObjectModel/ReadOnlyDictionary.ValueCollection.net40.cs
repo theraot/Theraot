4a08e00b035d5e1c4b8c841c749368f3270fac28
Alfonso J. Ramos <theraot@gmail.com>
2013-07-12 11:32:08 -0500

    Added ReadOnlyDictionary

---
+++ Core/System/Collections/ObjectModel/ReadOnlyDictionary.ValueCollection.net40.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Theraot.Core;
+
+namespace System.Collections.ObjectModel
+{
+    public partial class ReadOnlyDictionary<TKey, TValue>
+    {
+        [System.Serializable]
+        public sealed class ValueCollection : ICollection<TValue>, IEnumerable<TValue>, ICollection, IEnumerable
+        {
+            private readonly ICollection<TValue> _wrapped;
+
+            internal ValueCollection(ICollection<TValue> wrapped)
+            {
+                _wrapped = Check.NotNullArgument(wrapped, "wrapped");
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return _wrapped.Count;
+                }
+            }
+
+            bool ICollection.IsSynchronized
+            {
+                get
+                {
+                    return ((ICollection)_wrapped).IsSynchronized;
+                }
+            }
+
+            object ICollection.SyncRoot
+            {
+                get
+                {
+                    return ((ICollection)_wrapped).SyncRoot;
+                }
+            }
+
+            bool ICollection<TValue>.IsReadOnly
+            {
+                get
+                {
+                    return true;
+                }
+            }
+
+            public void CopyTo(TValue[] array, int arrayIndex)
+            {
+                _wrapped.CopyTo(array, arrayIndex);
+            }
+
+            public IEnumerator<TValue> GetEnumerator()
+            {
+                return _wrapped.GetEnumerator();
+            }
+
+            void ICollection.CopyTo(Array array, int index)
+            {
+                ((ICollection)_wrapped).CopyTo(array, index);
+            }
+
+            void ICollection<TValue>.Add(TValue item)
+            {
+                throw new NotSupportedException();
+            }
+
+            void ICollection<TValue>.Clear()
+            {
+                throw new NotSupportedException();
+            }
+
+            bool ICollection<TValue>.Contains(TValue item)
+            {
+                return _wrapped.Contains(item);
+            }
+
+            bool ICollection<TValue>.Remove(TValue item)
+            {
+                throw new NotSupportedException();
+            }
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
---
+++ Core/System/Collections/ObjectModel/ReadOnlyDictionary.net40.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Theraot.Collections.Specialized;
+using Theraot.Core;
+
+namespace System.Collections.ObjectModel
+{
+    [System.Serializable]
+    public partial class ReadOnlyDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ICollection<KeyValuePair<TKey, TValue>>, IDictionary, ICollection, IReadOnlyDictionary<TKey, TValue>, IReadOnlyCollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IEnumerable
+    {
+        private KeyCollection _keys;
+        private ValueCollection _values;
+        private IDictionary<TKey, TValue> _wrapped;
+
+        public ReadOnlyDictionary(IDictionary<TKey, TValue> dictionary)
+        {
+            _wrapped = Check.NotNullArgument(dictionary, "dictioanry");
+            _keys = new KeyCollection(new DelegatedCollection<TKey>(() => _wrapped.Keys));
+            _values = new ValueCollection(new DelegatedCollection<TValue>(() => _wrapped.Values));
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _wrapped.Count;
+            }
+        }
+
+        public IDictionary<TKey, TValue> Dictionary
+        {
+            get
+            {
+                return _wrapped;
+            }
+        }
+
+        bool ICollection.IsSynchronized
+        {
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            get
+            {
+                return ((ICollection)_wrapped).IsSynchronized;
+            }
+        }
+
+        object ICollection.SyncRoot
+        {
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            get
+            {
+                return ((ICollection)_wrapped).SyncRoot;
+            }
+        }
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly
+        {
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            get
+            {
+                return true;
+            }
+        }
+
+        bool IDictionary.IsFixedSize
+        {
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            get
+            {
+                return ((IDictionary)_wrapped).IsFixedSize;
+            }
+        }
+
+        bool IDictionary.IsReadOnly
+        {
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            get
+            {
+                return true;
+            }
+        }
+
+        ICollection IDictionary.Keys
+        {
+            get
+            {
+                return _keys;
+            }
+        }
+
+        ICollection IDictionary.Values
+        {
+            get
+            {
+                return _values;
+            }
+        }
+
+        ICollection<TKey> IDictionary<TKey, TValue>.Keys
+        {
+            get
+            {
+                return _keys;
+            }
+        }
+
+        ICollection<TValue> IDictionary<TKey, TValue>.Values
+        {
+            get
+            {
+                return _values;
+            }
+        }
+
+        IEnumerable<TKey> IReadOnlyDictionary<TKey, TValue>.Keys
+        {
+            get
+            {
+                return _keys;
+            }
+        }
+
+        IEnumerable<TValue> IReadOnlyDictionary<TKey, TValue>.Values
+        {
+            get
+            {
+                return _values;
+            }
+        }
+
+        public KeyCollection Keys
+        {
+            get
+            {
+                return _keys;
+            }
+        }
+
+        public ValueCollection Values
+        {
+            get
+            {
+                return _values;
+            }
+        }
+
+        object IDictionary.this[object key]
+        {
+            get
+            {
+                if (ReferenceEquals(key, null))
+                {
+                    throw new ArgumentNullException("key");
+                }
+                else if (key is TKey)
+                {
+                    return this[(TKey)key];
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        TValue IDictionary<TKey, TValue>.this[TKey key]
+        {
+            get
+            {
+                return this[key];
+            }
+            [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+            set
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return _wrapped[key];
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return _wrapped.ContainsKey(key);
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _wrapped.GetEnumerator();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void ICollection.CopyTo(Array array, int index)
+        {
+            ((ICollection)_wrapped).CopyTo(array, index);
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void ICollection<KeyValuePair<TKey, TValue>>.Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        bool ICollection<KeyValuePair<TKey, TValue>>.Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _wrapped.Contains(item);
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void ICollection<KeyValuePair<TKey, TValue>>.CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            _wrapped.CopyTo(array, arrayIndex);
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void IDictionary.Add(object key, object value)
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void IDictionary.Clear()
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        bool IDictionary.Contains(object key)
+        {
+            if (ReferenceEquals(key, null))
+            {
+                throw new ArgumentNullException("key");
+            }
+            else if (key is TKey)
+            {
+                return ContainsKey((TKey)key);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        IDictionaryEnumerator IDictionary.GetEnumerator()
+        {
+            return ((IDictionary)_wrapped).GetEnumerator();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void IDictionary.Remove(object key)
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        void IDictionary<TKey, TValue>.Add(TKey key, TValue value)
+        {
+            throw new NotSupportedException();
+        }
+
+        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes", Justification = "Microsoft's Design")]
+        bool IDictionary<TKey, TValue>.Remove(TKey key)
+        {
+            throw new NotSupportedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _wrapped.TryGetValue(key, out value);
+        }
+    }
+}
