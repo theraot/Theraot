284a1f4ac864ec6e71554a8deabde16d181fedd1
Alfonso J. Ramos <theraot@gmail.com>
2013-07-16 00:02:27 -0500

    Added DelegateEqualityComparer

---
+++ Core/Theraot/Core/DelegateEqualityComparer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Theraot.Core
+{
+    public sealed class DelegateEqualityComparer : IEqualityComparer<Delegate>
+    {
+        private static readonly DelegateEqualityComparer _instance = new DelegateEqualityComparer();
+
+        private DelegateEqualityComparer()
+        {
+            //Empty
+        }
+
+        public static DelegateEqualityComparer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public bool Equals(Delegate x, Delegate y)
+        {
+            return CompareInternal(x, y);
+        }
+
+        public int GetHashCode(Delegate obj)
+        {
+            return obj.Method.GetHashCode();
+        }
+
+        private static bool CompareInternal(Delegate x, Delegate y)
+        {
+            if (!ReferenceEquals(x.Target, y.Target))
+            {
+                return false;
+            }
+            else
+            {
+                var leftBody = x.Method.GetMethodBody().GetILAsByteArray();
+                var rightBody = y.Method.GetMethodBody().GetILAsByteArray();
+                if (leftBody.Length != rightBody.Length)
+                {
+                    return false;
+                }
+                else
+                {
+                    for (var index = 0; index < leftBody.Length; index++)
+                    {
+                        if (leftBody[index] != rightBody[index])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+        }
+    }
+}
