d32c7ac848119fe0f9ea676a55348d3d7cad5ff8
Alfonso J. Ramos <theraot@gmail.com>
2013-09-09 19:33:19 -0500

    ReentryGuard now uses Promise

--- Core/Theraot/Threading/ReentryGuard.cs
+++ Core/Theraot/Threading/ReentryGuard.cs
@@ -1,6 +1,4 @@
-﻿#if FAT
-
-using System;
+﻿using System;
 
 using Theraot.Collections;
 using Theraot.Threading.Needles;
@@ -23,8 +21,17 @@ namespace Theraot.Threading
                 );
         }
 
-        [global::System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "Pokemon")]
-        public void Execute(Action action)
+        private static Action ExecutePending(Tuple<ExtendedQueue<Action>, Guard> local)
+        {
+            Action action;
+            while (local.Item1.TryTake(out action))
+            {
+                action.Invoke();
+            }
+            return action;
+        }
+
+        public IPromise Execute(Action action)
         {
             var local = _workQueue.Value.Value;
             IDisposable engagement;
@@ -34,34 +41,84 @@ namespace Theraot.Threading
                 {
                     try
                     {
-                        action();
+                        action.Invoke();
+                        return new PromiseNeedle(true);
                     }
-                    catch
+                    catch (Exception exception)
                     {
-                        //Pokemon
+                        return new PromiseNeedle(exception);
                     }
                     finally
                     {
-                        while (local.Item1.TryTake(out action))
-                        {
-                            try
-                            {
-                                action();
-                            }
-                            catch
-                            {
-                                //Pokemon
-                            }
-                        }
+                        ExecutePending(local);
                     }
                 }
             }
             else
             {
-                local.Item1.Add(action);
+                IPromised promised;
+                var result = new PromiseNeedle(out promised, false);
+                local.Item1.Add
+                (
+                    () =>
+                    {
+                        try
+                        {
+                            action.Invoke();
+                            promised.OnCompleted();
+                        }
+                        catch (Exception exception)
+                        {
+                            promised.OnError(exception);
+                        }
+                    }
+                );
+                return result;
+            }
+        }
+
+        public IPromise<T> Execute<T>(Func<T> action)
+        {
+            var local = _workQueue.Value.Value;
+            IDisposable engagement;
+            if (local.Item2.Enter(out engagement))
+            {
+                using (engagement)
+                {
+                    try
+                    {
+                        return new PromiseNeedle<T>(action.Invoke());
+                    }
+                    catch (Exception exception)
+                    {
+                        return new PromiseNeedle<T>(exception);
+                    }
+                    finally
+                    {
+                        ExecutePending(local);
+                    }
+                }
+            }
+            else
+            {
+                IPromised<T> promised;
+                var result = new PromiseNeedle<T>(out promised, false);
+                local.Item1.Add
+                (
+                    () =>
+                    {
+                        try
+                        {
+                            promised.OnNext(action.Invoke());
+                        }
+                        catch (Exception exception)
+                        {
+                            promised.OnError(exception);
+                        }
+                    }
+                );
+                return result;
             }
         }
     }
 }
-
-#endif
