5d0ce68aa65b3a6aee5f1784582ff39b9a6bbe71
Alfonso J. Ramos <theraot@gmail.com>
2013-09-10 13:39:38 -0500

    Rece condition improvement
    Promises are queued before attempting to execute
    
    Note: I need to chain promises, first Work must return Promises

--- Core/Theraot/Threading/ReentryGuard.cs
+++ Core/Theraot/Threading/ReentryGuard.cs
@@ -21,6 +21,79 @@ namespace Theraot.Threading
                 );
         }
 
+        public IPromise Execute(Action action)
+        {
+            var local = _workQueue.Value.Value;
+            var result = AddExecution(action, local);
+            IDisposable engagement;
+            if (local.Item2.Enter(out engagement))
+            {
+                using (engagement)
+                {
+                    ExecutePending(local);
+                }
+            }
+            return result;
+        }
+
+        public IPromise<T> Execute<T>(Func<T> action)
+        {
+            var local = _workQueue.Value.Value;
+            var result = AddExecution<T>(action, local);
+            IDisposable engagement;
+            if (local.Item2.Enter(out engagement))
+            {
+                using (engagement)
+                {
+                    ExecutePending(local);
+                }
+            }
+            return result;
+        }
+
+        private static IPromise AddExecution(Action action, Tuple<ExtendedQueue<Action>, Guard> local)
+        {
+            IPromised promised;
+            var result = new PromiseNeedle(out promised, false);
+            local.Item1.Add
+            (
+                () =>
+                {
+                    try
+                    {
+                        action.Invoke();
+                        promised.OnCompleted();
+                    }
+                    catch (Exception exception)
+                    {
+                        promised.OnError(exception);
+                    }
+                }
+            );
+            return result;
+        }
+
+        private static IPromise<T> AddExecution<T>(Func<T> action, Tuple<ExtendedQueue<Action>, Guard> local)
+        {
+            IPromised<T> promised;
+            var result = new PromiseNeedle<T>(out promised, false);
+            local.Item1.Add
+            (
+                () =>
+                {
+                    try
+                    {
+                        promised.OnNext(action.Invoke());
+                    }
+                    catch (Exception exception)
+                    {
+                        promised.OnError(exception);
+                    }
+                }
+            );
+            return result;
+        }
+
         private static Action ExecutePending(Tuple<ExtendedQueue<Action>, Guard> local)
         {
             Action action;
@@ -29,96 +102,6 @@ namespace Theraot.Threading
                 action.Invoke();
             }
             return action;
-        }
-
-        public IPromise Execute(Action action)
-        {
-            var local = _workQueue.Value.Value;
-            IDisposable engagement;
-            if (local.Item2.Enter(out engagement))
-            {
-                using (engagement)
-                {
-                    try
-                    {
-                        action.Invoke();
-                        return new PromiseNeedle(true);
-                    }
-                    catch (Exception exception)
-                    {
-                        return new PromiseNeedle(exception);
-                    }
-                    finally
-                    {
-                        ExecutePending(local);
-                    }
-                }
-            }
-            else
-            {
-                IPromised promised;
-                var result = new PromiseNeedle(out promised, false);
-                local.Item1.Add
-                (
-                    () =>
-                    {
-                        try
-                        {
-                            action.Invoke();
-                            promised.OnCompleted();
-                        }
-                        catch (Exception exception)
-                        {
-                            promised.OnError(exception);
-                        }
-                    }
-                );
-                return result;
-            }
-        }
-
-        public IPromise<T> Execute<T>(Func<T> action)
-        {
-            var local = _workQueue.Value.Value;
-            IDisposable engagement;
-            if (local.Item2.Enter(out engagement))
-            {
-                using (engagement)
-                {
-                    try
-                    {
-                        return new PromiseNeedle<T>(action.Invoke());
-                    }
-                    catch (Exception exception)
-                    {
-                        return new PromiseNeedle<T>(exception);
-                    }
-                    finally
-                    {
-                        ExecutePending(local);
-                    }
-                }
-            }
-            else
-            {
-                IPromised<T> promised;
-                var result = new PromiseNeedle<T>(out promised, false);
-                local.Item1.Add
-                (
-                    () =>
-                    {
-                        try
-                        {
-                            promised.OnNext(action.Invoke());
-                        }
-                        catch (Exception exception)
-                        {
-                            promised.OnError(exception);
-                        }
-                    }
-                );
-                return result;
-            }
         }
     }
 }
