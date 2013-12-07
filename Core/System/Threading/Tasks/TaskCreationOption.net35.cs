b7b79377a53203be7e5f8d6f7753358609b024fa
= <theraot@gmail.com>
2013-12-07 17:50:00 -0500

    Added TaskStatus and TaskCreationOption

---
+++ Core/System/Threading/Tasks/TaskCreationOption.net35.cs
@@ -0,0 +1,20 @@
+﻿#if NET20 || NET30 || NET35
+
+using System;
+
+namespace System.Threading.Tasks
+{
+    [Flags]
+    [Serializable]
+    public enum TaskCreationOptions
+    {
+        None = 0,
+        PreferFairness = 1,
+        LongRunning = 2,
+        AttachedToParent = 4,
+        DenyChildAttach = 8,
+        HideScheduler = 16
+    }
+}
+
+#endif
---
+++ Core/System/Threading/Tasks/TaskStatus.net35.cs
@@ -0,0 +1,20 @@
+﻿#if NET20 || NET30 || NET35
+
+using System;
+
+namespace System.Threading.Tasks
+{
+    public enum TaskStatus
+    {
+        Created,
+        WaitingForActivation,
+        WaitingToRun,
+        Running,
+        WaitingForChildrenToComplete,
+        RanToCompletion,
+        Canceled,
+        Faulted
+    }
+}
+
+#endif
