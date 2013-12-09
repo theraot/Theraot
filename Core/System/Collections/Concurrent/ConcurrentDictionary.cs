74239b5991e38b70ac4cf3e2fa3449ab24092694
= <theraot@gmail.com>
2013-12-09 15:54:40 -0500

    Added ConcurrentDictionary stub.

---
+++ Core/System/Collections/Concurrent/ConcurrentDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Theraot.Collections.Specialized;
+using Theraot.Collections.ThreadSafe;
+
+namespace System.Collections.Concurrent
+{
+    [SerializableAttribute]
+    public class ConcurrentDictionary<TKey, TValue> : IDictionary<TKey, TValue>, ICollection<KeyValuePair<TKey, TValue>>, IEnumerable<KeyValuePair<TKey, TValue>>, IDictionary, ICollection, IEnumerable
+    {
+        //TODO
+    }
+}
