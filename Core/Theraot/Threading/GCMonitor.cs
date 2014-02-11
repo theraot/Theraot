1b35253ac4f7a754a825bf1a8f5882dc3af0d1e6
= <theraot@gmail.com>
2014-02-11 04:25:36 -0500

    Changed state machine
    The new implementation will not allow simultaneous execution of the
    Collect event.

--- Core/Theraot/Threading/GCMonitor.cs
+++ Core/Theraot/Threading/GCMonitor.cs
@@ -8,15 +8,15 @@ namespace Theraot.Threading
     [global::System.Diagnostics.DebuggerNonUserCode]
     public static class GCMonitor
     {
-        private const int INT_BoolFalse = 0;
-        private const int INT_BoolTrue = 1;
         private const int INT_CapacityHint = 1024;
         private const int INT_MaxProbingHint = 32;
+        private const int INT_StatusFinished = 3;
         private const int INT_StatusNotReady = -2;
         private const int INT_StatusPending = -1;
         private const int INT_StatusReady = 0;
+        private const int INT_StatusRequested = 2;
+        private const int INT_StatusRunning = 1;
         private static WeakDelegateSet _collectedEventHandlers;
-        private static int _finished = INT_BoolFalse;
         private static int _status = INT_StatusNotReady;
 
         static GCMonitor()
@@ -75,28 +75,27 @@ namespace Theraot.Threading
         private static void Initialize()
         {
             var check = Interlocked.CompareExchange(ref _status, INT_StatusPending, INT_StatusNotReady);
-            if (check == INT_StatusNotReady)
+            switch (check)
             {
-                GC.KeepAlive(new GCProbe());
-                _collectedEventHandlers = new WeakDelegateSet(INT_CapacityHint, false, false, INT_MaxProbingHint);
-                Thread.VolatileWrite(ref _status, INT_StatusReady);
-            }
-            else
-            {
-                if (check != INT_StatusReady)
-                {
+                case INT_StatusNotReady:
+                    GC.KeepAlive(new GCProbe());
+                    _collectedEventHandlers = new WeakDelegateSet(INT_CapacityHint, false, false, INT_MaxProbingHint);
+                    Thread.VolatileWrite(ref _status, INT_StatusReady);
+                    break;
+
+                case INT_StatusPending:
                     ThreadingHelper.SpinWaitUntil(ref _status, INT_StatusReady);
-                }
+                    break;
+
+                default:
+                    break;
             }
         }
 
         private static void RaiseCollected()
         {
-            if (Thread.VolatileRead(ref _finished) == INT_BoolTrue || AppDomain.CurrentDomain.IsFinalizingForUnload())
-            {
-                return;
-            }
-            else
+            var check = Interlocked.CompareExchange(ref _status, INT_StatusRunning, INT_StatusRequested);
+            if (check == INT_StatusRequested)
             {
                 try
                 {
@@ -107,12 +106,13 @@ namespace Theraot.Threading
                 {
                     //Pokemon
                 }
+                Thread.VolatileWrite(ref _status, INT_StatusReady);
             }
         }
 
         private static void ReportApplicationDomainExit(object sender, EventArgs e)
         {
-            Thread.VolatileWrite(ref _finished, INT_BoolTrue);
+            Thread.VolatileWrite(ref _status, INT_StatusFinished);
         }
 
         [global::System.Diagnostics.DebuggerNonUserCode]
@@ -128,7 +128,8 @@ namespace Theraot.Threading
                 {
                     try
                     {
-                        if (Thread.VolatileRead(ref _finished) != INT_BoolTrue && !GCMonitor.FinalizingForUnload)
+                        var check = Interlocked.CompareExchange(ref _status, INT_StatusRequested, INT_StatusReady);
+                        if (check == INT_StatusReady)
                         {
                             GC.ReRegisterForFinalize(this);
                             WorkContext.DefaultContext.AddWork(GCMonitor.RaiseCollected).Start();
