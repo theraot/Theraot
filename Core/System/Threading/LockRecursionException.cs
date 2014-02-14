fae28d01f0761c4d58bead7ca942d7c5cfebbbab
= <theraot@gmail.com>
2014-02-14 16:39:51 -0500

    Added LockRecursionException

---
+++ Core/System/Threading/LockRecursionException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Theraot.Core.System.Threading
+{
+    [Serializable]
+    public class LockRecursionException : Exception
+    {
+        public LockRecursionException()
+        {
+            //Empty
+        }
+
+        public LockRecursionException(string message)
+            : base(message)
+        {
+            //Empty
+        }
+
+        public LockRecursionException(string message, Exception inner)
+            : base(message, inner)
+        {
+            //Empty
+        }
+
+        protected LockRecursionException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            //Empty
+        }
+    }
+}
