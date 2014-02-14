13cf11b1d1cd27359710585923eccf545e76a7c3
= <theraot@gmail.com>
2014-02-14 06:44:18 -0500

    Added ThreadEqualityComparer
    ThreadEqualityComparer uses ThreadingHelper.ThreadUniqueId as Hash
    function. The advantage of ThreadUniqueId over Thread.ManagedId is that
    Thread.ManagedId may get recycled. The purpose of ThreadEqualityComparer
    is to ease using Threads as keys for hash structures.

---
+++ Core/Theraot/Core/ThreadEqualityComparer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Theraot.Threading;
+
+namespace Theraot.Core
+{
+    public class ThreadEqualityComparer : IEqualityComparer<Thread>
+    {
+        private static readonly ThreadEqualityComparer _default = new ThreadEqualityComparer();
+
+        private ThreadEqualityComparer()
+        {
+            //Empty
+        }
+
+        public static ThreadEqualityComparer Default
+        {
+            get
+            {
+                return _default;
+            }
+        }
+
+        public bool Equals(Thread x, Thread y)
+        {
+            return ReferenceEquals(x, y);
+        }
+
+        public int GetHashCode(Thread obj)
+        {
+            return ThreadingHelper.ThreadUniqueId;
+        }
+    }
+}
