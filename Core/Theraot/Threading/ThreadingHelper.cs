50bb54419619bed84f6e4054f1bdbc65b0c6103b
= <theraot@gmail.com>
2014-02-14 06:38:18 -0500

    Added HasThreadUniqueId

--- Core/Theraot/Threading/ThreadingHelper.cs
+++ Core/Theraot/Threading/ThreadingHelper.cs
@@ -12,6 +12,14 @@ namespace Theraot.Threading
         private static readonly RuntimeUniqueIdProdiver _threadIdProvider = new RuntimeUniqueIdProdiver();
         private static readonly NoTrackingThreadLocal<RuntimeUniqueIdProdiver.UniqueId> _threadRuntimeUniqueId = new NoTrackingThreadLocal<RuntimeUniqueIdProdiver.UniqueId>(_threadIdProvider.GetNextId);
 
+        public static bool HasThreadUniqueId
+        {
+            get
+            {
+                return _threadRuntimeUniqueId.IsValueCreated;
+            }
+        }
+
         public static RuntimeUniqueIdProdiver.UniqueId ThreadUniqueId
         {
             get
