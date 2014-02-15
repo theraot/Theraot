3da89196a4343c50b5a430774f61f9e32e9459e8
= <theraot@gmail.com>
2014-02-15 03:28:16 -0500

    Added MonitorHelper
    MonitorHelper is intender to add the functionality of the overloads
    added to Monitor in .NET 4.0 and more (CancellationTokens)

---
+++ Core/Theraot/Core/MonitorHelper.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Threading;
+using Theraot.Threading;
+
+namespace Theraot.Core
+{
+    public static class MonitorHelper
+    {
+        public static void Enter(object obj)
+        {
+            Monitor.Enter(obj);
+        }
+
+        public static void Enter(object obj, ref bool taken)
+        {
+            Monitor.Enter(obj);
+            Thread.MemoryBarrier();
+            taken = true;
+        }
+
+        public static void Enter(object obj, CancellationToken cancellationToken)
+        {
+            int count = 0;
+        retry:
+            cancellationToken.ThrowIfCancellationRequested();
+            GC.KeepAlive(cancellationToken.WaitHandle);
+            if (Monitor.TryEnter(obj))
+            {
+                return;
+            }
+            else
+            {
+                ThreadingHelper.SpinOnce(ref count);
+                goto retry;
+            }
+        }
+
+        public static bool TryEnter(object obj)
+        {
+            return Monitor.TryEnter(obj);
+        }
+
+        public static void TryEnter(object obj, ref bool taken)
+        {
+            taken = Monitor.TryEnter(obj);
+        }
+
+        public static bool TryEnter(object obj, int millisecondsTimeout)
+        {
+            return Monitor.TryEnter(obj, millisecondsTimeout);
+        }
+
+        public static void TryEnter(object obj, int millisecondsTimeout, ref bool taken)
+        {
+            taken = Monitor.TryEnter(obj, millisecondsTimeout);
+        }
+
+        public static bool TryEnter(object obj, TimeSpan timeout)
+        {
+            return Monitor.TryEnter(obj, timeout);
+        }
+
+        public static void TryEnter(object obj, TimeSpan timeout, ref bool taken)
+        {
+            taken = Monitor.TryEnter(obj, timeout);
+        }
+
+        public static bool TryEnter(object obj, int millisecondsTimeout, CancellationToken cancellationToken)
+        {
+            if (millisecondsTimeout < -1)
+            {
+                throw new ArgumentOutOfRangeException("millisecondsTimeout");
+            }
+            else if (millisecondsTimeout == -1)
+            {
+                Enter(millisecondsTimeout, cancellationToken);
+                return true;
+            }
+            int count = 0;
+            var start = ThreadingHelper.TicksNow();
+        retry:
+            cancellationToken.ThrowIfCancellationRequested();
+            GC.KeepAlive(cancellationToken.WaitHandle);
+            if (TryEnter(obj))
+            {
+                return true;
+            }
+            else
+            {
+                if (ThreadingHelper.Milliseconds(ThreadingHelper.TicksNow() - start) < millisecondsTimeout)
+                {
+                    ThreadingHelper.SpinOnce(ref count);
+                    goto retry;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static void TryEnter(object obj, int millisecondsTimeout, ref bool taken, CancellationToken cancellationToken)
+        {
+            taken = TryEnter(obj, millisecondsTimeout, cancellationToken);
+        }
+
+        public static bool TryEnter(object obj, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            var milliseconds = (long)timeout.TotalMilliseconds;
+            int count = 0;
+            var start = ThreadingHelper.TicksNow();
+        retry:
+            cancellationToken.ThrowIfCancellationRequested();
+            GC.KeepAlive(cancellationToken.WaitHandle);
+            if (TryEnter(obj))
+            {
+                return true;
+            }
+            else
+            {
+                if (ThreadingHelper.Milliseconds(ThreadingHelper.TicksNow() - start) < milliseconds)
+                {
+                    ThreadingHelper.SpinOnce(ref count);
+                    goto retry;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public static void TryEnter(object obj, TimeSpan timeout, ref bool taken, CancellationToken cancellationToken)
+        {
+            taken = TryEnter(obj, timeout, cancellationToken);
+        }
+    }
+}
