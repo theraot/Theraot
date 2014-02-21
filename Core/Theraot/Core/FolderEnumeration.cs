b6678d3201039c5289f25e37c64f461c4a8bc633
= <theraot@gmail.com>
2014-02-21 16:21:30 -0500

    Added FolderEnumeration
    FolderEnumeration provides an API to iterate over files and folders, it
    will use Directory.GetFiles and Directory.GetDirectories from .NET 2.0
    to .NET 3.5 from .NET 4.0 onward it uses Directory.EnumerateFiles and
    Directory.EnumerateDirectories

---
+++ Core/Theraot/Core/FolderEnumeration.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Theraot.Collections;
+
+namespace Theraot.Core
+{
+    [global::System.Diagnostics.DebuggerNonUserCode]
+    public static class FolderEnumeration
+    {
+        public static IEnumerable<string> GetFiles(string folder, string pattern)
+        {
+            IEnumerable<string> fileEntries = null;
+            try
+            {
+#if NET20 || NET30 || NET35
+                fileEntries = Directory.GetFiles(folder, pattern, SearchOption.TopDirectoryOnly);
+#else
+                fileEntries = Directory.EnumerateFiles(folder, pattern, SearchOption.TopDirectoryOnly);
+#endif
+            }
+            catch (DirectoryNotFoundException)
+            {
+                //Empty
+            }
+            catch (UnauthorizedAccessException)
+            {
+                //Empty
+            }
+            if (fileEntries != null)
+            {
+                return fileEntries;
+            }
+            else
+            {
+                return EmptySet<string>.Instance;
+            }
+        }
+
+        public static IEnumerable<string> GetFilesAndFoldersRecursive(string folder, string pattern)
+        {
+            return GraphHelper.ExploreBreadthFirstTree<string, IEnumerable<string>>
+            (
+                folder,
+                current => GetFolders(current),
+                current => GetFiles(current, pattern).Prepend(current)
+            ).SelectMany(input => input);
+        }
+
+        public static IEnumerable<string> GetFilesRecursive(string folder, string pattern)
+        {
+            return GraphHelper.ExploreBreadthFirstTree<string, IEnumerable<string>>
+            (
+                folder,
+                current => GetFolders(folder),
+                current => GetFiles(current, pattern)
+            ).SelectMany(input => input);
+        }
+
+        public static IEnumerable<string> GetFolders(string folder)
+        {
+            IEnumerable<string> directories;
+#if NET20 || NET30 || NET35
+            directories = Directory.GetDirectories(folder);
+#else
+            directories = Directory.EnumerateDirectories(folder);
+#endif
+            return directories.Where(subFolder => ((File.GetAttributes(subFolder) & FileAttributes.ReparsePoint) != FileAttributes.ReparsePoint));
+        }
+
+        public static IEnumerable<string> GetFoldersRecursive(string folder)
+        {
+            return GraphHelper.ExploreBreadthFirstTree<string>
+            (
+                folder,
+                current => GetFolders(current)
+            );
+        }
+    }
+}
