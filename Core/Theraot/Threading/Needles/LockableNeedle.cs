ba874c2f3c85a3fc286edc3804da7bed965118e0
= <theraot@gmail.com>
2014-05-27 09:02:59 -0500

    ConcurrentDictionary development advance

--- Core/Theraot/Threading/Needles/LockableNeedle.cs
+++ Core/Theraot/Threading/Needles/LockableNeedle.cs
@@ -3,48 +3,119 @@ using System.Threading;
 
 namespace Theraot.Threading.Needles
 {
-    public sealed class LockableNeedle<T, TNeedle> : INeedle<T>
+    public interface ILockable
+    {
+        bool HasOwner { get; }
+
+        bool Capture();
+
+        bool CheckAccess(Thread thread);
+
+        void Uncapture();
+    }
+
+    public sealed class Lockable : ILockable
+    {
+        private readonly LockableContext _context;
+        private readonly NeedleLock<Thread> _lock;
+
+        public Lockable(LockableContext context)
+        {
+            if (ReferenceEquals(context, null))
+            {
+                throw new ArgumentNullException("context");
+            }
+            else
+            {
+                _context = context;
+                _lock = new NeedleLock<Thread>(context._context);
+            }
+        }
+
+        public bool HasOwner
+        {
+            get
+            {
+                return !ReferenceEquals(_lock.Value, null);
+            }
+        }
+
+        public bool Capture()
+        {
+            _context._slot.Value.Capture(_lock);
+            return ReferenceEquals(_lock.Value, Thread.CurrentThread);
+        }
+
+        public bool CheckAccess(Thread thread)
+        {
+            var value = _lock.Value;
+            return ReferenceEquals(value, thread) || ReferenceEquals(value, null);
+        }
+
+        public void Uncapture()
+        {
+            LockSlot<Thread> slot;
+            if (_context._slot.TryGet(out slot))
+            {
+                slot.Uncapture(_lock);
+            }
+            _lock.Free();
+        }
+    }
+
+    public sealed class LockableContext
+    {
+        internal readonly LockContext<Thread> _context;
+        internal TrackingThreadLocal<LockSlot<Thread>> _slot;
+
+        public LockableContext(int capacity)
+        {
+            _context = new LockContext<Thread>(capacity);
+            _slot = new TrackingThreadLocal<LockSlot<Thread>>
+                (
+                    () =>
+                    {
+                        LockSlot<Thread> _lockSlot = null;
+                        ThreadingHelper.SpinWaitUntil(() => _context.ClaimSlot(out _lockSlot));
+                        _lockSlot.Value = Thread.CurrentThread;
+                        return _lockSlot;
+                    }
+                );
+        }
+    }
+
+    public sealed class LockableNeedle<T, TNeedle> : INeedle<T>, ILockable
         where TNeedle : class, INeedle<T>
     {
-        [ThreadStatic]
-        private static LockSlot<Thread> _thread_slot;
-        private readonly Context _context;
-        private LockSlot<Thread> _slot;
-        private int _count;
+        private readonly Lockable _lockable;
         private readonly TNeedle _needle;
-        public sealed class Context
-        {
-            internal readonly LockContext<Thread> _context;
 
-            public Context(int capacity)
+        public LockableNeedle(LockableContext context, TNeedle needle)
+        {
+            if (ReferenceEquals(context, null))
             {
-                _context = new LockContext<Thread>(capacity);
+                throw new ArgumentNullException("context");
+            }
+            else
+            {
+                _lockable = new Lockable(context);
+                _needle = needle;
             }
         }
 
-        public LockableNeedle(Context context, TNeedle needle)
+        public bool HasOwner
         {
-            _context = context;
-            _needle = needle;
+            get
+            {
+                return _lockable.HasOwner;
+            }
         }
 
-        public IDisposable Lock()
+        bool IReadOnlyNeedle<T>.IsAlive
         {
-            if (ReferenceEquals(_thread_slot, null))
+            get
             {
-                ThreadingHelper.SpinWaitUntil(() => _context._context.ClaimSlot(out _thread_slot));
-                _thread_slot.Value = Thread.CurrentThread;
-            }
-            _slot = _thread_slot;
-            Interlocked.Increment(ref _count);
-            return DisposableAkin.Create(OnRelease);
-        }
-        private void OnRelease()
-        {
-            if (Interlocked.Decrement(ref _count) == 0 && !ReferenceEquals(_slot, null))
-            {
-                _slot.Free();
-                _slot = null;
+                return _needle.IsAlive;
             }
         }
 
@@ -56,27 +127,38 @@ namespace Theraot.Threading.Needles
             }
             set
             {
-                using (Lock())
+                if (_lockable.CheckAccess(Thread.CurrentThread))
                 {
                     _needle.Value = value;
+                }
+                else
+                {
+                    throw new InvalidOperationException("");
                 }
             }
         }
 
+        public bool Capture()
+        {
+            return _lockable.Capture();
+        }
+
+        public bool CheckAccess(Thread thread)
+        {
+            return _lockable.CheckAccess(thread);
+        }
+
         void INeedle<T>.Free()
         {
-            using (Lock())
+            if (_lockable.CheckAccess(Thread.CurrentThread))
             {
                 _needle.Free();
             }
         }
 
-        bool IReadOnlyNeedle<T>.IsAlive
+        public void Uncapture()
         {
-            get
-            {
-                return _needle.IsAlive;
-            }
+            _lockable.Uncapture();
         }
     }
 }
