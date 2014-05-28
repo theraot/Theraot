671ab0b736829a96d133c4569a39ca4616f5a7af
= <theraot@gmail.com>
2014-05-28 13:19:18 -0500

    Linq Clean up (more)

--- Core/System/Linq/EnumerableExecutor1.net35.cs
+++ Core/System/Linq/EnumerableExecutor1.net35.cs
@@ -1,7 +1,5 @@
 #if NET20 || NET30 || NET35
 
-using System.Linq.Expressions;
-
 namespace System.Linq
 {
     public class EnumerableExecutor<T> : EnumerableExecutor
--- Core/System/Linq/IGrouping.net30.cs
+++ Core/System/Linq/IGrouping.net30.cs
@@ -1,11 +1,10 @@
 ﻿#if NET20 || NET30
 
-using System.Collections;
 using System.Collections.Generic;
 
 namespace System.Linq
 {
-    public interface IGrouping<out TKey, TElement> : IEnumerable<TElement>, IEnumerable
+    public interface IGrouping<out TKey, TElement> : IEnumerable<TElement>
     {
         TKey Key
         {
