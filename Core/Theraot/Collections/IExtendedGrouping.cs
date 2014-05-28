769b6400111ca463f8b0b045f1795ff721b40fc6
= <theraot@gmail.com>
2014-05-28 10:30:15 -0500

    Made IExtendedGrouping variant when it can be

--- Core/Theraot/Collections/IExtendedGrouping.cs
+++ Core/Theraot/Collections/IExtendedGrouping.cs
@@ -2,7 +2,11 @@ using System.Linq;
 
 namespace Theraot.Collections
 {
+#if NET35
     public interface IExtendedGrouping<TKey, TElement> : IGrouping<TKey, TElement>, IExtendedReadOnlyCollection<TElement>
+#else
+    public interface IExtendedGrouping<out TKey, TElement> : IGrouping<TKey, TElement>, IExtendedReadOnlyCollection<TElement>
+#endif
     {
         //Empty
     }
