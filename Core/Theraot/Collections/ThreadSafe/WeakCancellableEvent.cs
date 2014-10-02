dc02b044019053ba2f28cf322f251822d147e77f
= <theraot@gmail.com>
2014-10-02 04:21:05 -0500

    Added WeakCancellableEvent

---
+++ Core/Theraot/Collections/ThreadSafe/WeakCancellableEvent.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel;
+
+namespace Theraot.Collections.ThreadSafe
+{
+    [global::System.Diagnostics.DebuggerNonUserCode]
+    public class WeakCancellableEvent<TCancelEventArgs> : WeakEvent<TCancelEventArgs>
+        where TCancelEventArgs : CancelEventArgs
+    {
+        public WeakCancellableEvent()
+        {
+            //Empty
+        }
+
+        public WeakCancellableEvent(bool reentryGuard)
+            :base(reentryGuard)
+        {
+            //Empty
+        }
+
+        public override void Invoke(object sender, TCancelEventArgs eventArgs)
+        {
+            foreach (var handler in EventHandlers)
+            {
+                handler.DynamicInvoke(eventArgs);
+                if (eventArgs.Cancel)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
