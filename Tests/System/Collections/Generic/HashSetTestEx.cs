06a65bca7b4be47fd4f77a20c272a59134802ef4
= <theraot@gmail.com>
2014-10-05 21:39:47 -0500

    One more little test for HashSet
    Making sure the enumerator throws when the HashSet is modified -
    ensuring I don't adventure into changing the enumerator behaviour.

---
+++ Tests/System/Collections/Generic/HashSetTestEx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MonoTests.System.Collections.Generic
+{
+    [TestFixture]
+    class HashSetTestEx
+    {
+        [Test]
+        public void NotSafeEnumerator()
+        {
+            // This is the way Enumerators normally work
+            var x = new HashSet<int> {14};
+            Assert.Throws
+            (
+                typeof(InvalidOperationException),
+                () =>
+                {
+                    foreach (var item in x)
+                    {
+                        x.Add(item * 3);
+                    }
+                }
+            );
+        }
+    }
+}
