34cea46a5eafc4a81b32548e232b19d9aa62adc0
= <theraot@gmail.com>
2014-10-09 17:02:08 -0500

    Progressor tests clean up

--- Tests/Theraot/Collections/ProgressorTest.cs
+++ Tests/Theraot/Collections/ProgressorTest.cs
@@ -1,9 +1,7 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
 using Theraot.Collections;
 
 namespace Tests.Theraot.Collections
@@ -12,9 +10,135 @@ namespace Tests.Theraot.Collections
     public class ProgressorTest
     {
         [Test]
+        public void ConvertedProgressor()
+        {
+            var source = new Progressor<int>(new[] { 0, 1, 2, 3, 4, 5 });
+            var progresor = Progressor<string>.CreateConverted(source, input => input.ToString(CultureInfo.InvariantCulture));
+            int indexA = 0;
+            int indexB = 0;
+            progresor.SubscribeAction
+            (
+                value =>
+                {
+                    Assert.AreEqual(value, indexB.ToString(CultureInfo.InvariantCulture));
+                    indexB++;
+                }
+            );
+            string item;
+            while (progresor.TryTake(out item))
+            {
+                Assert.AreEqual(item, indexA.ToString(CultureInfo.InvariantCulture));
+                indexA++;
+            }
+            Assert.AreEqual(indexA, 6);
+            Assert.AreEqual(indexA, indexB);
+        }
+
+        [Test]
+        public void CreatedFilteredConverted()
+        {
+            var source = new Progressor<int>(new[] { 0, 8, 1, 8, 2, 3, 4, 8, 5 });
+            var progresor = Progressor<string>.CreatedFilteredConverted(source, input => input != 8, input => input.ToString(CultureInfo.InvariantCulture));
+            int indexA = 0;
+            int indexB = 0;
+            progresor.SubscribeAction
+            (
+                value =>
+                {
+                    Assert.AreEqual(value, indexB.ToString(CultureInfo.InvariantCulture));
+                    indexB++;
+                }
+            );
+            string item;
+            while (progresor.TryTake(out item))
+            {
+                Assert.AreEqual(item, indexA.ToString(CultureInfo.InvariantCulture));
+                indexA++;
+            }
+            Assert.AreEqual(indexA, 6);
+            Assert.AreEqual(indexA, indexB);
+        }
+
+        [Test]
         public void EnumerableProgressor()
         {
-            var source = new [] {0, 1, 2, 3, 4, 5};
+            var source = new[] { 0, 1, 2, 3, 4, 5 };
+            var progresor = new Progressor<int>(source);
+            int indexA = 0;
+            int indexB = 0;
+            progresor.SubscribeAction
+            (
+                value =>
+                {
+                    Assert.AreEqual(value, indexB);
+                    indexB++;
+                }
+            );
+            int item;
+            while (progresor.TryTake(out item))
+            {
+                Assert.AreEqual(item, indexA);
+                indexA++;
+            }
+            Assert.AreEqual(indexA, 6);
+            Assert.AreEqual(indexA, indexB);
+        }
+
+        [Test]
+        public void FilteredProgressor()
+        {
+            var source = new Progressor<int>(new[] { 0, 8, 1, 8, 2, 3, 4, 8, 5 });
+            var progresor = Progressor<int>.CreatedFiltered(source, input => input != 8);
+            int indexA = 0;
+            int indexB = 0;
+            progresor.SubscribeAction
+            (
+                value =>
+                {
+                    Assert.AreEqual(value, indexB);
+                    indexB++;
+                }
+            );
+            int item;
+            while (progresor.TryTake(out item))
+            {
+                Assert.AreEqual(item, indexA);
+                indexA++;
+            }
+            Assert.AreEqual(indexA, 6);
+            Assert.AreEqual(indexA, indexB);
+        }
+
+        [Test]
+        public void ObservableProgressor()
+        {
+            var source = new Progressor<int>(new[] { 0, 1, 2, 3, 4, 5 });
+            var progresor = new Progressor<int>((source as IObservable<int>));
+            int indexA = 0;
+            int indexB = 0;
+            progresor.SubscribeAction
+            (
+                value =>
+                {
+                    Assert.AreEqual(value, indexB);
+                    indexB++;
+                }
+            );
+            source.AsEnumerable().Consume();
+            int item;
+            while (progresor.TryTake(out item))
+            {
+                Assert.AreEqual(item, indexA);
+                indexA++;
+            }
+            Assert.AreEqual(indexA, 6);
+            Assert.AreEqual(indexA, indexB);
+        }
+
+        [Test]
+        public void ProgressorProgressor()
+        {
+            var source = new Progressor<int>(new[] { 0, 1, 2, 3, 4, 5 });
             var progresor = new Progressor<int>(source);
             int indexA = 0;
             int indexB = 0;
@@ -76,132 +200,6 @@ namespace Tests.Theraot.Collections
             while (progresor.TryTake(out item))
             {
                 Assert.AreEqual(item, indexA);
-                indexA++;
-            }
-            Assert.AreEqual(indexA, 6);
-            Assert.AreEqual(indexA, indexB);
-        }
-        
-        [Test]
-        public void ProgressorProgressor()
-        {
-            var source = new Progressor<int>(new[] {0, 1, 2, 3, 4, 5});
-            var progresor = new Progressor<int>(source);
-            int indexA = 0;
-            int indexB = 0;
-            progresor.SubscribeAction
-            (
-                value =>
-                {
-                    Assert.AreEqual(value, indexB);
-                    indexB++;
-                }
-            );
-            int item;
-            while (progresor.TryTake(out item))
-            {
-                Assert.AreEqual(item, indexA);
-                indexA++;
-            }
-            Assert.AreEqual(indexA, 6);
-            Assert.AreEqual(indexA, indexB);
-        }
-
-        [Test]
-        public void ObservableProgressor()
-        {
-            var source = new Progressor<int>(new[] { 0, 1, 2, 3, 4, 5 });
-            var progresor = new Progressor<int>((source as IObservable<int>));
-            int indexA = 0;
-            int indexB = 0;
-            progresor.SubscribeAction
-            (
-                value =>
-                {
-                    Assert.AreEqual(value, indexB);
-                    indexB++;
-                }
-            );
-            source.AsEnumerable().Consume();
-            int item;
-            while (progresor.TryTake(out item))
-            {
-                Assert.AreEqual(item, indexA);
-                indexA++;
-            }
-            Assert.AreEqual(indexA, 6);
-            Assert.AreEqual(indexA, indexB);
-        }
-
-        [Test]
-        public void ConvertedProgressor()
-        {
-            var source = new Progressor<int>(new[] { 0, 1, 2, 3, 4, 5 });
-            var progresor = Progressor<string>.CreateConverted(source, input => input.ToString(CultureInfo.InvariantCulture));
-            int indexA = 0;
-            int indexB = 0;
-            progresor.SubscribeAction
-            (
-                value =>
-                {
-                    Assert.AreEqual(value, indexB.ToString(CultureInfo.InvariantCulture));
-                    indexB++;
-                }
-            );
-            string item;
-            while (progresor.TryTake(out item))
-            {
-                Assert.AreEqual(item, indexA.ToString(CultureInfo.InvariantCulture));
-                indexA++;
-            }
-            Assert.AreEqual(indexA, 6);
-            Assert.AreEqual(indexA, indexB);
-        }
-
-        [Test]
-        public void FilteredProgressor()
-        {
-            var source = new Progressor<int>(new[] { 0, 8, 1, 8, 2, 3, 4, 8, 5 });
-            var progresor = Progressor<int>.CreatedFiltered(source, input => input != 8);
-            int indexA = 0;
-            int indexB = 0;
-            progresor.SubscribeAction
-            (
-                value =>
-                {
-                    Assert.AreEqual(value, indexB);
-                    indexB++;
-                }
-            );
-            int item;
-            while (progresor.TryTake(out item))
-            {
-                Assert.AreEqual(item, indexA);
-                indexA++;
-            }
-            Assert.AreEqual(indexA, 6);
-            Assert.AreEqual(indexA, indexB);
-        }
-
-        [Test]
-        public void CreatedFilteredConverted()
-        {
-            var source = new Progressor<int>(new[] { 0, 8, 1, 8, 2, 3, 4, 8, 5 });
-            var progresor = Progressor<string>.CreatedFilteredConverted(source, input => input != 8, input => input.ToString(CultureInfo.InvariantCulture));
-            int indexA = 0;
-            int indexB = 0;
-            progresor.SubscribeAction
-            (
-                value =>
-                {
-                    Assert.AreEqual(value, indexB.ToString(CultureInfo.InvariantCulture));
-                    indexB++;
-                }
-            );
-            string item;
-            while (progresor.TryTake(out item))
-            {
-                Assert.AreEqual(item, indexA.ToString(CultureInfo.InvariantCulture));
                 indexA++;
             }
             Assert.AreEqual(indexA, 6);
