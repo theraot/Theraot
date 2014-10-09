75cb9c124708e77d0356a9926dd50efec875c9f1
= <theraot@gmail.com>
2014-10-09 13:42:01 -0500

    Observable extensions

--- Core/Theraot/Collections/ProgressorExtensions.cs
+++ Core/Theraot/Collections/ProgressorExtensions.cs
@@ -7,16 +7,6 @@ namespace Theraot.Collections
 {
     public static class ProgressorExtensions
     {
-        public static IDisposable SubscribeAction<T>(this IObservable<T> observable, Action<T> listener)
-        {
-            return Check.NotNullArgument(observable, "observable").Subscribe(listener.ToObserver());
-        }
-
-        public static IDisposable SubscribeAction<TInput, TOutput>(this IObservable<TInput> observable, Action<TOutput> listener, Converter<TInput, TOutput> converter)
-        {
-            return Check.NotNullArgument(observable, "observable").Subscribe(listener.ToObserver(converter));
-        }
-
         public static IEnumerable<T> All<T>(this IProgressor<T> progressor)
         {
             var _progressor = Check.NotNullArgument(progressor, "progressor");
@@ -60,43 +50,6 @@ namespace Theraot.Collections
                 {
                     break;
                 }
-            }
-        }
-
-        public static IObserver<T> ToObserver<T>(this Action<T> listener)
-        {
-            return new ActionObserver<T>(listener);
-        }
-
-        public static IObserver<TInput> ToObserver<TInput, TOutput>(this Action<TOutput> listener, Converter<TInput, TOutput> converter)
-        {
-            var _converter = Check.NotNullArgument(converter, "converter");
-            return new ActionObserver<TInput>(input => listener(_converter.Invoke(input)));
-        }
-
-        [Serializable]
-        private sealed class ActionObserver<T> : IObserver<T>
-        {
-            private readonly Action<T> _action;
-
-            public ActionObserver(Action<T> action)
-            {
-                _action = Check.NotNullArgument(action, "action");
-            }
-
-            public void OnCompleted()
-            {
-                //Empty
-            }
-
-            public void OnError(Exception error)
-            {
-                GC.KeepAlive(error);
-            }
-
-            public void OnNext(T value)
-            {
-                _action.Invoke(value);
             }
         }
     }
