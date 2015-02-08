c57fabab56648702cb8051b6b1beb1655150c716
= <theraot@gmail.com>
2015-02-08 06:07:18 -0500

    Imported ArrayReservoir and Pool
    This code comes form the project Theraot.Collections.ThreadSafe

--- Core/Theraot/Collections/ThreadSafe/ArrayReservoir.cs
+++ Core/Theraot/Collections/ThreadSafe/ArrayReservoir.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-
-#if FAT
-
-using Theraot.Threading;
-
-#else
-
+using System.Threading;
 using Theraot.Core;
-
-#endif
 
 namespace Theraot.Collections.ThreadSafe
 {
     public static class ArrayReservoir<T>
     {
-#if !FAT
-        private const int INT_MaxCapacity = 1024;
-        private const int INT_MinCapacity = 16;
-#endif
+        // sizes:
+        // 0  1   2   3   4    5    6    7
+        // 8, 16, 32, 64, 128, 256, 512, 1024
+
+        private const int INT_CapacityCount = INT_MaxCapacityLog2 - INT_MinCapacityLog2;
+        private const int INT_MaxCapacity = 1 << INT_MaxCapacityLog2;
+        private const int INT_MaxCapacityLog2 = 10;
+        private const int INT_MinCapacity = 1 << INT_MinCapacityLog2;
+        private const int INT_MinCapacityLog2 = 3;
+        private const int INT_PoolSize = 16;
 
         private static readonly T[] _emptyArray;
+        private static readonly Pool<T[]>[] _pools;
+        private static int _done;
 
         static ArrayReservoir()
         {
@@ -31,6 +31,21 @@ namespace Theraot.Collections.ThreadSafe
             {
                 _emptyArray = new T[0];
             }
+            _pools = new Pool<T[]>[INT_CapacityCount];
+            for (int index = 0; index < INT_CapacityCount; index++)
+            {
+                var currentIndex = index;
+                _pools[index] = new Pool<T[]>
+                    (
+                        INT_PoolSize,
+                        item =>
+                        {
+                            var currentCapacity = INT_MinCapacity << currentIndex;
+                            Array.Clear(item, 0, currentCapacity);
+                        }
+                    );
+            }
+            Thread.VolatileWrite(ref _done, 1);
         }
 
         public static T[] EmptyArray
@@ -43,38 +58,42 @@ namespace Theraot.Collections.ThreadSafe
 
         internal static void DonateArray(T[] donation)
         {
-#if FAT
-            ArrayPool<T>.DonateArray(donation);
-#endif
+            if (donation == null || Thread.VolatileRead(ref _done) == 0)
+            {
+                return;
+            }
+            var capacity = donation.Length;
+            if (capacity == 0 || capacity < INT_MinCapacity || capacity > INT_MaxCapacity)
+            {
+                return;
+            }
+            capacity = NumericHelper.PopulationCount(capacity) == 1 ? capacity : NumericHelper.NextPowerOf2(capacity);
+            var index = NumericHelper.Log2(capacity) - INT_MinCapacityLog2;
+            _pools[index].Donate(donation);
         }
 
         internal static T[] GetArray(int capacity)
         {
-#if FAT
-            return ArrayPool<T>.GetArray(capacity);
-#else
             if (capacity == 0)
             {
                 return _emptyArray;
             }
-            else if (capacity < INT_MinCapacity)
+            if (capacity < INT_MinCapacity)
             {
                 capacity = INT_MinCapacity;
             }
-            else
+            capacity = NumericHelper.PopulationCount(capacity) == 1 ? capacity : NumericHelper.NextPowerOf2(capacity);
+            if (capacity <= INT_MaxCapacity && Thread.VolatileRead(ref _done) == 1)
             {
-                if (capacity > INT_MaxCapacity)
+                var index = NumericHelper.Log2(capacity) - INT_MinCapacityLog2;
+                T[] result;
+                var currentPool = _pools[index];
+                if (currentPool.TryGet(out result))
                 {
-                    //Too big to leak it
-                    return new T[capacity];
-                }
-                else
-                {
-                    capacity = NumericHelper.PopulationCount(capacity) == 1 ? capacity : NumericHelper.NextPowerOf2(capacity);
+                    return result;
                 }
             }
             return new T[capacity];
-#endif
         }
     }
 }
---
+++ Core/Theraot/Collections/ThreadSafe/Pool.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Theraot.Collections.ThreadSafe
+{
+    internal class Pool<T>
+        where T : class
+    {
+        private readonly int _id;
+        private readonly FixedSizeQueue<T> _entries;
+        private readonly Action<T> _recycler;
+
+        public Pool(int capacity, Action<T> recycler)
+        {
+            if (recycler == null)
+            {
+                throw new ArgumentNullException("recycler");
+            }
+            _id = PoolHelper.GetId();
+            _entries = new FixedSizeQueue<T>(capacity);
+            _recycler = recycler;
+        }
+
+        internal void Donate(T entry)
+        {
+            if (!ReferenceEquals(entry, null) && !AppDomain.CurrentDomain.IsFinalizingForUnload() && PoolHelper.Enter(_id))
+            {
+                try
+                {
+                    _recycler(entry);
+                    _entries.Add(entry);
+                }
+                finally
+                {
+                    PoolHelper.Leave(_id);
+                }
+            }
+        }
+
+        internal bool TryGet(out T result)
+        {
+            return _entries.TryTake(out result);
+        }
+    }
+}
