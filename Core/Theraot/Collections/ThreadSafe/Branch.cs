a2729e02e9cd8c5361681e0e9a8f0554a4820284
= <theraot@gmail.com>
2015-02-08 06:33:29 -0500

    Imported Branch and Mapper
    Also from the project Theraot.Collections.ThreadSafe

---
+++ Core/Theraot/Collections/ThreadSafe/Branch.cs
@@ -0,0 +1,449 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Theraot.Collections.ThreadSafe
+{
+    [Serializable]
+    internal class Branch : IEnumerable<object>
+    {
+        private const int INT_Capacity = 1 << INT_OffsetStep;
+        private const int INT_OffsetStep = 4;
+
+        private static readonly Pool<Branch> _branchPool;
+        private object[] _buffer;
+        private object[] _entries;
+        private int _offset;
+        private Branch _parent;
+        private int _subindex;
+        private int _useCount;
+
+        static Branch()
+        {
+            _branchPool = new Pool<Branch>(16, Recycle);
+        }
+
+        private Branch(int offset, Branch parent, int subindex)
+        {
+            _offset = offset;
+            _parent = parent;
+            _subindex = subindex;
+            _entries = ArrayReservoir<object>.GetArray(INT_Capacity);
+            _buffer = ArrayReservoir<object>.GetArray(INT_Capacity);
+        }
+
+        ~Branch()
+        {
+            if (!AppDomain.CurrentDomain.IsFinalizingForUnload())
+            {
+                ArrayReservoir<object>.DonateArray(_entries);
+                ArrayReservoir<object>.DonateArray(_buffer);
+            }
+        }
+
+        public static Branch Create(int offset, Branch parent, int subindex)
+        {
+            Branch result;
+            if (_branchPool.TryGet(out result))
+            {
+                result._offset = offset;
+                result._parent = parent;
+                result._subindex = subindex;
+                result._entries = ArrayReservoir<object>.GetArray(INT_Capacity);
+                result._buffer = ArrayReservoir<object>.GetArray(INT_Capacity);
+                return result;
+            }
+            return new Branch(offset, parent, subindex);
+        }
+
+        public bool Exchange(uint index, object item, out object previous)
+        {
+            // Get the target branches
+            int resultCount;
+            var branches = Map(index, out resultCount);
+            // ---
+            var branch = branches[resultCount - 1];
+            var result = branch.PrivateExchange(index, item, out previous);
+            Leave(branches, resultCount);
+            return result;
+        }
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            foreach (var child in _entries)
+            {
+                if (!ReferenceEquals(child, null))
+                {
+                    var items = child as Branch;
+                    if (items != null)
+                    {
+                        foreach (var item in items)
+                        {
+                            yield return item;
+                        }
+                    }
+                    else
+                    {
+                        yield return child;
+                    }
+                }
+            }
+        }
+
+        public bool Insert(uint index, object item, out object previous)
+        {
+            // Get the target branches
+            int resultCount;
+            var branches = Map(index, out resultCount);
+            // ---
+            var branch = branches[resultCount - 1];
+            var result = branch.PrivateInsert(index, item, out previous);
+            Leave(branches, resultCount);
+            return result;
+            // if this returns true, the new item was inserted, so there was no previous item
+            // if this returns false, something was inserted first... so we get the previous item
+        }
+
+        public bool RemoveAt(uint index, out object previous)
+        {
+            previous = null;
+            // Get the target branch  - can be null
+            var branch = MapReadonly(index);
+            // Check if we got a branch
+            if (branch == null)
+            {
+                // We didn't get a branch, meaning that what we look for is not there
+                return false;
+            }
+            // ---
+            if (branch.PrivateRemoveAt(index, out previous))
+            {
+                branch.Shrink();
+                return true;
+            }
+            return false;
+        }
+
+        public void Set(uint index, object value, out bool isNew)
+        {
+            // Get the target branches
+            int resultCount;
+            var branches = Map(index, out resultCount);
+            // ---
+            var branch = branches[resultCount - 1];
+            branch.PrivateSet(index, value, out isNew);
+            Leave(branches, resultCount);
+            // if this returns true, the new item was inserted, so isNew is set to true
+            // if this returns false, some other thread inserted first... so isNew is set to false
+            // yet we pretend we inserted first and the value was replaced by the other thread
+            // So we say the operation was a success regardless
+        }
+
+        public bool TryGet(uint index, out object value)
+        {
+            value = null;
+            // Get the target branch  - can be null
+            var branch = MapReadonly(index);
+            // Check if we got a branch
+            if (branch == null)
+            {
+                // We didn't get a branch, meaning that what we look for is not there
+                return false;
+            }
+            // ---
+            return branch.PrivateTryGet(index, out value);
+        }
+
+        public IEnumerable<object> Where(Predicate<object> predicate)
+        {
+            for (var index = 0; index < _entries.Length; index++)
+            {
+                var child = _entries[index];
+                if (!ReferenceEquals(child, null))
+                {
+                    var items = child as Branch;
+                    if (items != null)
+                    {
+                        foreach (var item in items.Where(predicate))
+                        {
+                            yield return item;
+                        }
+                    }
+                    else
+                    {
+                        if (predicate(child))
+                        {
+                            yield return child;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Leave(Branch[] branches, int resultCount)
+        {
+            for (int index = 0; index < resultCount; index++)
+            {
+                var branch = branches[index];
+                Interlocked.Decrement(ref branch._useCount);
+            }
+            ArrayReservoir<Branch>.DonateArray(branches);
+        }
+        private static void Recycle(Branch branch)
+        {
+            branch._entries = null;
+            branch._buffer = null;
+            branch._useCount = 0;
+            branch._parent = null;
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private int GetSubindex(uint index)
+        {
+            return (int)((index >> _offset) & 0xF);
+        }
+
+        private Branch Grow(uint index)
+        {
+            // Grow is never called when _offset == 0
+            Interlocked.Increment(ref _useCount); // We are most likely to add - overstatimate count
+            var offset = _offset - INT_OffsetStep;
+            var subindex = GetSubindex(index);
+            var node = _entries[subindex];
+            // node can only be Branch or null
+            if (node != null)
+            {
+                // Already grown
+                return node as Branch;
+            }
+            var branch = Create(offset, this, subindex);
+            var result = Interlocked.CompareExchange(ref _buffer[subindex], branch, null);
+            if (result == null)
+            {
+                result = branch;
+            }
+            else
+            {
+                _branchPool.Donate(branch);
+            }
+            var found = Interlocked.CompareExchange(ref _entries[subindex], result, null);
+            if (found == null)
+            {
+                Interlocked.Exchange(ref _buffer[subindex], null);
+                return result as Branch;
+            }
+            Interlocked.Decrement(ref _useCount); // We did not add after all
+            return this;
+        }
+
+        private Branch[] Map(uint index, out int resultCount)
+        {
+            var result = ArrayReservoir<Branch>.GetArray(16);
+            var branch = this;
+            resultCount = 0;
+            while (true)
+            {
+                Interlocked.Increment(ref branch._useCount);
+                result[resultCount] = branch;
+                resultCount++;
+                // do we need a leaf?
+                if (branch._offset == 0)
+                {
+                    // It is not responsability of this method to handle leafs
+                    return result;
+                }
+                object found;
+                if (branch.PrivateTryGetBranch(index, out found))
+                {
+                    // if found were null, PrivateTryGetBranch would have returned false
+                    // if found cannot be a leaf, because Leaf only appear on _offset == 0
+                    // only Branch and Leaf are inserted, ergo... found is Branch
+                    branch = (Branch)found;
+                    continue;
+                }
+                branch = branch.Grow(index);
+            }
+        }
+
+        private Branch MapReadonly(uint index)
+        {
+            var branch = this;
+            while (true)
+            {
+                // do we need a leaf?
+                if (branch._offset == 0)
+                {
+                    // It is not responsability of this method to handle leafs
+                    return branch;
+                }
+                object found;
+                if (branch.PrivateTryGetBranch(index, out found))
+                {
+                    // if found were null, PrivateTryGetBranch would have returned false
+                    // if found cannot be a leaf, because Leaf only appear on _offset == 0
+                    // only Branch and Leaf are inserted, ergo... found is Branch
+                    branch = (Branch)found;
+                    continue;
+                }
+                return null;
+            }
+        }
+
+        private bool PrivateExchange(uint index, object item, out object previous)
+        {
+            Interlocked.Increment(ref _useCount); // We are most likely to add - overstatimate count
+            var subindex = GetSubindex(index);
+            previous = Interlocked.Exchange(ref _entries[subindex], item ?? BucketHelper.Null);
+            if (previous == null)
+            {
+                return true;
+            }
+            if (previous == BucketHelper.Null)
+            {
+                previous = null;
+            }
+            Interlocked.Decrement(ref _useCount); // We did not add after all
+            return false;
+        }
+
+        private bool PrivateInsert(uint index, object item, out object previous)
+        {
+            Interlocked.Increment(ref _useCount); // We are most likely to add - overstatimate count
+            var subindex = GetSubindex(index);
+            previous = Interlocked.CompareExchange(ref _entries[subindex], item ?? BucketHelper.Null, null);
+            if (previous == null)
+            {
+                return true;
+            }
+            if (previous == BucketHelper.Null)
+            {
+                previous = null;
+            }
+            Interlocked.Decrement(ref _useCount); // We did not add after all
+            return false;
+        }
+
+        private bool PrivateRemoveAt(uint index, out object previous)
+        {
+            try
+            {
+                var subindex = GetSubindex(index);
+                previous = Interlocked.Exchange(ref _entries[subindex], null);
+                if (previous == null)
+                {
+                    return false;
+                }
+                if (previous == BucketHelper.Null)
+                {
+                    previous = null;
+                }
+                Interlocked.Decrement(ref _useCount);
+                return true;
+            }
+            catch (NullReferenceException)
+            {
+                // Eating null reference, the branch has been removed
+                previous = null;
+            }
+            return false;
+        }
+
+        private void PrivateSet(uint index, object item, out bool isNew)
+        {
+            Interlocked.Increment(ref _useCount); // We are most likely to add - overstatimate count
+            var subindex = GetSubindex(index);
+            isNew = false;
+            var previous = Interlocked.Exchange(ref _entries[subindex], item ?? BucketHelper.Null);
+            if (previous == null)
+            {
+                isNew = true;
+            }
+            else
+            {
+                Interlocked.Decrement(ref _useCount); // We did not add after all
+            }
+        }
+
+        private bool PrivateTryGet(uint index, out object previous)
+        {
+            try
+            {
+                var subindex = GetSubindex(index);
+                previous = Interlocked.CompareExchange(ref _entries[subindex], null, null);
+                if (previous == null)
+                {
+                    return false;
+                }
+                if (previous == BucketHelper.Null)
+                {
+                    previous = null;
+                }
+                return true;
+            }
+            catch (NullReferenceException)
+            {
+                // Eating null reference, the branch has been removed
+                previous = null;
+            }
+            return false;
+        }
+
+        private bool PrivateTryGetBranch(uint index, out object previous)
+        {
+            try
+            {
+                var subindex = GetSubindex(index);
+                previous = Interlocked.CompareExchange(ref _entries[subindex], null, null);
+                if (previous == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (NullReferenceException)
+            {
+                // Eating null reference, the branch has been removed
+                previous = null;
+            }
+            return false;
+        }
+
+        private void Shrink()
+        {
+            if
+                (
+                _parent != null
+                && Interlocked.CompareExchange(ref _parent._buffer[_subindex], this, null) == null
+                && Interlocked.CompareExchange(ref _useCount, 0, 0) == 0
+                && Interlocked.CompareExchange(ref _parent._entries[_subindex], null, this) == this // Did --
+                )
+            {
+                if (Interlocked.CompareExchange(ref _useCount, 0, 0) == 0)
+                {
+                    var found = Interlocked.CompareExchange(ref _parent._buffer[_subindex], null, this);
+                    if (found == this)
+                    {
+                        var parent = _parent;
+                        _branchPool.Donate(this);
+                        Interlocked.Decrement(ref parent._useCount); // did not undo --
+                        parent.Shrink();
+                    }
+                }
+                else
+                {
+                    var found = Interlocked.CompareExchange(ref _parent._entries[_subindex], _parent._buffer[_subindex], null);
+                    if (found != null)
+                    {
+                        var parent = _parent;
+                        _branchPool.Donate(this);
+                        Interlocked.Decrement(ref parent._useCount); // did not undo --
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Core/Theraot/Collections/ThreadSafe/Mapper.cs
@@ -0,0 +1,236 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Theraot.Collections.ThreadSafe
+{
+    [Serializable]
+    public class Mapper<T> : IEnumerable<T>
+    {
+        private const int INT_MaxOffset = 32;
+        private const int INT_OffsetStep = 4;
+        private readonly Branch _root;
+        private int _count;
+
+        public Mapper()
+        {
+            _count = 0;
+            _root = Branch.Create(INT_MaxOffset - INT_OffsetStep, null, 0);
+        }
+
+        /// <summary>
+        /// Gets the number of items actually contained.
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        /// <summary>
+        /// Copies the items to a compatible one-dimensional array, starting at the specified index of the target array.
+        /// </summary>
+        /// <param name="array">The array.</param>
+        /// <param name="arrayIndex">Index of the array.</param>
+        /// <exception cref="System.ArgumentNullException">array</exception>
+        /// <exception cref="System.ArgumentOutOfRangeException">arrayIndex;Non-negative number is required.</exception>
+        /// <exception cref="System.ArgumentException">array;The array can not contain the number of elements.</exception>
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+            if (arrayIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException("arrayIndex", "Non-negative number is required.");
+            }
+            if (_count > array.Length - arrayIndex)
+            {
+                throw new ArgumentException("The array can not contain the number of elements.", "array");
+            }
+            try
+            {
+                foreach (var entry in _root)
+                {
+                    array[arrayIndex] = (T)entry;
+                    arrayIndex++;
+                }
+            }
+            catch (IndexOutOfRangeException exception)
+            {
+                throw new ArgumentOutOfRangeException("array", exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Sets the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="previous">The previous item in the specified index.</param>
+        /// <returns>
+        ///   <c>true</c> if the item was new; otherwise, <c>false</c>.
+        /// </returns>
+        public bool Exchange(int index, T item, out T previous)
+        {
+            previous = default(T);
+            object previousObject;
+            if (_root.Exchange(unchecked((uint)index), item, out previousObject))
+            {
+                Interlocked.Increment(ref _count);
+                return true;
+            }
+            previous = (T)previousObject;
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _root)
+            {
+                yield return (T)item;
+            }
+        }
+
+        /// <summary>
+        /// Inserts the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="item">The item.</param>
+        /// <returns>
+        ///   <c>true</c> if the item was inserted; otherwise, <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// The insertion can fail if the index is already used or is being written by another thread.
+        /// If the index is being written it can be understood that the insert operation happened before but the item was overwritten or removed.
+        /// </remarks>
+        public bool Insert(int index, T item)
+        {
+            object previous;
+            if (_root.Insert(unchecked((uint)index), item, out previous))
+            {
+                Interlocked.Increment(ref _count);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Inserts the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="item">The item.</param>
+        /// <param name="previous">The previous item in the specified index.</param>
+        /// <returns>
+        ///   <c>true</c> if the item was inserted; otherwise, <c>false</c>.
+        /// </returns>
+        /// <remarks>
+        /// The insertion can fail if the index is already used or is being written by another thread.
+        /// If the index is being written it can be understood that the insert operation happened before but the item was overwritten or removed.
+        /// </remarks>
+        public bool Insert(int index, T item, out T previous)
+        {
+            previous = default(T);
+            object previousObject;
+            if (_root.Insert(unchecked((uint)index), item, out previousObject))
+            {
+                Interlocked.Increment(ref _count);
+                return true;
+            }
+            previous = (T)previousObject;
+            return false;
+        }
+
+        /// <summary>
+        /// Removes the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>
+        ///   <c>true</c> if the item was removed; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">index;index must be greater or equal to 0 and less than capacity</exception>
+        public bool RemoveAt(int index)
+        {
+            object _previous;
+            if (_root.RemoveAt(unchecked((uint)index), out _previous))
+            {
+                Interlocked.Decrement(ref _count);
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Removes the item at the specified index.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <param name="previous">The previous item in the specified index.</param>
+        /// <returns>
+        ///   <c>true</c> if the item was removed; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">index;index must be greater or equal to 0 and less than capacity</exception>
+        public bool RemoveAt(int index, out T previous)
+        {
+            object _previous;
+            if (_root.RemoveAt(unchecked((uint)index), out _previous))
+            {
+                Interlocked.Decrement(ref _count);
+                previous = (T)_previous;
+                return true;
+            }
+            previous = default(T);
+            return false;
+        }
+
+        public void Set(int index, T value, out bool isNew)
+        {
+            _root.Set(unchecked((uint)index), value, out isNew);
+            if (isNew)
+            {
+                Interlocked.Increment(ref _count);
+            }
+        }
+
+        public bool TryGet(int index, out T value)
+        {
+            value = default(T);
+            object valueObject;
+            if (_root.TryGet(unchecked((uint)index), out valueObject))
+            {
+                value = (T)valueObject;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the values where the predicate is satisfied.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <returns>
+        /// An <see cref="IEnumerable{T}" /> that allows to iterate over the removed values.
+        /// </returns>
+        /// <remarks>
+        /// It is not guaranteed that all the values that satisfies the predicate will be removed.
+        /// </remarks>
+        public IEnumerable<T> Where(Predicate<T> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException("predicate");
+            }
+            foreach (var value in _root.Where(value => predicate((T)value)))
+            {
+                yield return (T)value;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
