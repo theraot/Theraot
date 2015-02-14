645ace34d7c4fef0fb7514f7b078fb6242fa8c0a
= <theraot@gmail.com>
2015-02-14 08:35:48 -0500

    By using needles each entry is distinct
    This allows multiple registration. This works thanks to recent changes
    to GetHashCode on Needle (that was a necesary step to get Recyclable
    needles).

--- Core/Theraot/Collections/ProxyObservable.cs
+++ Core/Theraot/Collections/ProxyObservable.cs
@@ -1,25 +1,25 @@
 using System;
 using Theraot.Collections.ThreadSafe;
 using Theraot.Threading;
+using Theraot.Threading.Needles;
 
 namespace Theraot.Collections
 {
     [Serializable]
     public sealed class ProxyObservable<T> : IObservable<T>, IObserver<T>, IProxyObservable<T>
     {
-        // Here we use SafeSet because it will allow iterating while it is being modified
-        private readonly SafeSet<IObserver<T>> _observers;
+        private readonly SafeSet<Needle<IObserver<T>>> _observers;
 
         public ProxyObservable()
         {
-            _observers = new SafeSet<IObserver<T>>();
+            _observers = new SafeSet<Needle<IObserver<T>>>();
         }
 
         public void OnCompleted()
         {
             foreach (var item in _observers)
             {
-                item.OnCompleted();
+                item.Value.OnCompleted();
             }
         }
 
@@ -27,7 +27,7 @@ namespace Theraot.Collections
         {
             foreach (var item in _observers)
             {
-                item.OnError(error);
+                item.Value.OnError(error);
             }
         }
 
@@ -35,15 +35,15 @@ namespace Theraot.Collections
         {
             foreach (var item in _observers)
             {
-                item.OnNext(value);
+                item.Value.OnNext(value);
             }
         }
 
         public IDisposable Subscribe(IObserver<T> observer)
         {
-            // TODO: can you subscribe twice? By using a Set this is not possible
-            _observers.AddNew(observer);
-            return Disposable.Create(() => _observers.Remove(observer));
+            var needle = new Needle<IObserver<T>>(observer);
+            _observers.AddNew(needle);
+            return Disposable.Create(() => _observers.Remove(needle));
         }
     }
 }
