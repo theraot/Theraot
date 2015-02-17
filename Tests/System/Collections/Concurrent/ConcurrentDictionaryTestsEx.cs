a8a5b8322befcdc191aa789010aba30b8cc54619
= <theraot@gmail.com>
2015-02-17 04:56:12 -0500

    New test for ConcurrentDictionary

---
+++ Tests/System/Collections/Concurrent/ConcurrentDictionaryTestsEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+
+namespace MonoTests.System.Collections.Concurrent
+{
+    [TestFixture]
+    public class ConcurrentDictionaryTestsEx
+    {
+        [Test]
+        public void PairCollide()
+        {
+            var firstPair = new KeyValuePair<string, string>("key", "validValue");
+            var secondPair = new KeyValuePair<string, string>("key", "wrongValue");
+
+            IDictionary<string, string> dict = new ConcurrentDictionary<string, string>();
+            dict.Add(firstPair);
+            Assert.Throws<ArgumentException>(() => dict.Add(secondPair));
+
+            Assert.IsTrue(dict.Contains(firstPair));
+            Assert.IsFalse(dict.Contains(secondPair));
+        }
+    }
+}
