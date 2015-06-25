39b055846afff75c1e4ed4b092e838c381244bb3
Alfonso J. Ramos <theraot@gmail.com>
2015-06-25 06:40:56 -0500

    Imported from ReaderWriterLockSlim

---
+++ Core/System/Threading/LockRecursionPolicy.net30.cs
@@ -0,0 +1,13 @@
+﻿#if NET20 || NET30
+
+namespace System.Threading
+{
+    [Serializable]
+    public enum LockRecursionPolicy
+    {
+        NoRecursion,
+        SupportsRecursion
+    }
+}
+
+#endif
