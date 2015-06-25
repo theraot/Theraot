63ba9045d26780563bac4ab4654f7122cc59638c
Alfonso J. Ramos <theraot@gmail.com>
2015-06-25 08:01:02 -0500

    Added Contract.EndContractBlock
    Which turns out to be a noop.

--- Core/System/Diagnostics/Contracts/Contract.net35.cs
+++ Core/System/Diagnostics/Contracts/Contract.net35.cs
@@ -1,9 +1,26 @@
 ﻿#if NET20 || NET30 || NET35
 
+// ==++==
+// 
+//   Copyright (c) Microsoft Corporation.  All rights reserved.
+// 
+// ==--==
+
 namespace System.Diagnostics.Contracts
 {
     public static class Contract
     {
+        /// <summary>
+        /// Marker to indicate the end of the contract section of a method.
+        /// </summary>
+        [Conditional("CONTRACTS_FULL")]
+        #if FEATURE_RELIABILITY_CONTRACTS
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        #endif
+        public static void EndContractBlock()
+        {
+            // Empty
+        }
     }
 }
 
