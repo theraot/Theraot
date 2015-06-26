843bd011ffeb56d87ed31aeff47f5167e28a3dcc
Alfonso J. Ramos <theraot@gmail.com>
2015-06-25 22:45:37 -0500

    Added Contract.Assert Tests

---
+++ Tests/System/Diagnostics/Contracts/AssertTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+
+namespace System.Diagnostics.Contracts.Tests
+{
+    [TestFixture]
+    public class AssertTests
+    {
+        [Test]
+        public static void AssertTrueDoesNotRaiseEvent()
+        {
+            bool eventRaised = false;
+            EventHandler<ContractFailedEventArgs> handler = (s, e) =>
+            {
+                eventRaised = true;
+                e.SetHandled();
+            };
+            using (Utilities.WithContractFailed(handler))
+            {
+                Contract.Assert(true);
+                NUnit.Framework.Assert.False(eventRaised, "ContractFailed event was raised");
+            }
+        }
+
+        [Test]
+        public static void AssertFalseRaisesEvent()
+        {
+            bool eventRaised = false;
+            EventHandler<ContractFailedEventArgs> handler = (s, e) =>
+            {
+                eventRaised = true;
+                e.SetHandled();
+            };
+            using (Utilities.WithContractFailed(handler))
+            {
+                Contract.Assert(false);
+                NUnit.Framework.Assert.True(eventRaised, "ContractFailed event not raised");
+            }
+        }
+
+        [Test]
+        public static void AssertFalseThrows()
+        {
+            bool eventRaised = false;
+            EventHandler<ContractFailedEventArgs> handler = (s, e) =>
+            {
+                eventRaised = true;
+                e.SetUnwind();
+            };
+            using (Utilities.WithContractFailed(handler))
+            {
+#if DEBUG
+                Utilities.AssertThrowsContractException(() => Contract.Assert(false, "Some kind of user message"));
+                NUnit.Framework.Assert.True(eventRaised, "ContractFailed was not raised");
+#else
+                Contract.Assert(false, "Some kind of user message");
+                NUnit.Framework.Assert.False(eventRaised, "ContractFailed event was raised");
+#endif
+            }
+        }
+    }
+}
---
+++ Tests/System/Diagnostics/Contracts/Utilities.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using NUnit.Framework;
+using System;
+
+namespace System.Diagnostics.Contracts.Tests
+{
+    internal static class Utilities
+    {
+        internal static void AssertThrowsContractException(Action action)
+        {
+            try
+            {
+                action();
+                NUnit.Framework.Assert.Fail("Did not throw");
+            }
+            catch (Exception exc)
+            {
+                NUnit.Framework.Assert.AreEqual("ContractException", exc.GetType().Name);
+            }
+        }
+
+        internal static IDisposable WithContractFailed(EventHandler<ContractFailedEventArgs> handler)
+        {
+            Contract.ContractFailed += handler;
+            return new UnregisterContractFailed { _handler = handler };
+        }
+
+        private class UnregisterContractFailed : IDisposable
+        {
+            internal EventHandler<ContractFailedEventArgs> _handler;
+
+            public void Dispose()
+            {
+                Contract.ContractFailed -= _handler;
+            }
+        }
+    }
+}
