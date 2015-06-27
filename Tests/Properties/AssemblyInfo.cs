55bb0b463fcf3864ee83ec0ba52a3312afb6a4fe
Alfonso J. Ramos <theraot@gmail.com>
2015-06-27 17:51:39 -0500

    Updated AssemblyInfo
    How did I forget this?

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014 - 2045 by Theraot")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
