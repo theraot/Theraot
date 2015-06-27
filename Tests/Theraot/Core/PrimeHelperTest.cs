89810d166eb6caf64450db0f47ea168b64ca5a53
Alfonso J. Ramos <theraot@gmail.com>
2015-06-27 18:44:52 -0500

    Testing IsPrime

---
+++ Tests/Theraot/Core/PrimeHelperTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using Theraot.Core;
+
+namespace Tests.Theraot.Core
+{
+    [TestFixture]
+    internal partial class PrimeHelperTest
+    {
+        [Test]
+        public void TestIsPrime()
+        {
+            var input = new int[]
+            {
+                18, 13, 68, 99, 93, 34, 11, 47, 64, 10,
+                809, 3420, 9391, 8999, 5154, 8934, 5416, 5669, 2755, 3159,
+                285080, 727881, 846232, 665370, 153194, 157833, 585896, 914054, 505284, 333258,
+                49086597, 78119724, 76928802, 23260170, 1955743, 39360664, 10885879, 30169506, 65889970, 95425647,
+                179424551, 179424571, 179424577, 179424601, 179424611, 179424617, 179424629, 179424667, 179424671, 179424673,
+                1683899352, 883641873, 114883641, 1000000007, 1000000009, 1000000021, 1000000033, 1000000087, 1000000093, 2147483629
+            };
+            var output = new int[]
+            {
+                0, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+                1, 0, 1, 1, 0, 0, 0, 1, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                0, 0, 0, 1, 1, 1, 1, 1, 1, 1
+            };
+            Assert.AreEqual(input.Length, output.Length, "setup");
+            for (int index = 0; index < output.Length; index++)
+            {
+                Assert.AreEqual(output[index] == 1, PrimeHelper.IsPrime(input[index]));
+            }
+        }
+    }
+}
