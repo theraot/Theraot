bfe541ed0351e5d41f4db0e989306a857926bb29
Alfonso J. Ramos <theraot@gmail.com>
2015-06-27 00:18:35 -0500

    Extended NumericHelperTest

--- Tests/Theraot/Core/NumericHelperTest.cs
+++ Tests/Theraot/Core/NumericHelperTest.cs
@@ -6,7 +6,7 @@ using Theraot.Core;
 namespace Tests.Theraot.Core
 {
     [TestFixture]
-    internal class NumericHelperTest
+    internal partial class NumericHelperTest
     {
 #if FAT
 
@@ -53,7 +53,7 @@ namespace Tests.Theraot.Core
         [Test]
         public void NextPowerOf2Int()
         {
-            var input = new int[] { -1,  835, 246444, 448905, 610268, 855701, 1017047, 1228188, 1717470, 1917025, 2130772 };
+            var input = new int[] { -1, 835, 246444, 448905, 610268, 855701, 1017047, 1228188, 1717470, 1917025, 2130772 };
             var output = new int[] { 1, 1024, 262144, 524288, 1048576, 1048576, 1048576, 2097152, 2097152, 2097152, 4194304 };
             for (int index = 0; index < input.Length; index++)
             {
@@ -82,11 +82,206 @@ namespace Tests.Theraot.Core
                 Assert.AreEqual(output[index], NumericHelper.Sqrt(input[index]));
             }
         }
+    }
+
+    internal partial class NumericHelperTest
+    {
+        [Test]
+        public void BinaryByte()
+        {
+            var input = new byte[] { 157, 155, 92, 129, 166, 100, 17, 168, 158, 128 };
+            var output = new string[]
+            {
+                "10011101",
+                "10011011",
+                "01011100",
+                "10000001",
+                "10100110",
+                "01100100",
+                "00010001",
+                "10101000",
+                "10011110",
+                "10000000"
+            };
+            for (int index = 0; index < input.Length; index++)
+            {
+                Assert.AreEqual(output[index], NumericHelper.ToStringBinary(input[index]));
+                var characters = output[index].ToCharArray();
+                var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
+                var bits = NumericHelper.Bits(input[index]).ToArray();
+                var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
+                var bitIndex = 0;
+                for (int subindex = 0; subindex < 8; subindex++)
+                {
+                    Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[7 - subindex].ToString(), rbitsBinary[subindex].ToString());
+                    if (bitIndex < bits.Length)
+                    {
+                        var log2 = NumericHelper.Log2(bits[bitIndex]);
+                        Assert.AreEqual(log2, bitsLog2[bitIndex]);
+                        if (log2 == 7 - subindex)
+                        {
+                            if (characters[subindex] == '0')
+                            {
+                                Assert.Fail();
+                            }
+                            bitIndex++;
+                        }
+                    }
+                }
+            }
+        }
 
         [Test]
-        public void Binary()
+        public void BinaryInt()
         {
-            var input = new int[] { 22141034, 146009798, 106447544, 66083576, 28048294, 3848650, 119601527, 182384611, 160860217, 52726162 };
+            var input = new int[] { 22141034, -46009798, 106447544, -6083576, 28048294, -848650, 119601527, -82384611, 160860217, -2726162 };
+            var output = new string[]
+            {
+                "00000001010100011101100001101010",
+                "11111101010000011111001000111010",
+                "00000110010110000100001010111000",
+                "11111111101000110010110000001000",
+                "00000001101010111111101110100110",
+                "11111111111100110000110011110110",
+                "00000111001000001111100101110111",
+                "11111011000101101110100100011101",
+                "00001001100101101000100000111001",
+                "11111111110101100110011011101110"
+            };
+            for (int index = 0; index < input.Length; index++)
+            {
+                Assert.AreEqual(output[index], NumericHelper.ToStringBinary(input[index]));
+                var characters = output[index].ToCharArray();
+                var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
+                var bits = NumericHelper.Bits(input[index]).ToArray();
+                var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
+                var bitIndex = 0;
+                for (int subindex = 0; subindex < 32; subindex++)
+                {
+                    Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[31 - subindex].ToString(), rbitsBinary[subindex].ToString());
+                    if (bitIndex < bits.Length)
+                    {
+                        var log2 = NumericHelper.Log2(unchecked((uint)bits[bitIndex]));
+                        Assert.AreEqual(log2, bitsLog2[bitIndex]);
+                        if (log2 == 31 - subindex)
+                        {
+                            if (characters[subindex] == '0')
+                            {
+                                Assert.Fail();
+                            }
+                            bitIndex++;
+                        }
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void BinarySByte()
+        {
+            var input = new sbyte[] { -57, -55, 92, -29, -66, 0, 17, -68, -58, -28 };
+            var output = new string[]
+            {
+                "11000111",
+                "11001001",
+                "01011100",
+                "11100011",
+                "10111110",
+                "00000000",
+                "00010001",
+                "10111100",
+                "11000110",
+                "11100100"
+            };
+            for (int index = 0; index < input.Length; index++)
+            {
+                Assert.AreEqual(output[index], NumericHelper.ToStringBinary(input[index]));
+                var characters = output[index].ToCharArray();
+                var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
+                var bits = NumericHelper.Bits(input[index]).ToArray();
+                var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
+                var bitIndex = 0;
+                for (int subindex = 0; subindex < 8; subindex++)
+                {
+                    Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[7 - subindex].ToString(), rbitsBinary[subindex].ToString());
+                    if (bitIndex < bits.Length)
+                    {
+                        var log2 = NumericHelper.Log2(unchecked((byte)bits[bitIndex]));
+                        Assert.AreEqual(log2, bitsLog2[bitIndex]);
+                        if (log2 == 7 - subindex)
+                        {
+                            if (characters[subindex] == '0')
+                            {
+                                Assert.Fail();
+                            }
+                            bitIndex++;
+                        }
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void BinaryShort()
+        {
+            var input = new short[] { 22141, -4600, 10644, -6083, 28048, -848, 11960, -8238, 16086, -2726 };
+            var output = new string[]
+            {
+                "0101011001111101",
+                "1110111000001000",
+                "0010100110010100",
+                "1110100000111101",
+                "0110110110010000",
+                "1111110010110000",
+                "0010111010111000",
+                "1101111111010010",
+                "0011111011010110",
+                "1111010101011010"
+            };
+            for (int index = 0; index < input.Length; index++)
+            {
+                Assert.AreEqual(output[index], NumericHelper.ToStringBinary(input[index]));
+                var characters = output[index].ToCharArray();
+                var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
+                var bits = NumericHelper.Bits(input[index]).ToArray();
+                var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
+                var bitIndex = 0;
+                for (int subindex = 0; subindex < 16; subindex++)
+                {
+                    Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[15 - subindex].ToString(), rbitsBinary[subindex].ToString());
+                    if (bitIndex < bits.Length)
+                    {
+                        var log2 = NumericHelper.Log2(unchecked((ushort)bits[bitIndex]));
+                        Assert.AreEqual(log2, bitsLog2[bitIndex]);
+                        if (log2 == 15 - subindex)
+                        {
+                            if (characters[subindex] == '0')
+                            {
+                                Assert.Fail();
+                            }
+                            bitIndex++;
+                        }
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void BinaryUint()
+        {
+            var input = new uint[] { 22141034, 146009798, 106447544, 66083576, 28048294, 3848650, 119601527, 182384611, 160860217, 52726162 };
             var output = new string[]
             {
                 "00000001010100011101100001101010",
@@ -107,13 +302,13 @@ namespace Tests.Theraot.Core
                 var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
                 var bits = NumericHelper.Bits(input[index]).ToArray();
                 var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
-                /*var reverse = NumericHelper.BinaryReverse(input[index]);
-                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();*/
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
                 var bitIndex = 0;
                 for (int subindex = 0; subindex < 32; subindex++)
                 {
                     Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
-                    //Assert.AreEqual(characters[31 - subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[31 - subindex].ToString(), rbitsBinary[subindex].ToString());
                     if (bitIndex < bits.Length)
                     {
                         var log2 = NumericHelper.Log2(bits[bitIndex]);
@@ -130,5 +325,54 @@ namespace Tests.Theraot.Core
                 }
             }
         }
+
+        [Test]
+        public void BinaryUShort()
+        {
+            var input = new ushort[] { 22141, 14600, 10644, 16083, 28048, 1848, 11960, 18238, 16086, 12726 };
+            var output = new string[]
+            {
+                "0101011001111101",
+                "0011100100001000",
+                "0010100110010100",
+                "0011111011010011",
+                "0110110110010000",
+                "0000011100111000",
+                "0010111010111000",
+                "0100011100111110",
+                "0011111011010110",
+                "0011000110110110"
+            };
+            for (int index = 0; index < input.Length; index++)
+            {
+                Assert.AreEqual(output[index], NumericHelper.ToStringBinary(input[index]));
+                var characters = output[index].ToCharArray();
+                var bitsBinary = NumericHelper.BitsBinary(input[index]).ToArray();
+                var bits = NumericHelper.Bits(input[index]).ToArray();
+                var bitsLog2 = NumericHelper.BitsLog2(input[index]).ToArray();
+                var reverse = NumericHelper.BinaryReverse(input[index]);
+                var rbitsBinary = NumericHelper.BitsBinary(reverse).ToArray();
+                var bitIndex = 0;
+                for (int subindex = 0; subindex < 16; subindex++)
+                {
+                    Assert.AreEqual(characters[subindex].ToString(), bitsBinary[subindex].ToString());
+                    Assert.AreEqual(characters[15 - subindex].ToString(), rbitsBinary[subindex].ToString());
+                    if (bitIndex < bits.Length)
+                    {
+                        var log2 = NumericHelper.Log2(unchecked((ushort)bits[bitIndex]));
+                        Assert.AreEqual(log2, bitsLog2[bitIndex]);
+                        if (log2 == 15 - subindex)
+                        {
+                            if (characters[subindex] == '0')
+                            {
+                                Assert.Fail();
+                            }
+                            bitIndex++;
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
