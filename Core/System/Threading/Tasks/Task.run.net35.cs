a5a83229a42280f7f2e3919c02a27473c284b236
Alfonso J. Ramos <theraot@gmail.com>
2015-07-06 06:37:57 -0500

    More run overloads

--- Core/System/Threading/Tasks/Task.run.net35.cs
+++ Core/System/Threading/Tasks/Task.run.net35.cs
@@ -21,6 +21,25 @@ namespace System.Threading.Tasks
             return result;
         }
 
+        public static Task Run(Func<Task> action)
+        {
+            // TODO: change to continue with and promise task?
+            var result = new Task(() => action().Wait(), CancellationToken.None, TaskCreationOptions.DenyChildAttach);
+            result.Start();
+            return result;
+        }
+
+        public static Task Run(Func<Task> action, CancellationToken cancellationToken)
+        {
+            // TODO: change to continue with and promise task?
+            var result = new Task(() => action().Wait(), cancellationToken, TaskCreationOptions.DenyChildAttach);
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                result.InternalStart(result.Scheduler);
+            }
+            return result;
+        }
+
         public static Task<TResult> Run<TResult>(Func<TResult> function)
         {
             var result = new Task<TResult>(function, CancellationToken.None, TaskCreationOptions.DenyChildAttach);
@@ -31,6 +50,25 @@ namespace System.Threading.Tasks
         public static Task<TResult> Run<TResult>(Func<TResult> function, CancellationToken cancellationToken)
         {
             var result = new Task<TResult>(function, cancellationToken, TaskCreationOptions.DenyChildAttach);
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                result.InternalStart(result.Scheduler);
+            }
+            return result;
+        }
+
+        public static Task<TResult> Run<TResult>(Func<Task<TResult>> function)
+        {
+            // TODO: change to continue with and promise task?
+            var result = new Task<TResult>(() => function().Result, CancellationToken.None, TaskCreationOptions.DenyChildAttach);
+            result.Start();
+            return result;
+        }
+
+        public static Task<TResult> Run<TResult>(Func<Task<TResult>> function, CancellationToken cancellationToken)
+        {
+            // TODO: change to continue with and promise task?
+            var result = new Task<TResult>(() => function().Result, cancellationToken, TaskCreationOptions.DenyChildAttach);
             if (!cancellationToken.IsCancellationRequested)
             {
                 result.InternalStart(result.Scheduler);
