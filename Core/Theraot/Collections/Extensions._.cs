4ea1d76bb957960f784ad7da4d84768bdceedb48
Alfonso J. Ramos <theraot@gmail.com>
2015-07-07 00:56:29 -0500

    Added Map extension method

--- Core/Theraot/Collections/Extensions._.cs
+++ Core/Theraot/Collections/Extensions._.cs
@@ -1456,6 +1456,22 @@ namespace Theraot.Collections
             return true;
         }
 
+        // Name needs to be different so it doesn't conflict with Enumerable.Select
+        public static U[] Map<T, U>(this ICollection<T> collection, Func<T, U> select)
+        {
+            // Copyright (c) Microsoft. All rights reserved.
+            // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+            int count = collection.Count;
+            U[] result = new U[count];
+            count = 0;
+            foreach (T t in collection)
+            {
+                result[count++] = select(t);
+            }
+            return result;
+        }
+
         public static void Move<T>(this IList<T> list, int oldIndex, int newIndex)
         {
             var item = list[oldIndex];
