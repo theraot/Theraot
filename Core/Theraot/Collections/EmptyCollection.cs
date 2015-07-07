fc0f9d376304099ea4c3474d3960e566294e50d3
Alfonso J. Ramos <theraot@gmail.com>
2015-07-06 20:20:02 -0500

    Added EmptyCollection

---
+++ Core/Theraot/Collections/EmptyCollection.cs
@@ -0,0 +1,29 @@
+// Needed for NET30
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Theraot.Collections
+{
+    [global::System.Diagnostics.DebuggerNonUserCode]
+    [global::System.ComponentModel.ImmutableObject(true)]
+    public sealed class EmptyCollection<T> : ReadOnlyCollection<T>, IReadOnlyCollection<T>
+    {
+        private static T[] _internal = new T[0];
+        private static readonly EmptyCollection<T> _instance = new EmptyCollection<T>();
+
+        private EmptyCollection()
+            : base(_internal)
+        {
+            
+        }
+
+        public static EmptyCollection<T> Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+    }
+}
