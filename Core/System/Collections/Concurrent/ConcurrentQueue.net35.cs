52bb7ef016058ac89c78080cffd2bcd37cbfb4b2
Alfonso J. Ramos <theraot@gmail.com>
2015-08-23 13:03:37 -0500

    ConcurrentQueue stub

---
+++ Core/System/Collections/Concurrent/ConcurrentQueue.net35.cs
@@ -0,0 +1,19 @@
+﻿#if NET20 || NET30 || NET35
+
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+namespace System.Collections.Concurrent
+{
+    [SerializableAttribute]
+    [ComVisible(false)]
+    [DebuggerDisplay("Count = {Count}")]
+    [HostProtection(SecurityAction.LinkDemand, Synchronization = true, ExternalThreading = true)]
+    public class ConcurrentQueue<T> : IProducerConsumerCollection<T>, IEnumerable<T>, IEnumerable, ICollection, IReadOnlyCollection<T>
+    {
+    }
+}
+
+#endif
