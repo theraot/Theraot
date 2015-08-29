1ac147c7813c9c4af7fcde1ddea2a080e95e5d8b
Alfonso J. Ramos <theraot@gmail.com>
2015-08-29 12:14:39 -0500

    Optimization for TryPeek in FixedSizeQueue

--- Core/Theraot/Collections/ThreadSafe/FixedSizeQueue.cs
+++ Core/Theraot/Collections/ThreadSafe/FixedSizeQueue.cs
@@ -146,7 +146,7 @@ namespace Theraot.Collections.ThreadSafe
         {
             item = default(T);
             int index = Interlocked.Add(ref _indexEnqueue, 0);
-            return index < _capacity && index > 0 && _entries.TryGet(index, out item);
+            return index < _capacity && index > 0 && _entries.TryGetInternal(index, out item);
         }
 
         /// <summary>
