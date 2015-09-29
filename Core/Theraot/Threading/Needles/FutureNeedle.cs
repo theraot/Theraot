c0bcb8b212d03dd01f511bf44a0eade47149986e
Alfonso J. Ramos <theraot@gmail.com>
2015-09-29 04:54:57 -0500

    FutureNeedle refactors
    - Added wait overloads
    - Added mechanism for custom scheduler

--- Core/Theraot/Threading/Needles/FutureNeedle.cs
+++ Core/Theraot/Threading/Needles/FutureNeedle.cs
@@ -9,18 +9,36 @@ namespace Theraot.Threading.Needles
     [System.Diagnostics.DebuggerNonUserCode]
     public class FutureNeedle<T> : LazyNeedle<T>
     {
+        private readonly Action<Action> _schedule;
         private int _status;
 
         public FutureNeedle(Func<T> valueFactory)
             : base(valueFactory)
         {
-            Schedule();
+            _schedule = Schedule;
         }
 
-        public FutureNeedle(Func<T> valueFactory, bool schedule)
+        public FutureNeedle(Func<T> valueFactory, bool autoSchedule)
             : base(valueFactory)
         {
-            if (schedule)
+            _schedule = Schedule;
+            if (autoSchedule)
+            {
+                Schedule();
+            }
+        }
+
+        public FutureNeedle(Action<Action> schedule, Func<T> valueFactory)
+            : base(valueFactory)
+        {
+            _schedule = schedule;
+        }
+
+        public FutureNeedle(Action<Action> schedule, Func<T> valueFactory, bool autoSchedule)
+            : base(valueFactory)
+        {
+            _schedule = schedule;
+            if (autoSchedule)
             {
                 Schedule();
             }
@@ -38,20 +56,95 @@ namespace Theraot.Threading.Needles
             }
         }
 
+        protected override void Initialize(Action beforeInitialize)
+        {
+            if (Interlocked.CompareExchange(ref _status, 1, 0) != 0)
+            {
+                base.Wait();
+            }
+            else
+            {
+                base.Initialize(beforeInitialize);
+            }
+        }
+
         public bool Schedule()
         {
             if (Interlocked.CompareExchange(ref _status, 1, 0) != 0)
             {
                 return false;
             }
-            var waitCallback = new WaitCallback(_ => Initialize());
-            ThreadPool.QueueUserWorkItem(waitCallback);
+            _schedule(base.Initialize);
             return true;
         }
 
         public override void Wait()
         {
             Initialize();
+        }
+
+        public override void Wait(CancellationToken cancellationToken)
+        {
+            if (Interlocked.CompareExchange(ref _status, 1, 0) != 0)
+            {
+                base.Wait(cancellationToken);
+            }
+            else
+            {
+                base.Initialize();
+            }
+        }
+
+        public override void Wait(int milliseconds)
+        {
+            if (Interlocked.CompareExchange(ref _status, 1, 0) != 0)
+            {
+                base.Wait(milliseconds);
+            }
+            else
+            {
+                var start = ThreadingHelper.TicksNow();
+                base.Initialize();
+                milliseconds = milliseconds - (int)(ThreadingHelper.TicksNow() - start);
+                if (milliseconds > 0)
+                {
+                    base.Wait(milliseconds);
+                }
+            }
+        }
+
+        public override void Wait(TimeSpan timeout)
+        {
+            Wait((int)timeout.TotalMilliseconds);
+        }
+
+        public override void Wait(int milliseconds, CancellationToken cancellationToken)
+        {
+            if (Interlocked.CompareExchange(ref _status, 1, 0) != 0)
+            {
+                base.Wait(milliseconds, cancellationToken);
+            }
+            else
+            {
+                var start = ThreadingHelper.TicksNow();
+                base.Initialize();
+                milliseconds = milliseconds - (int)(ThreadingHelper.TicksNow() - start);
+                if (milliseconds > 0)
+                {
+                    base.Wait(milliseconds, cancellationToken);
+                }
+            }
+        }
+
+        public override void Wait(TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            Wait((int)timeout.TotalMilliseconds, cancellationToken);
+        }
+
+        private static void Schedule(Action action)
+        {
+            var waitCallback = new WaitCallback(_ => action());
+            ThreadPool.QueueUserWorkItem(waitCallback);
         }
     }
 }
