8ca1e485428fa357a566da0d8cdae12f6e1e400f
Alfonso J. Ramos <theraot@gmail.com>
2015-10-11 18:47:25 -0500

    Added new workaround for Is64BitProcess

---
+++ Core/Theraot/Core/EnvironmentHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Theraot.Core
+{
+    public static class EnvironmentHelper
+    {
+        public static bool Is64BitProcess
+        {
+            get
+            {
+                // "The value of this property is 4 in a 32-bit process, and 8 in a 64-bit process." -- MSDN
+                return IntPtr.Size == 8;
+            }
+        }
+    }
+}
