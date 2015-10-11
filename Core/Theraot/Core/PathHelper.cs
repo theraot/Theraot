8e7f4972fed2696f70c66bd4b84f02b2198c6149
Alfonso J. Ramos <theraot@gmail.com>
2015-10-11 18:47:53 -0500

    Added PathHelper stub and tests

---
+++ Core/Theraot/Core/PathHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Theraot.Core
+{
+    public static class PathHelper
+    {
+        public static string Combine(params string[] paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Combine(string path1, string path2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Combine(string path1, string path2, string path3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Combine(string path1, string path2, string path3, string path4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Theraot/Threading/PathHelperTest.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Theraot.Core;
+
+namespace Tests.Theraot.Threading
+{
+    internal class PathHelperTest
+    {
+
+        [Test]
+        public void Combine()
+        {
+            Assert.AreEqual("a", PathHelper.Combine(new[] {"a", ""}), "Combine #08");
+        }
+
+        [Test]
+        public void Combine_3Params()
+        {
+            string sep = Path.DirectorySeparatorChar.ToString();
+
+            try
+            {
+                PathHelper.Combine(null, "two", "three");
+                Assert.Fail("#A1-1");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", null, "three");
+                Assert.Fail("#A1-2");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", null);
+                Assert.Fail("#A1-3");
+            }
+            catch
+            {
+                // success
+            }
+
+            Assert.AreEqual(Concat(sep, "one", "two", "three"), PathHelper.Combine("one", "two", "three"), "#A2-1");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three"), PathHelper.Combine(sep + "one", "two", "three"),
+                "#A2-2");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three"),
+                PathHelper.Combine(sep + "one" + sep, "two", "three"), "#A2-3");
+            Assert.AreEqual(Concat(sep, sep + "two", "three"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", "three"), "#A2-4");
+            Assert.AreEqual(Concat(sep, sep + "three"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three"), "#A2-5");
+
+            Assert.AreEqual(Concat(sep, sep + "one" + sep, "two", "three"),
+                PathHelper.Combine(sep + "one" + sep + sep, "two", "three"), "#A3");
+
+            Assert.AreEqual("", PathHelper.Combine("", "", ""), "#A4");
+        }
+
+        [Test]
+        public void Combine_4Params()
+        {
+            string sep = Path.DirectorySeparatorChar.ToString();
+
+            try
+            {
+                PathHelper.Combine(null, "two", "three", "four");
+                Assert.Fail("#A1-1");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", null, "three", "four");
+                Assert.Fail("#A1-2");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", null, "four");
+                Assert.Fail("#A1-3");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", "three", null);
+                Assert.Fail("#A1-4");
+            }
+            catch
+            {
+                // success
+            }
+
+            Assert.AreEqual(Concat(sep, "one", "two", "three", "four"),
+                PathHelper.Combine("one", "two", "three", "four"), "#A2-1");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three", "four"),
+                PathHelper.Combine(sep + "one", "two", "three", "four"), "#A2-2");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three", "four"),
+                PathHelper.Combine(sep + "one" + sep, "two", "three", "four"), "#A2-3");
+            Assert.AreEqual(Concat(sep, sep + "two", "three", "four"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", "three", "four"), "#A2-4");
+            Assert.AreEqual(Concat(sep, sep + "three", "four"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three", "four"), "#A2-5");
+            Assert.AreEqual(Concat(sep, sep + "four"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three", sep + "four"), "#A2-6");
+
+            Assert.AreEqual(Concat(sep, sep + "one" + sep, "two", "three", "four"),
+                PathHelper.Combine(sep + "one" + sep + sep, "two", "three", "four"), "#A3");
+
+            Assert.AreEqual("", PathHelper.Combine("", "", "", ""), "#A4");
+        }
+
+        [Test]
+        public void Combine_ManyParams()
+        {
+            string sep = Path.DirectorySeparatorChar.ToString();
+
+            try
+            {
+                PathHelper.Combine(null, "two", "three", "four", "five");
+                Assert.Fail("#A1-1");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", null, "three", "four", "five");
+                Assert.Fail("#A1-2");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", null, "four", "five");
+                Assert.Fail("#A1-3");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", "three", null, "five");
+                Assert.Fail("#A1-4");
+            }
+            catch
+            {
+                // success
+            }
+
+            try
+            {
+                PathHelper.Combine("one", "two", "three", "four", null);
+                Assert.Fail("#A1-5");
+            }
+            catch
+            {
+                // success
+            }
+
+            Assert.AreEqual(Concat(sep, "one", "two", "three", "four", "five"),
+                PathHelper.Combine("one", "two", "three", "four", "five"), "#A2-1");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three", "four", "five"),
+                PathHelper.Combine(sep + "one", "two", "three", "four", "five"), "#A2-2");
+            Assert.AreEqual(Concat(sep, sep + "one", "two", "three", "four", "five"),
+                PathHelper.Combine(sep + "one" + sep, "two", "three", "four", "five"), "#A2-3");
+            Assert.AreEqual(Concat(sep, sep + "two", "three", "four", "five"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", "three", "four", "five"), "#A2-4");
+            Assert.AreEqual(Concat(sep, sep + "three", "four", "five"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three", "four", "five"), "#A2-5");
+            Assert.AreEqual(Concat(sep, sep + "four", "five"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three", sep + "four", "five"), "#A2-6");
+            Assert.AreEqual(Concat(sep, sep + "five"),
+                PathHelper.Combine(sep + "one" + sep, sep + "two", sep + "three", sep + "four", sep + "five"), "#A2-6");
+
+            Assert.AreEqual(Concat(sep, sep + "one" + sep, "two", "three", "four", "five"),
+                PathHelper.Combine(sep + "one" + sep + sep, "two", "three", "four", "five"), "#A3");
+
+            Assert.AreEqual("", PathHelper.Combine("", "", "", "", ""), "#A4");
+        }
+
+        private string Concat(string sep, params string[] parms)
+        {
+            return String.Join(sep, parms);
+        }
+    }
+}
