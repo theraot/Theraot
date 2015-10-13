a2eb86c8e428c78a8b3f53c018c001de691d25c6
Alfonso J. Ramos <theraot@gmail.com>
2015-10-13 00:52:18 -0500

    Experimental implementation for pointer arithmetic
    I have no test cases for this at the moment of commit.

---
+++ Core/Theraot/Core/IntPtrHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Theraot.Core
+{
+    public static class IntPtrHelper
+    {
+        public static IntPtr Add(IntPtr pointer, int offset)
+        {
+            switch (IntPtr.Size)
+            {
+                case 4:
+                    return new IntPtr(unchecked((int)pointer + offset));
+                case 8:
+                    return new IntPtr(unchecked((long)pointer + offset));
+                default:
+                    throw new NotSupportedException("Not supported platform");
+            }
+        }
+
+        public static IntPtr Substract(IntPtr pointer, int offset)
+        {
+            switch (IntPtr.Size)
+            {
+                case 4:
+                    return new IntPtr(unchecked((int)pointer - offset));
+                case 8:
+                    return new IntPtr(unchecked((long)pointer - offset));
+                default:
+                    throw new NotSupportedException("Not supported platform");
+            }
+        }
+    }
+
+    public static class UIntPtrHelper
+    {
+        [CLSCompliantAttribute(false)]
+        public static UIntPtr Add(UIntPtr pointer, int offset)
+        {
+            switch (UIntPtr.Size)
+            {
+                case 4:
+                    return new UIntPtr(unchecked((uint)((int)pointer + offset)));
+                case 8:
+                    return new UIntPtr(unchecked((ulong)((long)pointer + offset)));
+                default:
+                    throw new NotSupportedException("Not supported platform");
+            }
+        }
+
+        [CLSCompliantAttribute(false)]
+        public static UIntPtr Substract(UIntPtr pointer, int offset)
+        {
+            switch (UIntPtr.Size)
+            {
+                case 4:
+                    return new UIntPtr(unchecked((uint)((int)pointer - offset)));
+                case 8:
+                    return new UIntPtr(unchecked((ulong)((long)pointer - offset)));
+                default:
+                    throw new NotSupportedException("Not supported platform");
+            }
+        }
+    }
+}
