4649dfa365a5bbd85135f5d0f5a4fd534bbac642
Alfonso J. Ramos <theraot@gmail.com>
2015-10-18 07:52:33 -0500

    New CharHelper.IsClassicWhitespace
    Because we don't always mean the unicode property when we say
    "Whitespace".

---
+++ Core/Theraot/Core/CharHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Theraot.Core
+{
+    public static class CharHelper
+    {
+        private static readonly string classicWhitespace =
+            "\u0009" + // Tab
+            "\u000A" + // NewLine
+            "\u000B" + // Vertical Tab
+            "\u000C" + // Form Feed
+            "\u000D" + // Carriage return
+            "\u0020";  // Space
+
+        public static bool IsClassicWhitespace(char character)
+        {
+            return classicWhitespace.IndexOf(character) >= 0;
+        }
+    }
+}
