946585ecd9a068612fed1e990248befb13df9194
Alfonso J. Ramos <theraot@gmail.com>
2015-10-22 23:01:39 -0500

    Fixed IntPtrAddTest and added IntPtrSubtractTest

--- Tests/System/IntPtrTest.cs
+++ Tests/System/IntPtrTest.cs
@@ -19,10 +19,34 @@ namespace Tests.System
                     IntPtr ptr = new IntPtr(parr);
                     // Get the size of an array element.
                     int size = sizeof(int);
+                    int index = 0;
                     for (int ctr = 0; ctr < arr.Length; ctr++)
                     {
                         IntPtr newPtr = IntPtrHelper.Add(ptr, ctr*size);
-                        Console.Write("{0}   ", Marshal.ReadInt32(newPtr));
+                        Assert.AreEqual(arr[index], Marshal.ReadInt32(newPtr));
+                        index++;
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void IntPtrSubtractTest()
+        {
+            int[] arr = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+            unsafe
+            {
+                fixed (int* parr = arr)
+                {
+                    // Get the size of an array element.
+                    int size = sizeof(int);
+                    IntPtr ptr = IntPtrHelper.Add(new IntPtr(parr), size * (arr.Length  - 1));
+                    int index = arr.Length - 1;
+                    for (int ctr = 0; ctr < arr.Length; ctr++)
+                    {
+                        IntPtr newPtr = IntPtrHelper.Subtract(ptr, ctr * size);
+                        Assert.AreEqual(arr[index], Marshal.ReadInt32(newPtr));
+                        index--;
                     }
                 }
             }
