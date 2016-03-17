92958d3ce5b9d481b557ba9b7ad624255684a1fe
Alfonso J. Ramos <theraot@gmail.com>
2016-03-17 00:48:40 -0500

    New test added

---
+++ Tests/System/Threading/Tasks/TaskTestEx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MonoTests.System.Threading.Tasks
+{
+    [TestFixture]
+    public class TaskTestsEx
+    {
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void StartCancelled()
+        {
+            var cancellation = new CancellationToken(true);
+            using (var task = new Task(() => { }, cancellation))
+            {
+                task.Start();
+            }
+        }
+    }
+}
