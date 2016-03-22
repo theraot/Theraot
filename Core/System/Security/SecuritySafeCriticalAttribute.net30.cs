1396acd4422fbef482e3a4d61f9d21890dde333b
Alfonso J. Ramos <theraot@gmail.com>
2016-03-22 16:00:46 -0500

    Fix: Moved SecuritySafeCritical to System.Security

---
+++ Core/System/Security/SecuritySafeCriticalAttribute.net30.cs
@@ -0,0 +1,12 @@
+﻿#if NET20 || NET30
+
+namespace System.Security
+{
+    [AttributeUsage(AttributeTargets.All, Inherited = false)]
+    public sealed class SecuritySafeCriticalAttribute : Attribute
+    {
+        // Empty
+    }
+}
+
+#endif
