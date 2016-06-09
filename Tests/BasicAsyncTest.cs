7dcded3fa84488104c12edbb897734cfee84f261
Alfonso J. Ramos <theraot@gmail.com>
2016-06-09 12:42:56 -0500

    Removed all mentions of .NET 2.0 and 3.0

--- Tests/BasicAsyncTest.cs
+++ Tests/BasicAsyncTest.cs
@@ -8,23 +8,23 @@ namespace Tests
     [TestFixture]
     class BasicAsyncTest
     {
-        private static async Task<string> GetData()
+        private static async Task<string> GetData ()
         {
-            var data = await SlowOperation();
-            return data.ToString(CultureInfo.InvariantCulture);
+            var data = await SlowOperation ();
+            return data.ToString (CultureInfo.InvariantCulture);
         }
 
-        private static Task<int> SlowOperation()
+        private static Task<int> SlowOperation ()
         {
-            Thread.Sleep(1);
-            // FromResult does not exist in .NET 4.0 - TaskEx offer a consistent interface from .NET 2.0 to .NET 4.5
-            return TaskEx.FromResult(7);
+            Thread.Sleep (1);
+            // FromResult does not exist in .NET 4.0 - TaskEx offer a consistent interface from .NET 3.5 to .NET 4.5
+            return TaskEx.FromResult (7);
         }
 
         [Test]
-        public void SimpleTest()
+        public void SimpleTest ()
         {
-            Assert.AreEqual("7", GetData().Result);
+            Assert.AreEqual ("7", GetData ().Result);
         }
     }
 }
