1c7ded3cc2a3aa17ff561906d6097c4d66d395b3
Alfonso J. Ramos <theraot@gmail.com>
2016-10-12 11:45:35 -0500

    Added DebuggerDisplayAttribute for netcf

---
+++ Core/System/Diagnostics/DebuggerDisplayAttribute.netcf.cs
@@ -0,0 +1,86 @@
+﻿#if NETCF
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace System.Diagnostics
+{
+    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Delegate, AllowMultiple = true)]
+    [ComVisibleAttribute(true)]
+    public sealed class DebuggerDisplayAttribute : Attribute
+    {
+        private string _name;
+        private Type _target;
+        private string _targetTypeName;
+        private string _type;
+        private string _value;
+
+        public DebuggerDisplayAttribute(string value)
+        {
+            _value = value;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public Type Target
+        {
+            get
+            {
+                return _target;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException("value");
+                }
+                _target = value;
+            }
+        }
+
+        public string TargetTypeName
+        {
+            get
+            {
+                return _targetTypeName;
+            }
+            set
+            {
+                _targetTypeName = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+    }
+}
+
+#endif
