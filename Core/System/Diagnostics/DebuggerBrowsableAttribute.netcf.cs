a8180100a43b26f003f52b46f9b51bf896d9e7da
Alfonso J. Ramos <theraot@gmail.com>
2016-10-12 16:49:46 -0500

    DebuggerBrowsableAttribute for NETCF

---
+++ Core/System/Diagnostics/DebuggerBrowsableAttribute.netcf.cs
@@ -0,0 +1,32 @@
+﻿#if NETCF
+
+using System.Runtime.InteropServices;
+
+namespace System.Diagnostics
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    [ComVisible(true)]
+    public sealed class DebuggerBrowsableAttribute : Attribute
+    {
+        private DebuggerBrowsableState _state;
+
+        public DebuggerBrowsableAttribute(DebuggerBrowsableState state)
+        {
+            if (state != DebuggerBrowsableState.Collapsed && state != DebuggerBrowsableState.Never && state != DebuggerBrowsableState.RootHidden)
+            {
+                throw new ArgumentOutOfRangeException("state");
+            }
+            _state = state;
+        }
+
+        public DebuggerBrowsableState State
+        {
+            get
+            {
+                return _state;
+            }
+        }
+    }
+}
+
+#endif
---
+++ Core/System/Diagnostics/DebuggerBrowsableState.netcf.cs
@@ -0,0 +1,16 @@
+﻿#if NETCF
+
+using System.Runtime.InteropServices;
+
+namespace System.Diagnostics
+{
+    [ComVisible(true)]
+    public enum DebuggerBrowsableState
+    {
+        Never = 0,
+        Collapsed = 2,
+        RootHidden = 3
+    }
+}
+
+#endif
