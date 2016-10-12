80445e65f52b37bd69b7356be9d80bccfacc1bf4
Alfonso J. Ramos <theraot@gmail.com>
2016-10-12 11:34:36 -0500

    Added SecurityCritical for compact framework

---
+++ Core/System/Security/SecurityCriticalAttribute.netcf.cs
@@ -0,0 +1,35 @@
+﻿#if NETCF
+
+using System;
+namespace System.Security
+{
+    [AttributeUsageAttribute(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Interface | AttributeTargets.Delegate,
+    AllowMultiple = false, Inherited = false)]
+    public sealed class SecurityCriticalAttribute : Attribute
+    {
+        private SecurityCriticalScope _scope;
+
+        public SecurityCriticalAttribute()
+        {
+            this(SecurityCriticalScope.Explicit);
+        }
+
+        public SecurityCriticalAttribute(SecurityCriticalScope scope)
+        {
+
+        }
+
+#if !NET20
+        [ObsoleteAttribute("SecurityCriticalScope is only used for .NET 2.0 transparency compatibility.")]
+#endif
+        public SecurityCriticalScope Scope
+        {
+            get
+            {
+                return _scope;
+            }
+        }
+    }
+}
+
+#endif
---
+++ Core/System/Security/SecurityCriticalScope.netcf.cs
@@ -0,0 +1,15 @@
+﻿#if NETCF
+
+namespace System.Security
+{
+#if !NET20
+    [ObsoleteAttribute("SecurityCriticalScope is only used for .NET 2.0 transparency compatibility.")]
+#endif
+    public enum SecurityCriticalScope
+    {
+        Everything,
+        Explicit
+    }
+}
+
+#endif
