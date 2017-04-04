12a8361674de82e960590534927eb8c2e8024942
Alfonso J. Ramos <theraot@gmail.com>
2017-04-04 10:10:10 -0500

    Marking version 1.0.3 for 2017

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -4,22 +4,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Theraot.Core")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Theraot")]
-[assembly: AssemblyProduct ("Theraot.Core")]
-[assembly: AssemblyCopyright ("Copyright 2010 - 2016 by Theraot")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("Theraot.Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Theraot")]
+[assembly: AssemblyProduct("Theraot.Core")]
+[assembly: AssemblyCopyright("Copyright 2010 - 2017 by Theraot")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("c9c38e25-42f4-460a-a956-fd605fba20e5")]
+[assembly: Guid("c9c38e25-42f4-460a-a956-fd605fba20e5")]
 
 // Version information for an assembly consists of the following four values:
 //
