a98569c897760cf1e43b49bae308c3480b7cdf56
Alfonso J. Ramos <theraot@gmail.com>
2017-04-03 23:55:12 -0500

    New CloneHelper implementation

--- Core/Theraot/Core/CloneHelper.cs
+++ Core/Theraot/Core/CloneHelper.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.IO;
+using System.Reflection;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
 
 namespace Theraot.Core
 {
@@ -11,21 +13,77 @@ namespace Theraot.Core
         public static ICloner<T> GetCloner()
         {
             var type = typeof(T);
-            if (type.IsValueTypeRecursive())
+            return GetStructCloner(type)
+                ?? GetGenericCloner(type)
+                ?? GetObjectCloner(type)
+                ?? GetMockCloner(type)
+                ?? GetConstructorCloner(type)
+                ?? GetDeconstructCloner(type)
+                ?? GetSerializerCloner(type);
+        }
+
+        private static ICloner<T> GetConstructorCloner(Type type)
+        {
+            var constructorInfo = type.GetConstructor(Type.EmptyTypes);
+            if (constructorInfo == null)
             {
-                return StructCloner.Instance;
+                return null;
             }
+            return ConstructorCloner.GetInstance(constructorInfo);
+        }
+
+        private static ICloner<T> GetDeconstructCloner(Type type)
+        {
+            var pair = TypeHelper.GetConstructorDeconstructPair(type);
+            if (pair == null)
+            {
+                return null;
+            }
+            return DeconstructCloner.GetInstance(pair.Item1, pair.Item2);
+        }
+
+        private static ICloner<T> GetGenericCloner(Type type)
+        {
             if (type.IsImplementationOf(typeof(ICloneable<T>)))
             {
-                return GenericCloner.Instance;
+                return GenericCloner.GetInstance();
             }
+            return null;
+        }
+
+        private static ICloner<T> GetMockCloner(Type type)
+        {
+            var method = type.GetMethod("Clone", Type.EmptyTypes);
+            if (method != null && method.ReturnType == type || method.ReturnType == typeof(object))
+            {
+                return MockCloner.GetInstance(method);
+            }
+            return null;
+        }
+
+        private static ICloner<T> GetObjectCloner(Type type)
+        {
             if (type.IsImplementationOf(typeof(ICloneable)))
             {
-                return Cloner.Instance;
+                return Cloner.GetInstance();
             }
+            return null;
+        }
+
+        private static ICloner<T> GetSerializerCloner(Type type)
+        {
             if (type.IsSerializable)
             {
-                return SerializerCloner.Instance;
+                return SerializerCloner.GetInstance();
+            }
+            return null;
+        }
+
+        private static ICloner<T> GetStructCloner(Type type)
+        {
+            if (type.IsValueTypeRecursive())
+            {
+                return StructCloner.GetInstance();
             }
             return null;
         }
@@ -39,12 +97,9 @@ namespace Theraot.Core
                 //Empty
             }
 
-            public static ICloner<T> Instance
+            public static ICloner<T> GetInstance()
             {
-                get
-                {
-                    return _instance;
-                }
+                return _instance;
             }
 
             public T Clone(T target)
@@ -52,6 +107,74 @@ namespace Theraot.Core
                 // Only called with ICloneable target
                 // No need to check for null - let it throw
                 return (T)(target as ICloneable).Clone();
+            }
+        }
+
+        private class ConstructorCloner : ICloner<T>
+        {
+            private static ICloner<T> _instance; // We need a cloner per T type.
+
+            private readonly ConstructorInfo _constructor;
+
+            private ConstructorCloner(ConstructorInfo constructor)
+            {
+                _constructor = constructor;
+            }
+
+            public static ICloner<T> GetInstance(ConstructorInfo constructor)
+            {
+                var found = _instance;
+                if (found == null)
+                {
+                    var created = new ConstructorCloner(constructor);
+                    found = Interlocked.CompareExchange(ref _instance, created, null);
+                    if (found == null)
+                    {
+                        return created;
+                    }
+                }
+                return found;
+            }
+
+            public T Clone(T target)
+            {
+                return (T)_constructor.Invoke(new object[] { target });
+            }
+        }
+
+        private class DeconstructCloner : ICloner<T>
+        {
+            private static ICloner<T> _instance; // We need a cloner per T type.
+
+            private readonly ConstructorInfo _constructor;
+            private readonly MethodInfo _deconstruct;
+
+            private DeconstructCloner(MethodInfo deconstruct, ConstructorInfo constructor)
+            {
+                _deconstruct = deconstruct;
+                _constructor = constructor;
+            }
+
+            public static ICloner<T> GetInstance(MethodInfo deconstruct, ConstructorInfo constructor)
+            {
+                var found = _instance;
+                if (found == null)
+                {
+                    var created = new DeconstructCloner(deconstruct, constructor);
+                    found = Interlocked.CompareExchange(ref _instance, created, null);
+                    if (found == null)
+                    {
+                        return created;
+                    }
+                }
+                return found;
+            }
+
+            public T Clone(T target)
+            {
+                var parameters = new object[] { _deconstruct.GetParameters().Length };
+                _deconstruct.Invoke(target, parameters);
+                return (T)_constructor.Invoke(parameters);
             }
         }
 
@@ -64,12 +187,9 @@ namespace Theraot.Core
                 //Empty
             }
 
-            public static ICloner<T> Instance
+            public static ICloner<T> GetInstance()
             {
-                get
-                {
-                    return _instance;
-                }
+                return _instance;
             }
 
             public T Clone(T target)
@@ -77,6 +197,38 @@ namespace Theraot.Core
                 // Only called with ICloneable<T> target
                 // No need to check for null - let it throw
                 return (target as ICloneable<T>).Clone();
+            }
+        }
+
+        private class MockCloner : ICloner<T>
+        {
+            private static ICloner<T> _instance; // We need a cloner per T type.
+
+            private readonly MethodInfo _method;
+
+            private MockCloner(MethodInfo method)
+            {
+                _method = method;
+            }
+
+            public static ICloner<T> GetInstance(MethodInfo method)
+            {
+                var found = _instance;
+                if (found == null)
+                {
+                    var created = new MockCloner(method);
+                    found = Interlocked.CompareExchange(ref _instance, created, null);
+                    if (found == null)
+                    {
+                        return created;
+                    }
+                }
+                return found;
+            }
+
+            public T Clone(T target)
+            {
+                return (T)_method.Invoke(target, TypeHelper.EmptyObjects);
             }
         }
 
@@ -89,12 +241,9 @@ namespace Theraot.Core
                 //Empty
             }
 
-            public static ICloner<T> Instance
+            public static ICloner<T> GetInstance()
             {
-                get
-                {
-                    return _instance;
-                }
+                return _instance;
             }
 
             public T Clone(T target)
@@ -118,12 +267,9 @@ namespace Theraot.Core
                 //Empty
             }
 
-            public static ICloner<T> Instance
+            public static ICloner<T> GetInstance()
             {
-                get
-                {
-                    return _instance;
-                }
+                return _instance;
             }
 
             public T Clone(T target)
