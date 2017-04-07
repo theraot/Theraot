731b8bea3b76331fd6a2db84a1363bccd4138b1f
Alfonso J. Ramos <theraot@gmail.com>
2017-04-07 08:44:08 -0500

    Build fIx

--- Core/System/Linq/Expressions/ExpressionVisitor.net30.cs
+++ Core/System/Linq/Expressions/ExpressionVisitor.net30.cs
@@ -3,6 +3,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Dynamic.Utils;
 using System.Runtime.CompilerServices;
