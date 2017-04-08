e1202ccf737c2f7298d6c51f6f96ffc5a72ecc66
Alfonso J. Ramos <theraot@gmail.com>
2017-04-08 13:03:18 -0500

    TryGetRawConstantValue's old TODO

--- Core/System/Linq/Expressions/Compiler/LambdaCompiler.Expressions.net30.cs
+++ Core/System/Linq/Expressions/Compiler/LambdaCompiler.Expressions.net30.cs
@@ -840,20 +840,8 @@ namespace System.Linq.Expressions.Compiler
 
         private static bool TryGetRawConstantValue(FieldInfo fi, out object value)
         {
-            // TODO: It looks like GetRawConstantValue is not available at the moment, use it when it comes back.
-            //value = fi.GetRawConstantValue();
-            //return true;
-
-            try
-            {
-                value = fi.GetValue(null);
-                return true;
-            }
-            catch
-            {
-                value = null;
-                return false;
-            }
+            value = fi.GetRawConstantValue();
+            return true;
         }
 
         private void EmitInstance(Expression instance, Type type)
