27e2a83107e2f88d626c1d3e26f248770f22df6b
Alfonso J. Ramos <theraot@gmail.com>
2017-04-12 11:48:55 -0500

    Empty constructors review

--- Core/System/Runtime/CompilerServices/CallerMemberNameAttribute.net40.cs
+++ Core/System/Runtime/CompilerServices/CallerMemberNameAttribute.net40.cs
@@ -31,6 +31,9 @@ namespace System.Runtime.CompilerServices
         [TargetedPatchingOptOut("Performance critical to inline this type of method across NGen image boundaries")]
         public CallerMemberNameAttribute()
         {
+            // Empty
+            // This constructor is not redundant
+            // You cannot put attributes on the default constructor
         }
     }
 }
