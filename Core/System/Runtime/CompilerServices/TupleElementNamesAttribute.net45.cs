d4fc4ae3bab36ae52c056843eaf17efa20c89c24
Alfonso J. Ramos <theraot@gmail.com>
2017-04-12 01:08:58 -0500

    Fixing comments

--- Core/System/Runtime/CompilerServices/TupleElementNamesAttribute.net45.cs
+++ Core/System/Runtime/CompilerServices/TupleElementNamesAttribute.net45.cs
@@ -18,8 +18,7 @@ namespace System.Runtime.CompilerServices
         private readonly string[] _transformNames;
 
         /// <summary>
-        /// Initializes a new instance of the <see
-        /// cref="TupleElementNamesAttribute"/> class.
+        /// Initializes a new instance of the <see cref="TupleElementNamesAttribute"/> class.
         /// </summary>
         /// <param name="transformNames">
         /// Specifies, in a pre-order depth-first traversal of a type's
@@ -30,9 +29,7 @@ namespace System.Runtime.CompilerServices
         /// This constructor is meant to be used on types that contain an
         /// instantiation of <see cref="System.ValueType"/> that contains
         /// element names.  For instance, if <c>C</c> is a generic type with
-        /// two type parameters, then a use of the constructed type <c>C{<see
-        /// cref="System.ValueTuple{T1, T2}"/>, <see
-        /// cref="System.ValueTuple{T1, T2, T3}"/></c> might be intended to
+        /// two type parameters, then a use of the constructed type <c>C{<see cref="System.ValueTuple{T1, T2}"/>, <see cref="System.ValueTuple{T1, T2, T3}"/></c> might be intended to
         /// treat the first type argument as a tuple with element names and the
         /// second as a tuple without element names. In which case, the
         /// appropriate attribute specification should use a
