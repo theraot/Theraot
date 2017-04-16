ef84975929dd45f6361026dadbe552a26f025419
Alfonso J. Ramos <theraot@gmail.com>
2017-04-15 20:38:50 -0500

    ValueTask Build fix

--- Core/System/Runtime/CompilerServices/AsyncMethodBuilderAttribute.net45.cs
+++ Core/System/Runtime/CompilerServices/AsyncMethodBuilderAttribute.net45.cs
@@ -13,15 +13,20 @@ namespace System.Runtime.CompilerServices
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface | AttributeTargets.Delegate | AttributeTargets.Enum, Inherited = false, AllowMultiple = false)]
     public sealed class AsyncMethodBuilderAttribute : Attribute
     {
+        private readonly Type _builderType;
+
         /// <summary>Initializes the <see cref="AsyncMethodBuilderAttribute"/>.</summary>
         /// <param name="builderType">The <see cref="Type"/> of the associated builder.</param>
         public AsyncMethodBuilderAttribute(Type builderType)
         {
-            BuilderType = builderType;
+            _builderType = builderType;
         }
 
         /// <summary>Gets the <see cref="Type"/> of the associated builder.</summary>
-        public Type BuilderType { get; }
+        public Type BuilderType
+        {
+            get { return _builderType; }
+        }
     }
 }
 
