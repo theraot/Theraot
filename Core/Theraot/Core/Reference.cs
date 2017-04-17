ab28c2cda82872b7f07338c87b13b50cc050a12b
Alfonso J. Ramos <theraot@gmail.com>
2017-04-17 07:34:23 -0500

    Using IntrospectionExtensions

--- Core/Theraot/Core/Reference.cs
+++ Core/Theraot/Core/Reference.cs
@@ -1,17 +1,21 @@
 #if FAT
 
+using System.Reflection;
+
 namespace Theraot.Core
 {
     public static class Reference
     {
         public static bool Equals<T>(T objA, T objB)
         {
-            return typeof(T).IsValueType ? false : ReferenceEquals(objA, objB);
+            var info = typeof(T).GetTypeInfo();
+            return info.IsValueType ? false : ReferenceEquals(objA, objB);
         }
 
         public static bool IsNull<T>(T objA)
         {
-            return typeof(T).IsValueType ? false : ReferenceEquals(objA, null);
+            var info = typeof(T).GetTypeInfo();
+            return info.IsValueType ? false : ReferenceEquals(objA, null);
         }
     }
 }
