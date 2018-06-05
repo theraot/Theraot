c44cff58442f276724aabf663cd857f72fc8ef16
Alfonso J. Ramos <theraot@gmail.com>
2018-06-05 05:41:05 -0500

    Making the test compile
    I have changed the string interpolation to string.Format and I have added the appropriate using statements. The code will compile now... The main problem now is the failure condition.

--- Tests/System/Threading/SemaphoreSlimTestsEx.cs
+++ Tests/System/Threading/SemaphoreSlimTestsEx.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace Tests.System.Threading
 {
@@ -12,7 +15,7 @@ namespace Tests.System.Threading
         {
             var source = new CancellationTokenSource(TimeSpan.FromSeconds(100));
             var semaphore = new SemaphoreSlim(0, 3);
-            System.Diagnostics.Debug.WriteLine($"{semaphore.CurrentCount} task can enter the semaphore.");
+            Debug.WriteLine(string.Format("{0} task can enter the semaphore.", semaphore.CurrentCount));
             int padding = 0;
             Task[] tasks = Enumerable.Range(0, 4)
                 .Select(index =>
@@ -20,31 +23,31 @@ namespace Tests.System.Threading
                     return Task.Factory.StartNew(async () =>
                     {
                         var CurrentId = Task.CurrentId;
-                        System.Diagnostics.Debug.WriteLine($"Task {CurrentId} begins and waits for the semaphore.");
+                        Debug.WriteLine(string.Format("Task {0} begins and waits for the semaphore.", CurrentId));
 
                         await semaphore.WaitAsync(source.Token);
 
                         Interlocked.Add(ref padding, 100);
 
-                        System.Diagnostics.Debug.WriteLine($"Task {CurrentId} enters the semaphore.");
+                        Debug.WriteLine(string.Format("Task {0} enters the semaphore.", CurrentId));
 
                         Thread.Sleep(1000 + padding);
 
-                        System.Diagnostics.Debug.WriteLine($"Task {CurrentId} release the semaphore; previous count: {semaphore.Release()} ");
+                        Debug.WriteLine(string.Format("Task {0} release the semaphore; previous count: {1} ", CurrentId, semaphore.Release()));
                     }).Unwrap();
                 }).ToArray();
 
             Thread.Sleep(TimeSpan.FromMilliseconds(500));
 
-            System.Diagnostics.Debug.WriteLine($"Main thread call Release(3) -->");
+            Debug.WriteLine("Main thread call Release(3) -->");
 
             semaphore.Release(3);
 
-            System.Diagnostics.Debug.WriteLine($"{semaphore.CurrentCount} tasks can enter the semaphore.");
+            Debug.WriteLine(string.Format("{0} tasks can enter the semaphore.", semaphore.CurrentCount));
 
             Task.WaitAll(tasks, source.Token);
 
-            System.Diagnostics.Debug.WriteLine("Main thread exits");
+            Debug.WriteLine("Main thread exits");
         }
     }
 }
