ef78500e14c0dcc1ed74af158c9afb2b87e7e52e
Alfonso J. Ramos <theraot@gmail.com>
2018-06-05 05:42:17 -0500

    Making static analysis happy
    Disposing, using var, making the class static.

--- Tests/System/Threading/SemaphoreSlimTestsEx.cs
+++ Tests/System/Threading/SemaphoreSlimTestsEx.cs
@@ -8,46 +8,50 @@ using System.Threading.Tasks;
 namespace Tests.System.Threading
 {
     [TestFixtureAttribute]
-    public class SemaphoreSlimTestsEx
+    public static class SemaphoreSlimTestsEx
     {
         [Test]
         public static void WaitAsyncWaitCorrectly()
         {
-            var source = new CancellationTokenSource(TimeSpan.FromSeconds(100));
-            var semaphore = new SemaphoreSlim(0, 3);
-            Debug.WriteLine(string.Format("{0} task can enter the semaphore.", semaphore.CurrentCount));
-            int padding = 0;
-            Task[] tasks = Enumerable.Range(0, 4)
-                .Select(index =>
+            using (var source = new CancellationTokenSource(TimeSpan.FromSeconds(100)))
+            {
+                using (var semaphore = new SemaphoreSlim(0, 3))
                 {
-                    return Task.Factory.StartNew(async () =>
-                    {
-                        var CurrentId = Task.CurrentId;
-                        Debug.WriteLine(string.Format("Task {0} begins and waits for the semaphore.", CurrentId));
+                    Debug.WriteLine(string.Format("{0} task can enter the semaphore.", semaphore.CurrentCount));
+                    var padding = 0;
+                    var tasks = Enumerable.Range(0, 4)
+                        .Select(_ =>
+                        {
+                            return Task.Factory.StartNew(async () =>
+                            {
+                                var CurrentId = Task.CurrentId;
+                                Debug.WriteLine(string.Format("Task {0} begins and waits for the semaphore.", CurrentId));
 
-                        await semaphore.WaitAsync(source.Token);
+                                await semaphore.WaitAsync(source.Token);
 
-                        Interlocked.Add(ref padding, 100);
+                                Interlocked.Add(ref padding, 100);
 
-                        Debug.WriteLine(string.Format("Task {0} enters the semaphore.", CurrentId));
+                                Debug.WriteLine(string.Format("Task {0} enters the semaphore.", CurrentId));
 
-                        Thread.Sleep(1000 + padding);
+                                Thread.Sleep(1000 + padding);
 
-                        Debug.WriteLine(string.Format("Task {0} release the semaphore; previous count: {1} ", CurrentId, semaphore.Release()));
-                    }).Unwrap();
-                }).ToArray();
+                                Debug.WriteLine(string.Format("Task {0} release the semaphore; previous count: {1} ", CurrentId, semaphore.Release()));
+                            }).Unwrap();
+                        }).ToArray();
 
-            Thread.Sleep(TimeSpan.FromMilliseconds(500));
+                    Thread.Sleep(TimeSpan.FromMilliseconds(500));
 
-            Debug.WriteLine("Main thread call Release(3) -->");
+                    Debug.WriteLine("Main thread call Release(3) -->");
 
-            semaphore.Release(3);
+                    semaphore.Release(3);
 
-            Debug.WriteLine(string.Format("{0} tasks can enter the semaphore.", semaphore.CurrentCount));
+                    Debug.WriteLine(string.Format("{0} tasks can enter the semaphore.", semaphore.CurrentCount));
 
-            Task.WaitAll(tasks, source.Token);
+                    Task.WaitAll(tasks, source.Token);
 
-            Debug.WriteLine("Main thread exits");
+                    Debug.WriteLine("Main thread exits");
+                }
+            }
         }
     }
 }
