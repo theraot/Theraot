17b9c01dae8210a228a4c328b290097783a89dee
Alfonso J. Ramos <theraot@gmail.com>
2018-06-07 14:16:44 -0500

    Revert "Experiment: Do not use the ThreadPool"
    This reverts commit f5b34d2ca9ad2217fb4ae85dea287e86fb48a3b3.

--- Core/System/Threading/SemaphoreSlim.net35.cs
+++ Core/System/Threading/SemaphoreSlim.net35.cs
@@ -356,7 +356,7 @@ namespace System.Threading
                 found = result;
                 if ((found == 0) == _canEnter.IsSet)
                 {
-                    SyncWaitHandle(false);
+                    SyncWaitHandle(true);
                 }
                 return true;
             }
