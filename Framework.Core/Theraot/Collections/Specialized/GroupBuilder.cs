1c7420df500226cac41895f182aaa5230f1bb47a
Alfonso J. Ramos <theraot@gmail.com>
2018-12-10 14:32:23 -0500

    Minimizing code in lock

--- Framework.Core/Theraot/Collections/Specialized/GroupBuilder.cs
+++ Framework.Core/Theraot/Collections/Specialized/GroupBuilder.cs
@@ -12,11 +12,11 @@ namespace Theraot.Collections.Specialized
         private readonly SafeQueue<Grouping<TKey, TElement>> _results;
         private readonly Func<TSource, TElement> _resultSelector;
         private IEnumerator<TSource> _enumerator;
-        private NullAwareDictionary<TKey, ProxyObservable<TElement>> _proxies;
+        private SafeDictionary<TKey, ProxyObservable<TElement>> _proxies;
 
         private GroupBuilder(IEnumerator<TSource> enumerator, IEqualityComparer<TKey> comparer, Func<TSource, TKey> keySelector, Func<TSource, TElement> resultSelector)
         {
-            _proxies = new NullAwareDictionary<TKey, ProxyObservable<TElement>>(comparer);
+            _proxies = new SafeDictionary<TKey, ProxyObservable<TElement>>(comparer);
             _enumerator = enumerator;
             _results = new SafeQueue<Grouping<TKey, TElement>>();
             _keySelector = keySelector;
@@ -77,31 +77,30 @@ namespace Theraot.Collections.Specialized
 
         private bool MoveNext(IEnumerator<TSource> enumerator)
         {
+            TSource item;
             lock (enumerator)
             {
-                if (enumerator.MoveNext())
+                if (!enumerator.MoveNext())
                 {
-                    var item = enumerator.Current;
-                    var key = _keySelector(item);
-                    var element = _resultSelector(item);
-                    if (!_proxies.TryGetValue(key, out var proxy))
-                    {
-                        proxy = new ProxyObservable<TElement>();
-                        var progressor = Progressor<TElement>.CreateFromIObservable(proxy, Advance);
-                        var items = ProgressiveCollection<TElement>.Create<SafeCollection<TElement>>
-                        (
-                            progressor,
-                            EqualityComparer<TElement>.Default
-                        );
-                        var result = new Grouping<TKey, TElement>(key, items);
-                        _proxies.Add(key, proxy);
-                        _results.Add(result);
-                    }
-                    proxy.OnNext(element);
-                    return true;
+                    return false;
                 }
+                item = enumerator.Current;
             }
-            return false;
+            var key = _keySelector(item);
+            var element = _resultSelector(item);
+            if (_proxies.TryGetOrAdd(key, _ => new ProxyObservable<TElement>(), out var proxy))
+            {
+                var progressor = Progressor<TElement>.CreateFromIObservable(proxy, Advance);
+                var items = ProgressiveCollection<TElement>.Create<SafeCollection<TElement>>
+                (
+                    progressor,
+                    EqualityComparer<TElement>.Default
+                );
+                var result = new Grouping<TKey, TElement>(key, items);
+                _results.Add(result);
+            }
+            proxy.OnNext(element);
+            return true;
         }
     }
 }
