18ddf909bb2698a81f2daf2352bdd900a106a591
Alfonso J. Ramos <theraot@gmail.com>
2018-12-18 13:02:47 -0500

    What do you do when there is no test engine for all platforms?
    You roll your own.
    
    Ok, to be fair, I did not look into every test engine out there... but I did look into those that have native support in Resharper and AppVeyor, and did not find any that would work for all .NET targets without conflicting with my libraries.
    
    I am basing this in NUnit, because I have been using NUnit. What I have implemented here should cover over 90% of all my test cases, and I can extend it to support anything else I need.
    
    Oh, what is that, that I am using my libraries to test my libraries? Yes. That I should not trust in a system to test a system? Also true. However, this is not my libraries testing themselves, this is me using my libraries to help me test my libraries. I am supervising this. Besides, the more I use my libraries the more likely is that I find edge cases.

---
+++ TestRunner/Program.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using Theraot.Collections;
+using Theraot.Collections.Specialized;
+using Theraot.Collections.ThreadSafe;
+using Theraot.Core;
+using Theraot.Reflection;
+
+namespace TestRunner
+{
+    public static class Assert
+    {
+        public static void AreEqual<T>(T expected, T found, string message = null)
+        {
+            if (Equals(expected, found))
+            {
+                return;
+            }
+            if (message != null)
+            {
+                throw new AssertionFailedException($"Expected: {expected} - Found: {found} - Message: {message}");
+            }
+            throw new AssertionFailedException($"Expected: {expected} - Found: {found}");
+        }
+
+        public static void AreNotEqual<T>(T expected, T found, string message = null)
+        {
+            if (!Equals(expected, found))
+            {
+                return;
+            }
+            if (message != null)
+            {
+                throw new AssertionFailedException($"Unexpected: {found} - Message: {message}");
+            }
+            throw new AssertionFailedException($"Unexpected: {found}");
+        }
+
+        public static void Fail(string message = null)
+        {
+            if (message != null)
+            {
+                throw new AssertionFailedException($"Failed - Message: {message}");
+            }
+            throw new AssertionFailedException("Failed");
+        }
+
+        public static void IsFalse(bool found, string message = null)
+        {
+            AreEqual(true, found, message);
+        }
+
+        public static void IsNotNull<T>(T found, string message = null)
+            where T : class
+        {
+            AreNotEqual(null, found, message);
+        }
+
+        public static void IsNull<T>(T found, string message = null)
+            where T : class
+        {
+            AreEqual(null, found, message);
+        }
+
+        public static void IsTrue(bool found, string message = null)
+        {
+            AreEqual(true, found, message);
+        }
+
+        public static void Throws<TException>(Action action, string message = null)
+            where TException : Exception
+        {
+            try
+            {
+                action();
+            }
+            catch (TException exception)
+            {
+                GC.KeepAlive(exception);
+            }
+            catch (Exception exception)
+            {
+                if (message != null)
+                {
+                    throw new AssertionFailedException($"Expected: {typeof(TException).Name} - Found: {exception} - Message: {message}");
+                }
+                throw new AssertionFailedException($"Expected: {typeof(TException).Name} - Found: {exception}");
+            }
+            if (message != null)
+            {
+                throw new AssertionFailedException($"Expected: {typeof(TException).Name} - Message: {message}");
+            }
+            throw new AssertionFailedException($"Expected: {typeof(TException).Name}");
+        }
+    }
+
+    public static class Program
+    {
+        public static void ExceptionReport(Exception exception)
+        {
+            var report = new StringBuilder();
+            report.Append("Exception");
+            report.Append("\r\n\r\n");
+            var current = exception;
+            for (; current != null; current = current.InnerException)
+            {
+                report.Append
+                (
+                    StringHelper.Join
+                    (
+                        "\r\n\r\n",
+                        "== Exception Type ==",
+                        current.GetType().Name,
+                        "== Exception Message ==",
+                        current.Message,
+                        "== Source ==",
+                        current.Source,
+                        "== Stacktrace ==",
+                        current.StackTrace
+                    )
+                );
+            }
+            report.Append("\r\n\r\n");
+            var extendedStackTrace = Environment.StackTrace.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            report.Append
+            (
+                StringHelper.Join
+                (
+                    "\r\n",
+                    new ExtendedEnumerable<string>(Extensions.AsUnaryIEnumerable("== Reconstructed StackTrace ==\r\n"), extendedStackTrace.SkipItems(4))
+                )
+            );
+            Console.WriteLine(report.ToString());
+        }
+
+        public static void Main()
+        {
+            var ignoredCategories = new string[]
+            {
+                
+            };
+            var tests = GetAllTests(ignoredCategories);
+            var stopwatch = new Stopwatch();
+            foreach (var test in tests)
+            {
+                stopwatch.Reset();
+                using (test)
+                {
+                    try
+                    {
+                        test.Invoke();
+                    }
+                    catch (Exception exception)
+                    {
+                        ExceptionReport(exception);
+                    }
+                    stopwatch.Stop();
+                    Console.WriteLine(stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private static IEnumerable<Test> GetAllTests(string[] ignoredCategories)
+        {
+            return GetAllTypes()
+                .Where(IsTestType)
+                .SelectMany(t => t.GetMethods())
+                .Where(IsTestMethod)
+                .Select(method => new CategorizedMethod(method))
+                .Where(categorizedMethod => !categorizedMethod.Categories.Overlaps(ignoredCategories))
+                .Select(categorizedMethod => new Test(categorizedMethod.Method));
+        }
+
+        private static IEnumerable<Type> GetAllTypes()
+        {
+            var programType = typeof(Program);
+            var assembly = programType.GetTypeInfo().Assembly;
+            return assembly.GetExportedTypes();
+        }
+
+        private static bool IsTestMethod(MethodInfo methodInfo)
+        {
+            return methodInfo.HasAttribute<TestAttribute>() && !methodInfo.HasAttribute<IgnoreAttribute>();
+        }
+
+        private static bool IsTestType(Type type)
+        {
+            return type.HasAttribute<TestFixtureAttribute>() && !type.HasAttribute<IgnoreAttribute>();
+        }
+
+        private sealed class CategorizedMethod
+        {
+            public CategorizedMethod(MethodInfo method)
+            {
+                Method = method;
+                Categories = method.GetAttributes<CategoryAttribute>(false).Select(category => category.Name);
+            }
+
+            public IEnumerable<string> Categories { get; }
+            public MethodInfo Method { get; }
+        }
+
+        private sealed class Test : IDisposable
+        {
+            private Delegate _delegate;
+            private object _instance;
+            public Test(MethodInfo methodInfo)
+            {
+                var type = methodInfo.DeclaringType;
+                if (type == null)
+                {
+                    throw new ArgumentException();
+                }
+                if (methodInfo.IsStatic)
+                {
+                    _instance = null;
+                }
+                else
+                {
+                    _instance = Activator.CreateInstance(type);
+                }
+                _delegate = TypeHelper.BuildDelegate(methodInfo, type);
+            }
+
+            public void Dispose()
+            {
+                var instance = Interlocked.Exchange(ref _instance, null);
+                if (instance is IDisposable disposable)
+                {
+                    disposable.Dispose();
+                }
+                Interlocked.Exchange(ref _delegate, null);
+            }
+
+            public void Invoke()
+            {
+                var @delegate = Volatile.Read(ref _delegate);
+                if (@delegate == null)
+                {
+                    throw new ObjectDisposedException(nameof(Test));
+                }
+                _delegate.DynamicInvoke(ArrayReservoir<object>.EmptyArray);
+            }
+        }
+    }
+
+    public sealed class AssertionFailedException : Exception
+    {
+
+        public AssertionFailedException()
+        {
+        }
+
+        public AssertionFailedException(string message)
+            : base(message)
+        {
+        }
+
+        public AssertionFailedException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public sealed class CategoryAttribute : Attribute
+    {
+        public CategoryAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = false)]
+    public sealed class IgnoreAttribute : Attribute
+    {
+        // Empty
+    }
+
+    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
+    public sealed class TestAttribute : Attribute
+    {
+        // Empty
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class TestFixtureAttribute : Attribute
+    {
+        // Empty
+    }
+}
