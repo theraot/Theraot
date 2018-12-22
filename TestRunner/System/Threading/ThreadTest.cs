4277f3492abb13d0dd1e1710c23c79b787b9af32
Alfonso J. Ramos <theraot@gmail.com>
2018-12-22 18:18:58 -0500

    First Thread test

---
+++ TestRunner/System/Threading/ThreadTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace TestRunner.System.Threading
+{
+    [TestFixture]
+    public static class ThreadTest
+    {
+        [Test]
+        public static void NameCurrentThread()
+        {
+            var thread = Thread.CurrentThread;
+            thread.Name = "threadName";
+            Assert.AreEqual("threadName", thread.Name);
+            Assert.IsTrue(thread.IsAlive);
+        }
+    }
+}
