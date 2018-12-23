51176d059a37ae41cb5ddfde2b58ee7805e8c9d7
Alfonso J. Ramos <theraot@gmail.com>
2018-12-23 18:12:08 -0500

    New thread tests

--- TestRunner/System/Threading/ThreadTest.cs
+++ TestRunner/System/Threading/ThreadTest.cs
@@ -18,5 +18,37 @@ namespace TestRunner.System.Threading
             Assert.Throws<InvalidOperationException>(() => { thread.Name = secondName; });
             Assert.IsTrue(thread.IsAlive);
         }
+
+        [Test]
+        public static void NewThreadRuns()
+        {
+            var value = 0;
+            var thread = new Thread(() => value = 1);
+            thread.Start();
+            thread.Join();
+            Assert.AreEqual(1, value);
+        }
+
+        [Test]
+        public static void ParametrizedThreadStart()
+        {
+            object found = null;
+            var thread = new Thread(param => found = param);
+            var sent = new object();
+            thread.Start(sent);
+            thread.Join();
+            Assert.AreEqual(sent, found);
+        }
+
+        [Test]
+        public static void NewThreadNullDelegate()
+        {
+            ThreadStart start = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => GC.KeepAlive(new Thread(start)));
+            ParameterizedThreadStart parameterizedStart = null;
+            // ReSharper disable once ExpressionIsAlwaysNull
+            Assert.Throws<ArgumentNullException>(() => GC.KeepAlive(new Thread(parameterizedStart)));
+        }
     }
 }
