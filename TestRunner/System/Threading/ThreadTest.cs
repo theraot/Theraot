7a5dcb105c1806641d6c87d10c290f57119911be
Alfonso J. Ramos <theraot@gmail.com>
2018-12-26 11:31:47 -0500

    New tests for Thread

--- TestRunner/System/Threading/ThreadTest.cs
+++ TestRunner/System/Threading/ThreadTest.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
+using Theraot.Core;
 using Theraot.Threading;
+using ThreadState = System.Threading.ThreadState;
 
 namespace TestRunner.System.Threading
 {
@@ -9,9 +12,24 @@ namespace TestRunner.System.Threading
     public static class ThreadTest
     {
         [Test]
+        public static void CurrentThreadInTaskIsBackground()
+        {
+            ThreadState found = default;
+            TaskEx.Run(() => found = Thread.CurrentThread.ThreadState).Wait();
+            Assert.AreEqual(ThreadState.Background, found);
+        }
+
+        [Test]
         public static void CurrentThreadIsAlive()
         {
             Assert.IsTrue(Thread.CurrentThread.IsAlive);
+        }
+
+        [Test]
+        public static void JoinNotStartedThrows()
+        {
+            var thread = new Thread(() => { });
+            Assert.Throws<ThreadStateException>(thread.Join);
         }
 
         [Test]
@@ -97,6 +115,124 @@ namespace TestRunner.System.Threading
             stopWatch.Start();
             Thread.Sleep(millisecondsTimeout);
             Assert.IsTrue(stopWatch.Elapsed.TotalMilliseconds > millisecondsTimeout);
+        }
+
+        [Test]
+        public static void StartingAnStartedThreadThrows()
+        {
+            var thread = new Thread(() => { });
+            thread.Start();
+            Assert.Throws<ThreadStateException>(thread.Start);
+        }
+
+        [Test]
+        public static void StartingATaskThreadThrows()
+        {
+            Thread thread = null;
+            TaskEx.Run(() => { thread = Thread.CurrentThread; }).Wait();
+            Assert.Throws<ThreadStateException>(thread.Start);
+        }
+
+        [Test]
+        public static void StartingCurrentThreadThrows()
+        {
+            var thread = Thread.CurrentThread;
+            Assert.Throws<ThreadStateException>(thread.Start);
+        }
+
+        [Test]
+        public static void TaskThreadIsBackgroundAfterWait()
+        {
+            Thread thread = null;
+            TaskEx.Run(() => thread = Thread.CurrentThread).Wait();
+            Assert.AreEqual(ThreadState.Background, thread.ThreadState);
+        }
+
+        [Test]
+        public static void TaskThreadIsBackgroundWhileLooping()
+        {
+            var signal = new[] { 0 };
+            Thread thread = null;
+            TaskEx.Run
+            (
+                () =>
+                {
+                    thread = Thread.CurrentThread;
+                    var spinWait = new SpinWait();
+                    while (Volatile.Read(ref signal[0]) == 0)
+                    {
+                        spinWait.SpinOnce();
+                    }
+                }
+            );
+            ThreadingHelper.SpinWaitWhileNull(ref thread);
+            Assert.AreEqual(ThreadState.Background, thread.ThreadState);
+            ThreadingHelper.MemoryBarrier();
+            Volatile.Write(ref signal[0], 1);
+        }
+
+        [Test]
+        public static void TaskThreadIsBackgroundWhileWaiting()
+        {
+            var manualResetEvent = new ManualResetEvent(false);
+            Thread thread = null;
+            TaskEx.Run
+            (
+                () =>
+                {
+                    thread = Thread.CurrentThread;
+                    manualResetEvent.WaitOne();
+                }
+            );
+            ThreadingHelper.SpinWaitWhileNull(ref thread);
+            Assert.IsTrue(EnumHelper.HasFlag(thread.ThreadState, ThreadState.Background));
+            manualResetEvent.Set();
+        }
+
+        [Test]
+        public static void ThreadIsRunningWhileLooping()
+        {
+            var signal = new[] { 0 };
+            var thread = new Thread
+            (
+                () =>
+                {
+                    var spinWait = new SpinWait();
+                    while (Volatile.Read(ref signal[0]) == 0)
+                    {
+                        spinWait.SpinOnce();
+                    }
+                }
+            );
+            thread.Start();
+            Assert.AreEqual(ThreadState.Running, thread.ThreadState);
+            Volatile.Write(ref signal[0], 1);
+        }
+
+        [Test]
+        public static void ThreadIsRunningWhileWaiting()
+        {
+            var manualResetEvent = new ManualResetEvent(false);
+            var thread = new Thread(() => { manualResetEvent.WaitOne(); });
+            thread.Start();
+            Assert.AreEqual(ThreadState.Running, thread.ThreadState);
+            manualResetEvent.Set();
+        }
+
+        [Test]
+        public static void ThreadIsStoppedAfterJoin()
+        {
+            var thread = new Thread(() => { });
+            thread.Start();
+            thread.Join();
+            Assert.AreEqual(ThreadState.Stopped, thread.ThreadState);
+        }
+
+        [Test]
+        public static void ThreadIsUnstartedBeforeStart()
+        {
+            var thread = new Thread(() => { });
+            Assert.AreEqual(ThreadState.Unstarted, thread.ThreadState);
         }
 
         [Test(IsolatedThread = true)]
