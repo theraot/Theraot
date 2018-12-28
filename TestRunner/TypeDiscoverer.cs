13b136014f58f1d1f5557f23dc51b13b7885d81a
Alfonso J. Ramos <theraot@gmail.com>
2018-12-27 20:24:47 -0500

    Making tools happy about TypeDiscoverer

--- TestRunner/TypeDiscoverer.cs
+++ TestRunner/TypeDiscoverer.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace TestRunner
+﻿namespace TestRunner
 {
     public static class TypeDiscoverer
     {
-        private static Type[] _types;
+        private static global::System.Type[] _types;
 
-        public static IEnumerable<Type> GetAllTypes()
+        public static global::System.Collections.Generic.IEnumerable<global::System.Type> GetAllTypes()
         {
             GetAllTypesPrivate(ref _types);
             return _types;
         }
 
-        private static void GetAllTypesPrivate(ref Type[] types)
+        private static void GetAllTypesPrivate(ref global::System.Type[] types)
         {
             if (_types != null)
             {
                 return;
             }
 #if NET20 || NET30 || NET40 || NET45 || NET46 || NET47 || NETCOREAPP1_0 || NETCOREAPP1_1 || NETCOREAPP2_1 || NETCOREAPP2_2 || NETSTANDARD1_5 || NETSTANDARD1_6
-            types = typeof(TypeDiscoverer).GetTypeInfo().Assembly.GetTypes();
+            types = global::System.Reflection.IntrospectionExtensions.GetTypeInfo(typeof(TypeDiscoverer)).Assembly.GetTypes();
 #else
-            types =  new Type[]
+            types = new []
 			{
-				typeof(TestRunner.System.Threading.ThreadTest),
+				typeof(System.Threading.ThreadTest),
 			};
 #endif
 		}
