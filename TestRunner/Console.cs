76255e2a134880a76af686645019f6403b4de7f6
Alfonso J. Ramos <theraot@gmail.com>
2018-12-28 06:52:48 -0500

    Console Trickery
    When running on .NET Standard there is no standard output... oh, but unhandled exception output to the console.

--- TestRunner/Console.cs
+++ TestRunner/Console.cs
@@ -1,108 +1,115 @@
 ﻿#if NETSTANDARD1_0 || NETSTANDARD1_1 || NETSTANDARD1_2
 
-using System.Diagnostics;
+using System.Text;
 
 namespace System
 {
     public static class Console
     {
+        private static readonly StringBuilder _stringBuilder = new StringBuilder();
+
+        public static void ReadKey()
+        {
+            throw new Exception(_stringBuilder.ToString());
+        }
+
         public static void WriteLine(string value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value);
         }
 
         [CLSCompliant(false)]
         public static void WriteLine(ulong value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         [CLSCompliant(false)]
         public static void WriteLine(uint value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(float value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(decimal value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(long value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(int value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(char value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(bool value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(char[] value)
         {
             var str = new string(value);
-            Debug.WriteLine(str);
+            _stringBuilder.AppendLine(str);
         }
 
         public static void WriteLine(char[] buffer, int index, int count)
         {
             var str = new string(buffer, index, count);
-            Debug.WriteLine(str);
+            _stringBuilder.AppendLine(str);
         }
 
         public static void WriteLine(object value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine()
         {
-            Debug.WriteLine(string.Empty);
+            _stringBuilder.AppendLine(string.Empty);
         }
 
         public static void WriteLine(double value)
         {
-            Debug.WriteLine(value);
+            _stringBuilder.AppendLine(value.ToString());
         }
 
         public static void WriteLine(string format, object arg0)
         {
-            Debug.WriteLine(format, arg0);
+            _stringBuilder.AppendLine(string.Format(format, arg0));
         }
 
         public static void WriteLine(string format, object arg0, object arg1)
         {
-            Debug.WriteLine(format, arg0, arg1);
+            _stringBuilder.AppendLine(string.Format(format, arg0, arg1));
         }
 
         public static void WriteLine(string format, object arg0, object arg1, object arg2)
         {
-            Debug.WriteLine(format, arg0, arg1, arg2);
+            _stringBuilder.AppendLine(string.Format(format, arg0, arg1, arg2));
         }
 
         public static void WriteLine(string format, object arg0, object arg1, object arg2, object arg3)
         {
-            Debug.WriteLine(format, arg0, arg1, arg2, arg3);
+            _stringBuilder.AppendLine(string.Format(format, arg0, arg1, arg2, arg3));
         }
 
         public static void WriteLine(string format, params object[] arg)
         {
-            Debug.WriteLine(format, arg);
+            _stringBuilder.AppendLine(string.Format(format, arg));
         }
     }
 }
