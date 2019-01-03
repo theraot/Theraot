ab16a7844960fe6e49e81282fddbb5e2367da75c
Alfonso J. Ramos <theraot@gmail.com>
2019-01-03 06:17:46 -0500

    Added TargetFrameworkAttribute

---
+++ Framework.Core/System/Runtime/Versioning/TargetFrameworkAttribute.cs
@@ -0,0 +1,19 @@
+﻿#if NET20 || NET30 || NET35
+
+namespace System.Runtime.Versioning
+{
+    [AttributeUsageAttribute(AttributeTargets.Assembly)]
+    public sealed class TargetFrameworkAttribute : Attribute
+    {
+        public TargetFrameworkAttribute(string frameworkName)
+        {
+            FrameworkName = frameworkName ?? throw new ArgumentNullException(nameof(frameworkName));
+        }
+
+        public string FrameworkName { get; }
+
+        public string FrameworkDisplayName { get; set; }
+    }
+}
+
+#endif
