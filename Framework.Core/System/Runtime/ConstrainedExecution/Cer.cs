09204943ed0dd64fd33b2fefad7e2c96978d2322
Alfonso J. Ramos <theraot@gmail.com>
2019-01-06 07:03:50 -0500

    Added ReliabilityContractAttribute et.al.
    For the sake of no conditional compilation around its use

---
+++ Framework.Core/System/Runtime/ConstrainedExecution/Cer.cs
@@ -0,0 +1,13 @@
+﻿#if LESSTHAN_NETCOREAPP20 || LESSTHAN_NETSTANDARD20
+
+namespace System.Runtime.ConstrainedExecution
+{
+    public enum Cer
+    {
+        None = 0,
+        MayFail = 1,
+        Success = 2
+    }
+}
+
+#endif
---
+++ Framework.Core/System/Runtime/ConstrainedExecution/Consistency.cs
@@ -0,0 +1,14 @@
+﻿#if LESSTHAN_NETCOREAPP20 || LESSTHAN_NETSTANDARD20
+
+namespace System.Runtime.ConstrainedExecution
+{
+    public enum Consistency
+    {
+        MayCorruptProcess = 0,
+        MayCorruptAppDomain = 1,
+        MayCorruptInstance = 2,
+        WillNotCorruptState = 3
+    }
+}
+
+#endif
