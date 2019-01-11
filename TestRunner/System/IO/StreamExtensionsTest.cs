442c7edfb0f58927dfa9669c953a3da201a725a4
Alfonso J. Ramos <theraot@gmail.com>
2019-01-10 21:08:32 -0500

    Added tests for ReadAsync
    This is the first async test.
    
    (That took a me a while to make it work correctly)

--- TestRunner/System/IO/StreamExtensionsTest.cs
+++ TestRunner/System/IO/StreamExtensionsTest.cs
@@ -1,13 +1,30 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using System.Threading.Tasks;
+
+#if LESSTHAN_NET45
+
 using Theraot.Core;
+
+#endif
 
 namespace TestRunner.System.IO
 {
     [TestFixture]
     public static class StreamExtensionsTest
     {
+        [Test]
+        public static async Task ReadAsyncReads()
+        {
+            var stream = new MemoryStream(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+            var buffer = new byte[10];
+            // ReSharper disable once MethodSupportsCancellation
+            int x = await stream.ReadAsync(buffer, 0, 10);
+            Assert.AreEqual(10, x);
+            Assert.CollectionEquals(buffer, new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 });
+        }
+
         [Test]
         public static void ReadAsyncThrowsOnCanceledToken()
         {
