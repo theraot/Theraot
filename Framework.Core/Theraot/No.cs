932ccaf0e3382667f2d0adfa429a94d9e71d2e2a
Alfonso J. Ramos <theraot@gmail.com>
2019-01-12 09:07:56 -0500

    No.Op initial implementation

---
+++ Framework.Core/Theraot/No.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Theraot
+{
+    public static class No
+    {
+        [Conditional("THERAOT_NEVER")]
+        public static void Op<T>(T arg)
+        {
+            // Empty
+        }
+    }
+}
