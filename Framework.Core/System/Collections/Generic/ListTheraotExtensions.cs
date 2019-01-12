1eb89df17b0f2dd893ea15c4a3bfead027b43263
Alfonso J. Ramos <theraot@gmail.com>
2019-01-12 07:51:00 -0500

    New ListTheraotExtensions
    Adding List.AsReadOnly for .NET Standard 1.0, 1.1 and 1.2

---
+++ Framework.Core/System/Collections/Generic/ListTheraotExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace System.Collections.Generic
+{
+    public static class ListTheraotExtensions
+    {
+#if LESSTHAN_NETSTANDARD13
+        public static ReadOnlyCollection<T> AsReadOnly<T>(this List<T> list)
+        {
+            return new ReadOnlyCollection<T>(list);
+        }
+#endif
+    }
+}
