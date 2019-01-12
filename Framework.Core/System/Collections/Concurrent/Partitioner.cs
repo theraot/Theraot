fbf6c36204cf8b841b45dc80f1e1f16b04a9bd18
Alfonso J. Ramos <theraot@gmail.com>
2019-01-12 08:39:21 -0500

    New PartitionerEx

--- Framework.Core/System/Collections/Concurrent/Partitioner.cs
+++ Framework.Core/System/Collections/Concurrent/Partitioner.cs
@@ -1,6 +1,7 @@
 ﻿#if LESSTHAN_NET40 || NETSTANDARD1_0
 
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using Theraot.Collections;
 using Theraot.Collections.Specialized;
@@ -46,23 +47,10 @@ namespace System.Collections.Concurrent
             return Create(source, EnumerablePartitionerOptions.None);
         }
 
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         public static OrderablePartitioner<TSource> Create<TSource>(IEnumerable<TSource> source, EnumerablePartitionerOptions partitionerOptions)
         {
-            if (source == null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-            if ((partitionerOptions & ~EnumerablePartitionerOptions.NoBuffering) != 0)
-            {
-                // We could support NoBuffering, no problem.
-                // In fact, what is that about chunking? We do nothing of that.
-                // I bet I could just use the current implementation as is...
-                // ... it was created to partition on demand, no buffering.
-                // However this is the documented behaviour.
-                // Which also means there are no examples for a dynamic no buffering partition.
-                throw new ArgumentOutOfRangeException(nameof(partitionerOptions));
-            }
-            return new DynamicOrderablePartitioner<TSource>(source);
+            return PartitionerEx.Create(source, partitionerOptions);
         }
 
         public static OrderablePartitioner<Tuple<long, long>> Create(long fromInclusive, long toExclusive)
@@ -162,83 +150,6 @@ namespace System.Collections.Concurrent
         }
 
         public abstract IList<IEnumerator<TSource>> GetPartitions(int partitionCount);
-    }
-
-    internal sealed class DynamicOrderablePartitioner<T> : OrderablePartitioner<T>
-    {
-        private readonly IEnumerable<T> _source;
-
-        public DynamicOrderablePartitioner(IEnumerable<T> source)
-            : base(true, false, true)
-        {
-            _source = source;
-        }
-
-        public override bool SupportsDynamicPartitions => true;
-
-        public override IEnumerable<T> GetDynamicPartitions()
-        {
-            if (_source is T[] array)
-            {
-                return Progressor<T>.CreateFromArray(array);
-            }
-            if (_source is IList<T> list)
-            {
-                return Progressor<T>.CreateFromIList(list);
-            }
-            return Progressor<T>.CreateFromIEnumerable(_source);
-        }
-
-        public override IEnumerable<KeyValuePair<long, T>> GetOrderableDynamicPartitions()
-        {
-            var source = GetDynamicPartitions();
-            var subIndex = 0L;
-            foreach (var item in source)
-            {
-                yield return new KeyValuePair<long, T>(subIndex, item);
-                subIndex++;
-            }
-        }
-
-        public override IList<IEnumerator<KeyValuePair<long, T>>> GetOrderablePartitions(int partitionCount)
-        {
-            var list = new List<IEnumerator<KeyValuePair<long, T>>>(partitionCount);
-            var source = GetDynamicPartitions();
-            for (var index = 0; index < partitionCount; index++)
-            {
-                list.Add(Enumerator(index));
-            }
-            return list.AsReadOnly();
-            IEnumerator<KeyValuePair<long, T>> Enumerator(int index)
-            {
-                var subIndex = 0;
-                // ReSharper disable once PossibleMultipleEnumeration
-                foreach (var item in source)
-                {
-                    yield return new KeyValuePair<long, T>(NumericHelper.BuildInt64(index, subIndex), item);
-                    subIndex++;
-                }
-            }
-        }
-
-        public override IList<IEnumerator<T>> GetPartitions(int partitionCount)
-        {
-            var list = new List<IEnumerator<T>>(partitionCount);
-            var source = GetDynamicPartitions();
-            for (var index = 0; index < partitionCount; index++)
-            {
-                list.Add(Enumerator());
-            }
-            return list.AsReadOnly();
-            IEnumerator<T> Enumerator()
-            {
-                // ReSharper disable once PossibleMultipleEnumeration
-                foreach (var item in source)
-                {
-                    yield return item;
-                }
-            }
-        }
     }
 
     internal sealed class StaticOrderablePartitioner<T> : OrderablePartitioner<T>
---
+++ Framework.Core/System/Collections/Concurrent/PartitionerEx.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+#if LESSTHAN_NET45 || NETSTANDARD1_0
+
+using Theraot.Collections;
+using Theraot.Core;
+
+#endif
+
+namespace System.Collections.Concurrent
+{
+    public static class PartitionerEx
+    {
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static OrderablePartitioner<TSource> Create<TSource>(IEnumerable<TSource> source, EnumerablePartitionerOptions partitionerOptions)
+        {
+#if LESSTHAN_NET45 || NETSTANDARD1_0
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if ((partitionerOptions & ~EnumerablePartitionerOptions.NoBuffering) != 0)
+            {
+                // We could support NoBuffering, no problem.
+                // In fact, what is that about chunking? We do nothing of that.
+                // I bet I could just use the current implementation as is...
+                // ... it was created to partition on demand, no buffering.
+                // However this is the documented behaviour.
+                // Which also means there are no examples for a dynamic no buffering partition.
+                throw new ArgumentOutOfRangeException(nameof(partitionerOptions));
+            }
+            return new DynamicOrderablePartitioner<TSource>(source);
+#else
+            return Partitioner.Create(source, partitionerOptions);
+#endif
+        }
+    }
+
+#if LESSTHAN_NET45 || NETSTANDARD1_0
+
+    internal sealed class DynamicOrderablePartitioner<T> : OrderablePartitioner<T>
+    {
+        private readonly IEnumerable<T> _source;
+
+        public DynamicOrderablePartitioner(IEnumerable<T> source)
+            : base(true, false, true)
+        {
+            _source = source;
+        }
+
+        public override bool SupportsDynamicPartitions => true;
+
+        public override IEnumerable<T> GetDynamicPartitions()
+        {
+            if (_source is T[] array)
+            {
+                return Progressor<T>.CreateFromArray(array);
+            }
+            if (_source is IList<T> list)
+            {
+                return Progressor<T>.CreateFromIList(list);
+            }
+            return Progressor<T>.CreateFromIEnumerable(_source);
+        }
+
+        public override IEnumerable<KeyValuePair<long, T>> GetOrderableDynamicPartitions()
+        {
+            var source = GetDynamicPartitions();
+            var subIndex = 0L;
+            foreach (var item in source)
+            {
+                yield return new KeyValuePair<long, T>(subIndex, item);
+                subIndex++;
+            }
+        }
+
+        public override IList<IEnumerator<KeyValuePair<long, T>>> GetOrderablePartitions(int partitionCount)
+        {
+            var list = new List<IEnumerator<KeyValuePair<long, T>>>(partitionCount);
+            var source = GetDynamicPartitions();
+            for (var index = 0; index < partitionCount; index++)
+            {
+                list.Add(Enumerator(index));
+            }
+            return list.AsReadOnly();
+            IEnumerator<KeyValuePair<long, T>> Enumerator(int index)
+            {
+                var subIndex = 0;
+                // ReSharper disable once PossibleMultipleEnumeration
+                foreach (var item in source)
+                {
+                    yield return new KeyValuePair<long, T>(NumericHelper.BuildInt64(index, subIndex), item);
+                    subIndex++;
+                }
+            }
+        }
+
+        public override IList<IEnumerator<T>> GetPartitions(int partitionCount)
+        {
+            var list = new List<IEnumerator<T>>(partitionCount);
+            var source = GetDynamicPartitions();
+            for (var index = 0; index < partitionCount; index++)
+            {
+                list.Add(Enumerator());
+            }
+            return list.AsReadOnly();
+            IEnumerator<T> Enumerator()
+            {
+                // ReSharper disable once PossibleMultipleEnumeration
+                foreach (var item in source)
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+
+#endif
+}
