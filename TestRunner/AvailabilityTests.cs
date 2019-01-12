d90d022d8d7b2323e0beeef3c1b58fd07a7fa38a
Alfonso J. Ramos <theraot@gmail.com>
2019-01-12 00:35:13 -0500

    Using explicit namespaces in AvailabilityTests

--- TestRunner/AvailabilityTests.cs
+++ TestRunner/AvailabilityTests.cs
@@ -1,54 +1,39 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Concurrent;
-using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.Contracts;
-using System.Dynamic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Runtime.ConstrainedExecution;
-using System.Runtime.Serialization;
-using System.Security;
-using System.Security.Permissions;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace TestRunner
+﻿namespace TestRunner
 {
     public class AvailabilityTests
     {
-        public static readonly Type ArrayList = typeof(ArrayList);
-        public static readonly Type BlockingCollection = typeof(BlockingCollection<int>);
-        public static readonly Type CallInfo = typeof(CallInfo);
-        public static readonly Type Closure = typeof(Closure);
-        public static readonly Type CodeAccessPermission = typeof(CodeAccessPermission);
-        public static readonly Type Comparer = typeof(Comparer);
-        public static readonly Type ConcurrentBag = typeof(ConcurrentBag<int>);
-        public static readonly Type ContractAbbreviatorAttribute = typeof(ContractAbbreviatorAttribute);
-        public static readonly Type DisplayAttribute = typeof(DisplayAttribute);
-        public static readonly Type DynamicAttribute = typeof(DynamicAttribute);
-        public static readonly Type EnumerableExecutor = typeof(EnumerableExecutor);
-        public static readonly Type Hashtable = typeof(Hashtable);
-        public static readonly Type HostProtectionAttribute = typeof(HostProtectionAttribute);
-        public static readonly Type HostProtectionResource = typeof(HostProtectionResource);
-        public static readonly Type IFormatterConverter = typeof(IFormatterConverter);
-        public static readonly Type IInvokeOnGetBinder = typeof(IInvokeOnGetBinder);
-        public static readonly Type IUnrestrictedPermission = typeof(IUnrestrictedPermission);
-        public static readonly Type PureAttribute = typeof(PureAttribute);
-        public static readonly Type ReliabilityContractAttribute = typeof(ReliabilityContractAttribute);
-        public static readonly Type RuleCache = typeof(RuleCache<string>);
-        public static readonly Type SecurityAction = typeof(SecurityAction);
-        public static readonly Type SecurityElement = typeof(SecurityElement);
-        public static readonly Type SecurityPermissionAttribute = typeof(SecurityPermissionAttribute);
-        public static readonly Type SerializableAttribute = typeof(SerializableAttribute);
-        public static readonly Type SerializationInfo = typeof(SerializationInfo);
-        public static readonly Type StreamingContext = typeof(StreamingContext);
-        public static readonly Type Task = typeof(Task);
-        public static readonly Type Tuple = typeof(Tuple);
-        public static readonly Type TupleElementNamesAttribute = typeof(TupleElementNamesAttribute);
-        public static readonly Type TupleExtensions = typeof(TupleExtensions);
-        public static readonly Type ValueTask = typeof(ValueTask<int>);
-        public static readonly Type ValueTuple = typeof(ValueTuple);
-        public static readonly Type Volatile = typeof(Volatile);
+        public static readonly global::System.Type ArrayList = typeof(global::System.Collections.ArrayList);
+        public static readonly global::System.Type BlockingCollection = typeof(global::System.Collections.Concurrent.BlockingCollection<int>);
+        public static readonly global::System.Type CallInfo = typeof(global::System.Dynamic.CallInfo);
+        public static readonly global::System.Type Closure = typeof(global::System.Runtime.CompilerServices.Closure);
+        public static readonly global::System.Type CodeAccessPermission = typeof(global::System.Security.CodeAccessPermission);
+        public static readonly global::System.Type Comparer = typeof(global::System.Collections.Comparer);
+        public static readonly global::System.Type ConcurrentBag = typeof(global::System.Collections.Concurrent.ConcurrentBag<int>);
+        public static readonly global::System.Type ContractAbbreviatorAttribute = typeof(global::System.Diagnostics.Contracts.ContractAbbreviatorAttribute);
+        public static readonly global::System.Type DisplayAttribute = typeof(global::System.ComponentModel.DataAnnotations.DisplayAttribute);
+        public static readonly global::System.Type DynamicAttribute = typeof(global::System.Runtime.CompilerServices.DynamicAttribute);
+        public static readonly global::System.Type EnumerableExecutor = typeof(global::System.Linq.EnumerableExecutor);
+        public static readonly global::System.Type Hashtable = typeof(global::System.Collections.Hashtable);
+        public static readonly global::System.Type HostProtectionAttribute = typeof(global::System.Security.Permissions.HostProtectionAttribute);
+        public static readonly global::System.Type HostProtectionResource = typeof(global::System.Security.Permissions.HostProtectionResource);
+        public static readonly global::System.Type IFormatterConverter = typeof(global::System.Runtime.Serialization.IFormatterConverter);
+        public static readonly global::System.Type IInvokeOnGetBinder = typeof(global::System.Dynamic.IInvokeOnGetBinder);
+        public static readonly global::System.Type IUnrestrictedPermission = typeof(global::System.Security.Permissions.IUnrestrictedPermission);
+        public static readonly global::System.Type PureAttribute = typeof(global::System.Diagnostics.Contracts.PureAttribute);
+        public static readonly global::System.Type ReliabilityContractAttribute = typeof(global::System.Runtime.ConstrainedExecution.ReliabilityContractAttribute);
+        public static readonly global::System.Type RuleCache = typeof(global::System.Runtime.CompilerServices.RuleCache<string>);
+        public static readonly global::System.Type SecurityAction = typeof(global::System.Security.Permissions.SecurityAction);
+        public static readonly global::System.Type SecurityElement = typeof(global::System.Security.SecurityElement);
+        public static readonly global::System.Type SecurityPermissionAttribute = typeof(global::System.Security.Permissions.SecurityPermissionAttribute);
+        public static readonly global::System.Type SerializableAttribute = typeof(global::System.SerializableAttribute);
+        public static readonly global::System.Type SerializationInfo = typeof(global::System.Runtime.Serialization.SerializationInfo);
+        public static readonly global::System.Type StreamingContext = typeof(global::System.Runtime.Serialization.StreamingContext);
+        public static readonly global::System.Type Task = typeof(global::System.Threading.Tasks.Task);
+        public static readonly global::System.Type Tuple = typeof(global::System.Tuple);
+        public static readonly global::System.Type TupleElementNamesAttribute = typeof(global::System.Runtime.CompilerServices.TupleElementNamesAttribute);
+        public static readonly global::System.Type TupleExtensions = typeof(global::System.TupleExtensions);
+        public static readonly global::System.Type ValueTask = typeof(global::System.Threading.Tasks.ValueTask<int>);
+        public static readonly global::System.Type ValueTuple = typeof(global::System.ValueTuple);
+        public static readonly global::System.Type Volatile = typeof(global::System.Threading.Volatile);
     }
 }
