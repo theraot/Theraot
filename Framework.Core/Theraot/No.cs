3e380b5058a11f5313f743831f0e2edb97a0a584
Alfonso J. Ramos <theraot@gmail.com>
2019-01-12 09:20:59 -0500

    Suppress the hell out of it

--- Framework.Core/Theraot/No.cs
+++ Framework.Core/Theraot/No.cs
@@ -1,11 +1,16 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Theraot
 {
     public static class No
     {
         [Conditional("THERAOT_NEVER")]
+#pragma warning disable RCS1163 // Unused parameter.
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "arg")]
+        // ReSharper disable once UnusedParameter.Global
         public static void Op<T>(T arg)
+#pragma warning restore RCS1163 // Unused parameter.
         {
             // Empty
         }
