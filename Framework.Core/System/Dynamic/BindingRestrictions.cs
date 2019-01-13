fe07809b4dcdb4bcb7125a3fc8f13caf50cc6226
Alfonso J. Ramos <theraot@gmail.com>
2019-01-13 10:02:09 -0500

    BindingRestrictions review

--- Framework.Core/System/Dynamic/BindingRestrictions.cs
+++ Framework.Core/System/Dynamic/BindingRestrictions.cs
@@ -6,7 +6,6 @@
 
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Dynamic.Utils;
 using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
