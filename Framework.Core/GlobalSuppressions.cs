268142a5d57311ea16ebbbc82cbf1141c15f269a
Alfonso J. Ramos <theraot@gmail.com>
2019-01-13 15:21:59 -0500

    I have decided against global suppressions

--- Framework.Core/GlobalSuppressions.cs
+++
@@ -1,2 +0,0 @@
-﻿// ※ GlobalSuppressions
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancy", "RCS1093:Remove file with no code.", Justification = "This is the suppression file")]
