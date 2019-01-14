bedbe2dfc22c2729b546391ad2b18d9361a674ab
Alfonso J. Ramos <theraot@gmail.com>
2019-01-14 02:41:40 -0500

    .NET Standard reflection solved
    Tested, works. With this solved, only the hiccup of output is a real problem for testing on .NET Standard.

--- TestRunner/TypeDiscoverer.cs
+++ TestRunner/TypeDiscoverer.cs
@@ -1,53 +1,32 @@
-﻿namespace TestRunner
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace TestRunner
 {
     public static class TypeDiscoverer
     {
-        private static global::System.Type[] _types;
+        private static Type[] _types;
 
-        public static global::System.Collections.Generic.IEnumerable<global::System.Type> GetAllTypes()
+        public static IEnumerable<Type> GetAllTypes()
         {
             GetAllTypesPrivate(ref _types);
             return _types;
         }
 
-        private static void GetAllTypesPrivate(ref global::System.Type[] types)
+        private static void GetAllTypesPrivate(ref Type[] types)
         {
             if (_types != null)
             {
                 return;
             }
+            var assembly = typeof(TypeDiscoverer).GetTypeInfo().Assembly;
 #if LESSTHAN_NETSTANDARD15
-            types = new []
-			{
-				typeof(Assert),
-				typeof(AvailabilityTests),
-				typeof(Console),
-				typeof(DataGenerator),
-				typeof(NumericGenerator),
-				typeof(SmallPositiveNumericGenerator),
-				typeof(StringGenerator),
-				typeof(DataGeneratorAttribute),
-				typeof(UseGeneratorAttribute),
-				typeof(InterfaceTests),
-				typeof(Program),
-				typeof(AssertionFailedException),
-				typeof(CategoryAttribute),
-				typeof(TestAttribute),
-				typeof(TestFixtureAttribute),
-				typeof(TypeDiscoverer),
-				typeof(System.IO.StreamExtensionsTest),
-				typeof(System.Linq.EnumerableTest),
-				typeof(System.Reflection.IntrospectionExtensionsTest),
-				typeof(System.Threading.SemaphoreSlimTestsEx),
-				typeof(System.Threading.TaskExFromTest),
-				typeof(System.Threading.ThreadPoolTest),
-				typeof(System.Threading.ThreadTest),
-				typeof(System.Threading.TimerTest),
-				typeof(System.Collections.Concurrent.BlockingCollectionTest),
-			};
+            types = assembly.DefinedTypes.Select(typeInfo => typeInfo.AsType()).ToArray();
 #else
-            types = global::System.Reflection.IntrospectionExtensions.GetTypeInfo(typeof(TypeDiscoverer)).Assembly.GetTypes();
+            types = assembly.GetTypes();
 #endif
-		}
+        }
     }
 }
