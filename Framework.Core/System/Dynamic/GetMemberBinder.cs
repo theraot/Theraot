73b9166aed1bbd8221557445048136bfe8ec64b7
Alfonso J. Ramos <theraot@gmail.com>
2019-01-15 08:05:16 -0500

    GetMemberBinder review

--- Framework.Core/System/Dynamic/GetMemberBinder.cs
+++ Framework.Core/System/Dynamic/GetMemberBinder.cs
@@ -1,5 +1,9 @@
 #if LESSTHAN_NET35
 
+#pragma warning disable RCS1193 // Overriding member cannot change 'params' modifier.
+// ReSharper disable RedundantParams
+// ReSharper disable UnusedParameter.Global
+
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
