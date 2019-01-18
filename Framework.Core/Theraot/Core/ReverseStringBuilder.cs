622e22e7e1dc115f9f48438902d1d047f11664b9
Alfonso J. Ramos <theraot@gmail.com>
2019-01-18 18:56:47 -0500

    ReverseStringBuilder reworked to use ArrayReservoir

--- Framework.Core/Theraot/Core/ReverseStringBuilder.cs
+++ Framework.Core/Theraot/Core/ReverseStringBuilder.cs
@@ -1,32 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Theraot.Collections.ThreadSafe;
+using Theraot.Threading;
 
 namespace Theraot.Core
 {
     public class ReverseStringBuilder : IEnumerable<char>
     {
-        private readonly char[] _buffer;
+        private char[] _buffer;
+        private readonly int _capacity;
         private int _start;
 
         public ReverseStringBuilder(int capacity)
         {
-            try
-            {
-                _buffer = new char[capacity];
-                _start = _buffer.Length;
-            }
-            catch
+            if (capacity < 0)
             {
                 throw new ArgumentOutOfRangeException(nameof(capacity));
             }
+            _buffer = ArrayReservoir<char>.GetArray(capacity);
+            _capacity = capacity;
+            _start = capacity;
         }
 
-        public int Length => _buffer.Length - _start;
+        ~ReverseStringBuilder()
+        {
+            // Assume anything could have been set to null, start no sync operation, this could be running during DomainUnload
+            if (!GCMonitor.FinalizingForUnload)
+            {
+                var buffer = _buffer;
+                if (buffer != null)
+                {
+                    ArrayReservoir<char>.DonateArray(buffer);
+                    _buffer = null;
+                }
+            }
+        }
+
+        public int Length => _capacity - _start;
 
         public IEnumerator<char> GetEnumerator()
         {
-            for (var position = _buffer.Length - 1; position >= 0; position--)
+            for (var position = _capacity - 1; position >= 0; position--)
             {
                 yield return _buffer[position];
             }
@@ -51,27 +66,31 @@ namespace Theraot.Core
 
         public override string ToString()
         {
-            return new string(_buffer, _start, _buffer.Length - _start);
+            return new string(_buffer, _start, _capacity - _start);
         }
 
         public string ToString(int maxLength)
         {
-            var length = _buffer.Length - _start;
+            var length = _capacity - _start;
             if (length > maxLength)
             {
                 length = maxLength;
             }
-            return new string(_buffer, _buffer.Length - length, length);
+            return new string(_buffer, _capacity - length, length);
         }
 
         public string ToString(int backIndex, int maxLength)
         {
-            var length = _buffer.Length - _start;
+            if (backIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(backIndex));
+            }
+            var length = _capacity - _start;
             if (length > maxLength)
             {
                 length = maxLength;
             }
-            return new string(_buffer, _buffer.Length - backIndex, length);
+            return new string(_buffer, _capacity - backIndex, length);
         }
     }
 }
