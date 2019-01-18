87560cf1e31c82e14307df340a456214c32b98f7
Alfonso J. Ramos <theraot@gmail.com>
2019-01-17 22:16:17 -0500

    ParallelMergeOptions review

--- Framework.Core/System/Linq/ParallelMergeOptions.cs
+++ Framework.Core/System/Linq/ParallelMergeOptions.cs
@@ -1,13 +1,15 @@
 ﻿#if LESSTHAN_NET40
 
+#pragma warning disable CA1717 // Only FlagsAttribute enums should have plural names
+
 namespace System.Linq
 {
     public enum ParallelMergeOptions
     {
         Default = 0,
-        NotBuffered,
-        AutoBuffered,
-        FullyBuffered
+        NotBuffered = 1,
+        AutoBuffered = 2,
+        FullyBuffered = 3
     }
 }
 
