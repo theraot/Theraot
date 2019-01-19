215db9ba8159524e6951d7f7f4c09682c7dd39a8
Alfonso J. Ramos <theraot@gmail.com>
2019-01-18 22:24:13 -0500

    New DebugEx.Fail

---
+++ Framework.Core/System/Diagnostics/DebugEx.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace System.Diagnostics
+{
+    public static class DebugEx
+    {
+        [Conditional("DEBUG")]
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void Fail(string message)
+        {
+#if LESSTHAN_NETSTANDARD13
+            Debug.Assert(false, message);
+#else
+            Debug.Fail(message);
+#endif
+        }
+    }
+}
