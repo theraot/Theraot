170b7b5c25c353322a02e00fccb1283b991508e3
Alfonso J. Ramos <theraot@gmail.com>
2019-01-21 06:49:42 -0500

    MethodInfoTheraotExtensions clean up

--- Framework.Core/System/Reflection/MethodInfoTheraotExtensions.cs
+++ Framework.Core/System/Reflection/MethodInfoTheraotExtensions.cs
@@ -1,38 +1,41 @@
 ﻿#if LESSTHAN_NET45
 
+using System.Reflection.Emit;
+
 namespace System.Reflection
 {
     public static class MethodInfoTheraotExtensions
     {
         /// <summary>
-        /// Creates a closed delegate for the given (dynamic)method.
+        ///     Creates a closed delegate for the given (dynamic)method.
         /// </summary>
         /// <param name="methodInfo">The MethodInfo for the target method.</param>
         /// <param name="delegateType">Delegate type with a matching signature.</param>
         public static Delegate CreateDelegate(this MethodInfo methodInfo, Type delegateType)
         {
-            if (methodInfo is Emit.DynamicMethod dynamicMethod)
+            if (methodInfo is DynamicMethod dynamicMethod)
             {
                 return dynamicMethod.CreateDelegate(delegateType);
             }
+
             return Delegate.CreateDelegate(delegateType, methodInfo);
         }
 
         /// <summary>
-        /// Creates a closed delegate for the given (dynamic)method.
+        ///     Creates a closed delegate for the given (dynamic)method.
         /// </summary>
         /// <param name="methodInfo">The MethodInfo for the target method.</param>
         /// <param name="delegateType">Delegate type with a matching signature.</param>
         /// <param name="target">The object to which the delegate is bound, or null to treat method as static.</param>
         public static Delegate CreateDelegate(this MethodInfo methodInfo, Type delegateType, object target)
         {
-            if (methodInfo is Emit.DynamicMethod dynamicMethod)
+            if (methodInfo is DynamicMethod dynamicMethod)
             {
                 return dynamicMethod.CreateDelegate(delegateType, target);
             }
+
             return Delegate.CreateDelegate(delegateType, target, methodInfo);
         }
-
-   }
+    }
 }
 #endif
