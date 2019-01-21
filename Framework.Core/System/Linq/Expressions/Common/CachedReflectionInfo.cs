1ef1dceb858a8fb6ef4487fa556160bee66f2766
Alfonso J. Ramos <theraot@gmail.com>
2019-01-20 23:29:56 -0500

    Moved all CachedReflectionInfo to a single file

--- Framework.Core/System/Linq/Expressions/Common/CachedReflectionInfo.cs
+++ Framework.Core/System/Linq/Expressions/Common/CachedReflectionInfo.cs
@@ -5,6 +5,7 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
@@ -202,6 +203,80 @@ namespace System.Linq.Expressions
         public static MethodInfo TypeGetTypeFromHandle =>
                                   _typeGetTypeFromHandle ??
                                  (_typeGetTypeFromHandle = typeof(Type).GetMethod(nameof(Type.GetTypeFromHandle)));
+    }
+
+    internal static partial class CachedReflectionInfo
+    {
+        private static MethodInfo _dynamicObjectTryBinaryOperation;
+        private static MethodInfo _dynamicObjectTryConvert;
+        private static MethodInfo _dynamicObjectTryCreateInstance;
+        private static MethodInfo _dynamicObjectTryDeleteIndex;
+        private static MethodInfo _dynamicObjectTryDeleteMember;
+        private static MethodInfo _dynamicObjectTryGetIndex;
+        private static MethodInfo _dynamicObjectTryGetMember;
+        private static MethodInfo _dynamicObjectTryInvoke;
+        private static MethodInfo _dynamicObjectTryInvokeMember;
+        private static MethodInfo _dynamicObjectTrySetIndex;
+        private static MethodInfo _dynamicObjectTrySetMember;
+        private static MethodInfo _dynamicObjectTryUnaryOperation;
+        private static ConstructorInfo _invalidCastExceptionCtorString;
+        private static MethodInfo _stringFormatStringObjectArray;
+
+        public static MethodInfo DynamicObjectTryBinaryOperation =>
+            _dynamicObjectTryBinaryOperation ??
+            (_dynamicObjectTryBinaryOperation = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryBinaryOperation)));
+
+        public static MethodInfo DynamicObjectTryConvert =>
+            _dynamicObjectTryConvert ??
+            (_dynamicObjectTryConvert = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryConvert)));
+
+        public static MethodInfo DynamicObjectTryCreateInstance =>
+            _dynamicObjectTryCreateInstance ??
+            (_dynamicObjectTryCreateInstance = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryCreateInstance)));
+
+        public static MethodInfo DynamicObjectTryDeleteIndex =>
+            _dynamicObjectTryDeleteIndex ??
+            (_dynamicObjectTryDeleteIndex = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryDeleteIndex)));
+
+        public static MethodInfo DynamicObjectTryDeleteMember =>
+            _dynamicObjectTryDeleteMember ??
+            (_dynamicObjectTryDeleteMember = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryDeleteMember)));
+
+        public static MethodInfo DynamicObjectTryGetIndex =>
+            _dynamicObjectTryGetIndex ??
+            (_dynamicObjectTryGetIndex = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryGetIndex)));
+
+        public static MethodInfo DynamicObjectTryGetMember =>
+            _dynamicObjectTryGetMember ??
+            (_dynamicObjectTryGetMember = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryGetMember)));
+
+        public static MethodInfo DynamicObjectTryInvoke =>
+            _dynamicObjectTryInvoke ??
+            (_dynamicObjectTryInvoke = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryInvoke)));
+
+        public static MethodInfo DynamicObjectTryInvokeMember =>
+            _dynamicObjectTryInvokeMember ??
+            (_dynamicObjectTryInvokeMember = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryInvokeMember)));
+
+        public static MethodInfo DynamicObjectTrySetIndex =>
+            _dynamicObjectTrySetIndex ??
+            (_dynamicObjectTrySetIndex = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TrySetIndex)));
+
+        public static MethodInfo DynamicObjectTrySetMember =>
+            _dynamicObjectTrySetMember ??
+            (_dynamicObjectTrySetMember = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TrySetMember)));
+
+        public static MethodInfo DynamicObjectTryUnaryOperation =>
+            _dynamicObjectTryUnaryOperation ??
+            (_dynamicObjectTryUnaryOperation = typeof(DynamicObject).GetMethod(nameof(DynamicObject.TryUnaryOperation)));
+
+        public static ConstructorInfo InvalidCastExceptionCtorString =>
+            _invalidCastExceptionCtorString ??
+            (_invalidCastExceptionCtorString = typeof(InvalidCastException).GetConstructor(new[] {typeof(string)}));
+
+        public static MethodInfo StringFormatStringObjectArray =>
+            _stringFormatStringObjectArray ??
+            (_stringFormatStringObjectArray = typeof(string).GetMethod(nameof(string.Format), new[] {typeof(string), typeof(object[])}));
     }
 }
 
