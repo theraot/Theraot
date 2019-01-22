5b67ca6d7991d600182f164a57cba9fbe94d4cdc
Alfonso J. Ramos <theraot@gmail.com>
2019-01-22 15:32:09 -0500

    Test cleanup (haven't rerun yet)

--- TestRunner/System/Collections/Concurrent/BlockingCollectionTest.cs
+++ TestRunner/System/Collections/Concurrent/BlockingCollectionTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Threading;
+using Theraot;
 
 namespace TestRunner.System.Collections.Concurrent
 {
@@ -11,68 +12,77 @@ namespace TestRunner.System.Collections.Concurrent
         [Test]
         public static void TryTakeFromEmptyBlockingCollectionShouldNotThrow()
         {
-            var a = new BlockingCollection<int>();
-            a.TryTake(out var _);
+            using (var a = new BlockingCollection<int>())
+            {
+                a.TryTake(out _);
+            }
         }
 
         [Test]
         public static void TryTakeFromEmptyBlockingCollectionWithTimeoutShouldWait()
         {
-            var a = new BlockingCollection<int>();
-            var stopWatch = new Stopwatch();
-            stopWatch.Start();
-            a.TryTake(out var _, 200);
-            stopWatch.Stop();
-            Assert.IsTrue(stopWatch.ElapsedMilliseconds >= 200);
+            using (var a = new BlockingCollection<int>())
+            {
+                var stopWatch = new Stopwatch();
+                stopWatch.Start();
+                a.TryTake(out _, 200);
+                stopWatch.Stop();
+                Assert.IsTrue(stopWatch.ElapsedMilliseconds >= 200);
+            }
         }
-
-        private delegate void TestAction();
 
         private static void GetConsumingEnumerableTestImpl(CancellationToken? cancellationToken)
         {
             var called = 0;
-            var ev = new ManualResetEventSlim(false);
-
-            var bc = new BlockingCollection<TestAction>();
-
-            var t = new Thread(() => {
-                try
+            var ev = new ManualResetEventSlim[1];
+            using (ev[0] = new ManualResetEventSlim(false))
+            {
+                var bc = new BlockingCollection<Action>[1];
+                using (bc[0] = new BlockingCollection<Action>())
                 {
-                    ev.Set();
-
-                    if (cancellationToken.HasValue)
-                    {
-                        foreach (var e in bc.GetConsumingEnumerable(cancellationToken.Value))
+                    var t = new Thread
+                    (
+                        () =>
                         {
-                            e();
+                            try
+                            {
+                                ev[0].Set();
+
+                                if (cancellationToken.HasValue)
+                                {
+                                    foreach (var e in bc[0].GetConsumingEnumerable(cancellationToken.Value))
+                                    {
+                                        e();
+                                    }
+                                }
+                                else
+                                {
+                                    foreach (var e in bc[0].GetConsumingEnumerable())
+                                    {
+                                        e();
+                                    }
+                                }
+                            }
+                            catch (OperationCanceledException exception)
+                            {
+                                No.Op(exception);
+                            }
                         }
-                    }
-                    else
-                    {
-                        foreach (var e in bc.GetConsumingEnumerable())
-                        {
-                            e();
-                        }
-                    }
+                    );
+                    t.Start();
+
+                    // Make sure thread is running
+                    ev[0].Wait();
+                    Thread.Sleep(200);
+
+                    bc[0].Add(() => called++);
+                    bc[0].Add(() => called++);
+
+                    bc[0].CompleteAdding();
+                    t.Join();
+                    Assert.AreEqual(2, called);
                 }
-                catch (OperationCanceledException)
-                {
-
-                }
-            });
-            t.Start();
-
-            // Make sure thread is running
-            ev.Wait();
-            Thread.Sleep(200);
-
-            bc.Add(() => called++);
-            bc.Add(() => called++);
-
-            bc.CompleteAdding();
-            t.Join();
-
-            Assert.AreEqual(2, called);
+            }
         }
 
         [Test]
@@ -90,8 +100,10 @@ namespace TestRunner.System.Collections.Concurrent
         [Test]
         public static void GetConsumingEnumerableShouldBlockWithCancellationTokenNotNone()
         {
-            var cancellationTokenSource = new CancellationTokenSource();
-            GetConsumingEnumerableTestImpl(cancellationTokenSource.Token);
+            using (var cancellationTokenSource = new CancellationTokenSource())
+            {
+                GetConsumingEnumerableTestImpl(cancellationTokenSource.Token);
+            }
         }
     }
 }
