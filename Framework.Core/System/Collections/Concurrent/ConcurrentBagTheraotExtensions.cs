f0a34b35721d3368621649a7d13a6904ce036040
Alfonso J. Ramos <theraot@gmail.com>
2019-01-26 20:48:01 -0500

    ConcurrentBagTheraotExtensions & ConcurrentQueueTheraotExtensions

---
+++ Framework.Core/System/Collections/Concurrent/ConcurrentBagTheraotExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if GREATERTHAN_NET35 || LESSTHAN_NETCOREAPP20 || TARGETS_NETSTANDARD
+
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Concurrent
+{
+    public static class ConcurrentBagTheraotExtensions
+    {
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void Clear<T>(this ConcurrentBag<T> concurrentBag)
+        {
+            while (concurrentBag.TryTake(out _))
+            {
+                // Empty
+            }
+        }
+    }
+}
+
+#endif
---
+++ Framework.Core/System/Collections/Concurrent/ConcurrentQueueTheraotExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if GREATERTHAN_NET35 || LESSTHAN_NETCOREAPP20 || TARGETS_NETSTANDARD
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Concurrent
+{
+    public static class ConcurrentQueueTheraotExtensions
+    {
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void Clear<T>(this ConcurrentQueue<T> concurrentQueue)
+        {
+            while (concurrentQueue.TryDequeue(out _))
+            {
+                // Empty
+            }
+            concurrentQueue.Clear();
+        }
+    }
+}
+
+#endif
