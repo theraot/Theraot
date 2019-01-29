7bc2654267afb01b29e8532f38d76e3a191c34ee
Alfonso J. Ramos <theraot@gmail.com>
2019-01-29 02:36:56 -0500

    Added task ContinueWith overloads with state

--- Framework.Core/System/Threading/Tasks/TaskTheraotExtensions.cs
+++ Framework.Core/System/Threading/Tasks/TaskTheraotExtensions.cs
@@ -4,30 +4,26 @@ using System.Runtime.CompilerServices;
 namespace System.Threading.Tasks
 {
     /// <summary>
-    /// Provides extension methods for threading-related types.
+    ///     Provides extension methods for threading-related types.
     /// </summary>
-    ///
     /// <summary>
-    /// Asynchronous wrappers for .NET Framework operations.
+    ///     Asynchronous wrappers for .NET Framework operations.
     /// </summary>
-    ///
     /// <summary>
-    /// Provides extension methods for threading-related types.
+    ///     Provides extension methods for threading-related types.
     /// </summary>
-    ///
-    /// <remarks>
-    /// AsyncCtpThreadingExtensions is a placeholder.
-    /// </remarks>
-    public static class TaskTheraotExtensions
+    public static partial class TaskTheraotExtensions
     {
         /// <summary>
-        /// Creates and configures an awaitable object for awaiting the specified task.
+        ///     Creates and configures an awaitable object for awaiting the specified task.
         /// </summary>
         /// <param name="task">The task to be awaited.</param>
-        /// <param name="continueOnCapturedContext">true to automatic marshal back to the original call site's current SynchronizationContext
-        ///             or TaskScheduler; otherwise, false.</param>
+        /// <param name="continueOnCapturedContext">
+        ///     true to automatic marshal back to the original call site's current SynchronizationContext
+        ///     or TaskScheduler; otherwise, false.
+        /// </param>
         /// <returns>
-        /// The instance to be awaited.
+        ///     The instance to be awaited.
         /// </returns>
         public static ConfiguredTaskAwaitable<TResult> ConfigureAwait<TResult>(this Task<TResult> task,
             bool continueOnCapturedContext)
@@ -41,13 +37,15 @@ namespace System.Threading.Tasks
         }
 
         /// <summary>
-        /// Creates and configures an awaitable object for awaiting the specified task.
+        ///     Creates and configures an awaitable object for awaiting the specified task.
         /// </summary>
         /// <param name="task">The task to be awaited.</param>
-        /// <param name="continueOnCapturedContext">true to automatic marshal back to the original call site's current SynchronizationContext
-        ///             or TaskScheduler; otherwise, false.</param>
+        /// <param name="continueOnCapturedContext">
+        ///     true to automatic marshal back to the original call site's current SynchronizationContext
+        ///     or TaskScheduler; otherwise, false.
+        /// </param>
         /// <returns>
-        /// The instance to be awaited.
+        ///     The instance to be awaited.
         /// </returns>
         public static ConfiguredTaskAwaitable ConfigureAwait(this Task task, bool continueOnCapturedContext)
         {
@@ -60,11 +58,11 @@ namespace System.Threading.Tasks
         }
 
         /// <summary>
-        /// Gets an awaiter used to await this <see cref="T:System.Threading.Tasks.Task"/>.
+        ///     Gets an awaiter used to await this <see cref="T:System.Threading.Tasks.Task" />.
         /// </summary>
         /// <param name="task">The task to await.</param>
         /// <returns>
-        /// An awaiter instance.
+        ///     An awaiter instance.
         /// </returns>
         public static TaskAwaiter GetAwaiter(this Task task)
         {
@@ -77,12 +75,12 @@ namespace System.Threading.Tasks
         }
 
         /// <summary>
-        /// Gets an awaiter used to await this <see cref="T:System.Threading.Tasks.Task"/>.
+        ///     Gets an awaiter used to await this <see cref="T:System.Threading.Tasks.Task" />.
         /// </summary>
         /// <typeparam name="TResult">Specifies the type of data returned by the task.</typeparam>
         /// <param name="task">The task to await.</param>
         /// <returns>
-        /// An awaiter instance.
+        ///     An awaiter instance.
         /// </returns>
         public static TaskAwaiter<TResult> GetAwaiter<TResult>(this Task<TResult> task)
         {
@@ -92,6 +90,293 @@ namespace System.Threading.Tasks
             }
 
             return new TaskAwaiter<TResult>(task);
+        }
+    }
+
+    public static partial class TaskTheraotExtensions
+    {
+        public static Task ContinueWith(this Task task, Action<Task, object> continuationAction, object state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions, TaskScheduler scheduler)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                cancellationToken,
+                continuationOptions,
+                scheduler
+            );
+        }
+
+        public static Task ContinueWith(this Task task, Action<Task, object> continuationAction, object state, TaskScheduler scheduler)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                scheduler
+            );
+        }
+
+        public static Task ContinueWith(this Task task, Action<Task, object> continuationAction, object state, CancellationToken cancellationToken)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                cancellationToken
+            );
+        }
+
+        public static Task ContinueWith(this Task task, Action<Task, object> continuationAction, object state, TaskContinuationOptions continuationOptions)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                continuationOptions
+            );
+        }
+
+        public static Task ContinueWith(this Task task, Action<Task, object> continuationAction, object state)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state)
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, object, TResult> continuationFunction, object state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions, TaskScheduler scheduler)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                cancellationToken,
+                continuationOptions,
+                scheduler
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, object, TResult> continuationFunction, object state, TaskScheduler scheduler)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                scheduler
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, object, TResult> continuationFunction, object state, CancellationToken cancellationToken)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                cancellationToken
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, object, TResult> continuationFunction, object state, TaskContinuationOptions continuationOptions)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                continuationOptions
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, object, TResult> continuationFunction, object state)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state)
+            );
+        }
+
+        public static Task ContinueWith<TResult>(this Task<TResult> task, Action<Task<TResult>, object> continuationAction, object state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions, TaskScheduler scheduler)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                cancellationToken,
+                continuationOptions,
+                scheduler
+            );
+        }
+
+        public static Task ContinueWith<TResult>(this Task<TResult> task, Action<Task<TResult>, object> continuationAction, object state, TaskScheduler scheduler)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                scheduler
+            );
+        }
+
+        public static Task ContinueWith<TResult>(this Task<TResult> task, Action<Task<TResult>, object> continuationAction, object state, CancellationToken cancellationToken)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                cancellationToken
+            );
+        }
+
+        public static Task ContinueWith<TResult>(this Task<TResult> task, Action<Task<TResult>, object> continuationAction, object state, TaskContinuationOptions continuationOptions)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state),
+                continuationOptions
+            );
+        }
+
+        public static Task ContinueWith<TResult>(this Task<TResult> task, Action<Task<TResult>, object> continuationAction, object state)
+        {
+            if (continuationAction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationAction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationAction(t, state)
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task<TResult> task, Func<Task<TResult>, object, TResult> continuationFunction, object state, CancellationToken cancellationToken, TaskContinuationOptions continuationOptions, TaskScheduler scheduler)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                cancellationToken,
+                continuationOptions,
+                scheduler
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task<TResult> task, Func<Task<TResult>, object, TResult> continuationFunction, object state, TaskScheduler scheduler)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                scheduler
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task<TResult> task, Func<Task<TResult>, object, TResult> continuationFunction, object state, CancellationToken cancellationToken)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                cancellationToken
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task<TResult> task, Func<Task<TResult>, object, TResult> continuationFunction, object state, TaskContinuationOptions continuationOptions)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state),
+                continuationOptions
+            );
+        }
+
+        public static Task<TResult> ContinueWith<TResult>(this Task<TResult> task, Func<Task<TResult>, object, TResult> continuationFunction, object state)
+        {
+            if (continuationFunction == null)
+            {
+                throw new ArgumentNullException(nameof(continuationFunction));
+            }
+
+            return task.ContinueWith
+            (
+                t => continuationFunction(t, state)
+            );
         }
     }
 }
