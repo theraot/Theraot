837b529a251ba780516e87dea50a4a3850c065a0
Alfonso J. Ramos <theraot@gmail.com>
2019-02-08 16:59:16 -0500

    HashSetTestEx

--- Tests/SystemTests/CollectionsTests/GenericTests/HashSetTestEx.cs
+++ Tests/SystemTests/CollectionsTests/GenericTests/HashSetTestEx.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 
-namespace MonoTests.System.Collections.Generic
+namespace SystemTests.CollectionsTests.ConcurrentTests
 {
     [TestFixture]
     internal class HashSetTestEx
