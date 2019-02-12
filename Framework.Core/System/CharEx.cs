7d9d92f2da26a9f664be2d9c69d5b66deeb46d7e
Alfonso J. Ramos <theraot@gmail.com>
2019-02-12 14:27:50 -0500

    Added CharEx #87

---
+++ Framework.Core/System/CharEx.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace System
+{
+    public static class CharEx
+    {
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static char ToUpper(char c, CultureInfo culture)
+        {
+#if TARGETS_NET
+            return char.ToUpper(c, culture);
+#else
+            if (culture == null)
+            {
+                throw new ArgumentNullException(nameof(culture));
+            }
+            return culture.TextInfo.ToUpper(c);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static char ToLower(char c, CultureInfo culture)
+        {
+#if TARGETS_NET
+            return char.ToLower(c, culture);
+#else
+            if (culture == null)
+            {
+                throw new ArgumentNullException(nameof(culture));
+            }
+            return culture.TextInfo.ToLower(c);
+#endif
+        }
+    }
+}
