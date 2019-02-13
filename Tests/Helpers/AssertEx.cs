7ef781e4ccb18d3953292cc3ed5aef89e735081d
Alfonso J. Ramos <theraot@gmail.com>
2019-02-13 10:25:03 -0500

    Redundant using

--- Tests/Helpers/AssertEx.cs
+++ Tests/Helpers/AssertEx.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Theraot;
 using Theraot.Collections;
 using Theraot.Reflection;
 
