226d430b415de9b0b3f3b3c4249f943c55c91b59
Alfonso J. Ramos <theraot@gmail.com>
2019-08-02 19:21:38 -0500

    HostProtectionAttribute fix

--- Framework.Core/System/Security/Permissions/HostProtectionAttribute.cs
+++ Framework.Core/System/Security/Permissions/HostProtectionAttribute.cs
@@ -1,4 +1,4 @@
-﻿#if LESSTHAN_NETCOREAPP20 || TARGETS_NETSTANDARD
+﻿#if LESSTHAN_NETCOREAPP20 || LESSTHAN_NETSTANDARD20
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
