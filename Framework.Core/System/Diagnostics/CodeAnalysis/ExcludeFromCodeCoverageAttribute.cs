47f0129616a16958edfc8c6c96c6b224cfa796ad
Alfonso J. Ramos <theraot@gmail.com>
2019-09-15 10:28:27 -0500

    Added ExcludeFromCodeCoverageAttribute

---
+++ Framework.Core/System/Diagnostics/CodeAnalysis/ExcludeFromCodeCoverageAttribute.cs
@@ -0,0 +1,12 @@
+﻿#if LESSTHAN_NET40 || LESSTHAN_NETCOREAPP20 || LESSTHAN_NETSTANDARD20
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Constructor | AttributeTargets.Event | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Struct, Inherited = false)]
+    public sealed class ExcludeFromCodeCoverageAttribute : Attribute
+    {
+        // Empty
+    }
+}
+
+#endif
