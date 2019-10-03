289582d6319c0a523e5a1a65e282168ae68bb13e
Theraot <Theraot@gmail.com>
2019-10-03 14:55:10 -0500

    ComparerEx documentation comments

--- Framework.Core/System/Collections/Generic/ComparerEx.cs
+++ Framework.Core/System/Collections/Generic/ComparerEx.cs
@@ -8,14 +8,25 @@ using Theraot.Collections.Specialized;
 
 namespace System.Collections.Generic
 {
+    /// <summary>
+    /// Defines the <see cref="ComparerEx" />
+    /// </summary>
     public static class ComparerEx
     {
+        /// <summary>
+        /// Creates a comparer by using the specified comparison.
+        /// </summary>
+        /// <typeparam name="T">The type of items compared.</typeparam>
+        /// <param name="comparison">The comparison<see cref="Comparison{T}"/></param>
+        /// <returns>The new <see cref="Comparer{T}"/>.</returns>
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         public static Comparer<T> Create<T>(Comparison<T> comparison)
         {
 #if LESSTHAN_NET45
+
             return new CustomComparer<T>(comparison);
 #else
+
             return Comparer<T>.Create(comparison);
 #endif
         }
