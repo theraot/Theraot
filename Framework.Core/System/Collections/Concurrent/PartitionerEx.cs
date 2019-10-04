8a2f16324d7f18dd4dc121e844d3ec082443e90b
Theraot <Theraot@gmail.com>
2019-10-03 19:15:14 -0500

    Removed unnecessary usings

--- Framework.Core/System/Collections/Concurrent/PartitionerEx.cs
+++ Framework.Core/System/Collections/Concurrent/PartitionerEx.cs
@@ -1,13 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
-#if LESSTHAN_NET45 || NETSTANDARD1_0
-
-using Theraot.Collections;
-using Theraot.Core;
-
-#endif
-
 namespace System.Collections.Concurrent
 {
     public static class PartitionerEx
