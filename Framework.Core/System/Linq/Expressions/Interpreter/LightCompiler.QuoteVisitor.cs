4745df915cb5fa481feb69d45e2ea4cd216762c4
Theraot <Theraot@gmail.com>
2019-10-17 03:55:44 -0500

    QuoteVisitor taken to a separate file

---
+++ Framework.Core/System/Linq/Expressions/Interpreter/LightCompiler.QuoteVisitor.cs
@@ -0,0 +1,119 @@
+﻿#if LESSTHAN_NET35
+
+#pragma warning disable CC0021 // Use nameof
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace System.Linq.Expressions.Interpreter
+{
+    internal sealed partial class LightCompiler
+    {
+        private sealed class QuoteVisitor : ExpressionVisitor
+        {
+            public readonly HashSet<ParameterExpression> HoistedParameters = new HashSet<ParameterExpression>();
+            private readonly Dictionary<ParameterExpression, int> _definedParameters = new Dictionary<ParameterExpression, int>();
+
+            protected internal override Expression VisitBlock(BlockExpression node)
+            {
+                PushParameters(node.Variables);
+
+                base.VisitBlock(node);
+
+                PopParameters(node.Variables);
+
+                return node;
+            }
+
+            protected internal override Expression VisitLambda<T>(Expression<T> node)
+            {
+                IEnumerable<ParameterExpression> parameters = ArrayEx.Empty<ParameterExpression>();
+
+                var count = node.ParameterCount;
+
+                if (count > 0)
+                {
+                    var parameterList = new List<ParameterExpression>(count);
+
+                    for (var i = 0; i < count; i++)
+                    {
+                        parameterList.Add(node.GetParameter(i));
+                    }
+
+                    parameters = parameterList;
+                }
+
+                PushParameters(parameters);
+
+                base.VisitLambda(node);
+
+                PopParameters(parameters);
+
+                return node;
+            }
+
+            protected internal override Expression VisitParameter(ParameterExpression node)
+            {
+                if (!_definedParameters.ContainsKey(node))
+                {
+                    HoistedParameters.Add(node);
+                }
+
+                return node;
+            }
+
+            protected override CatchBlock VisitCatchBlock(CatchBlock node)
+            {
+                if (node.Variable != null)
+                {
+                    PushParameters(new[] { node.Variable });
+                }
+
+                Visit(node.Body);
+                Visit(node.Filter);
+                if (node.Variable != null)
+                {
+                    PopParameters(new[] { node.Variable });
+                }
+
+                return node;
+            }
+
+            private void PopParameters(IEnumerable<ParameterExpression> parameters)
+            {
+                foreach (var param in parameters)
+                {
+                    var count = _definedParameters[param];
+                    if (count == 0)
+                    {
+                        _definedParameters.Remove(param);
+                    }
+                    else
+                    {
+                        _definedParameters[param] = count - 1;
+                    }
+                }
+            }
+
+            private void PushParameters(IEnumerable<ParameterExpression> parameters)
+            {
+                foreach (var param in parameters)
+                {
+                    if (_definedParameters.TryGetValue(param, out var count))
+                    {
+                        _definedParameters[param] = count + 1;
+                    }
+                    else
+                    {
+                        _definedParameters[param] = 1;
+                    }
+                }
+            }
+        }
+    }
+}
+
+#endif
--- Framework.Core/System/Linq/Expressions/Interpreter/LightCompiler.cs
+++ Framework.Core/System/Linq/Expressions/Interpreter/LightCompiler.cs
@@ -17,7 +17,7 @@ using AstUtils = System.Linq.Expressions.Utils;
 
 namespace System.Linq.Expressions.Interpreter
 {
-    internal sealed class LightCompiler
+    internal sealed partial class LightCompiler
     {
         private static readonly LocalDefinition[] _emptyLocals = ArrayEx.Empty<LocalDefinition>();
         private readonly List<DebugInfo> _debugInfos = new List<DebugInfo>();
@@ -2967,108 +2967,6 @@ namespace System.Linq.Expressions.Interpreter
                     }
 
                     return (labelBlock, LabelScopeKind.Expression, null);
-            }
-        }
-
-        private sealed class QuoteVisitor : ExpressionVisitor
-        {
-            public readonly HashSet<ParameterExpression> HoistedParameters = new HashSet<ParameterExpression>();
-            private readonly Dictionary<ParameterExpression, int> _definedParameters = new Dictionary<ParameterExpression, int>();
-
-            protected internal override Expression VisitBlock(BlockExpression node)
-            {
-                PushParameters(node.Variables);
-
-                base.VisitBlock(node);
-
-                PopParameters(node.Variables);
-
-                return node;
-            }
-
-            protected internal override Expression VisitLambda<T>(Expression<T> node)
-            {
-                IEnumerable<ParameterExpression> parameters = ArrayEx.Empty<ParameterExpression>();
-
-                var count = node.ParameterCount;
-
-                if (count > 0)
-                {
-                    var parameterList = new List<ParameterExpression>(count);
-
-                    for (var i = 0; i < count; i++)
-                    {
-                        parameterList.Add(node.GetParameter(i));
-                    }
-
-                    parameters = parameterList;
-                }
-
-                PushParameters(parameters);
-
-                base.VisitLambda(node);
-
-                PopParameters(parameters);
-
-                return node;
-            }
-
-            protected internal override Expression VisitParameter(ParameterExpression node)
-            {
-                if (!_definedParameters.ContainsKey(node))
-                {
-                    HoistedParameters.Add(node);
-                }
-
-                return node;
-            }
-
-            protected override CatchBlock VisitCatchBlock(CatchBlock node)
-            {
-                if (node.Variable != null)
-                {
-                    PushParameters(new[] { node.Variable });
-                }
-
-                Visit(node.Body);
-                Visit(node.Filter);
-                if (node.Variable != null)
-                {
-                    PopParameters(new[] { node.Variable });
-                }
-
-                return node;
-            }
-
-            private void PopParameters(IEnumerable<ParameterExpression> parameters)
-            {
-                foreach (var param in parameters)
-                {
-                    var count = _definedParameters[param];
-                    if (count == 0)
-                    {
-                        _definedParameters.Remove(param);
-                    }
-                    else
-                    {
-                        _definedParameters[param] = count - 1;
-                    }
-                }
-            }
-
-            private void PushParameters(IEnumerable<ParameterExpression> parameters)
-            {
-                foreach (var param in parameters)
-                {
-                    if (_definedParameters.TryGetValue(param, out var count))
-                    {
-                        _definedParameters[param] = count + 1;
-                    }
-                    else
-                    {
-                        _definedParameters[param] = 1;
-                    }
-                }
             }
         }
     }
