18326fe3c94f449509c3a67bb7457c5291d2b635
Theraot <Theraot@gmail.com>
2019-10-24 09:46:53 -0500

    Assume type isn't IsCollectible, given LESSTHAN_NET35

--- Framework.Core/System/Linq/Expressions/Compiler/DelegateHelpers.cache.cs
+++ Framework.Core/System/Linq/Expressions/Compiler/DelegateHelpers.cache.cs
@@ -3,7 +3,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
-
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
@@ -75,10 +74,7 @@ namespace System.Linq.Expressions.Compiler
             if (!curTypeInfo.TypeChain.TryGetValue(lookingUp, out var nextTypeInfo))
             {
                 nextTypeInfo = new TypeInfo();
-                /*if (!lookingUp.IsCollectible)
-                {
-                    curTypeInfo.TypeChain[lookingUp] = nextTypeInfo;
-                }*/
+                curTypeInfo.TypeChain[lookingUp] = nextTypeInfo;
             }
 
             return nextTypeInfo;
