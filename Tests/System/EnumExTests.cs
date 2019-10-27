0b0df072b08d43d10bdb1da2f3cbdf2ae5a21c72
Theraot <Theraot@gmail.com>
2019-10-27 13:52:08 -0500

    EnumEx test

---
+++ Tests/System/EnumExTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests.System
+{
+    [TestFixture]
+    public class EnumExTest
+    {
+        [Flags]
+        private enum WithZero
+        {
+            Zero = 0,
+            One = 1,
+        }
+
+        [Test]
+        public void TryParseZero()
+        {
+            Assert.IsTrue(EnumEx.TryParse<WithZero>("Zero", out var zero));
+            Assert.AreEqual(WithZero.Zero, zero);
+        }
+    }
+}
