0419537cc10580a6fd9cdd0809849ff42e5e9f3f
Theraot <Theraot@gmail.com>
2019-11-13 20:11:26 -0500

    Parameter should be "address" instead of "location"

--- Framework.Core/System/Threading/ThreadEx.cs
+++ Framework.Core/System/Threading/ThreadEx.cs
@@ -22,12 +22,12 @@ namespace System.Threading
 
         [MethodImpl(MethodImplOptionsEx.NoInlining)]
 #endif
-        public static bool VolatileRead(ref bool location)
+        public static bool VolatileRead(ref bool address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            var flag = location;
+            var flag = address;
             Thread.MemoryBarrier();
             return flag;
 #endif
@@ -35,148 +35,148 @@ namespace System.Threading
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static sbyte VolatileRead(ref sbyte location)
+        public static sbyte VolatileRead(ref sbyte address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static byte VolatileRead(ref byte location)
+        public static byte VolatileRead(ref byte address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileRead(ref short location)
+        public static void VolatileRead(ref short address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Read(ref location);
+            Volatile.Read(ref address);
 #else
-            Thread.VolatileRead(ref location);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        [CLSCompliant(false)]
-        public static ushort VolatileRead(ref ushort location)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
-#else
-            return Thread.VolatileRead(ref location);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static int VolatileRead(ref int location)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
-#else
-            return Thread.VolatileRead(ref location);
+            Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static uint VolatileRead(ref uint location)
+        public static ushort VolatileRead(ref ushort address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static long VolatileRead(ref long location)
+        public static int VolatileRead(ref int address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        [CLSCompliant(false)]
-        public static ulong VolatileRead(ref ulong location)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
-#else
-            return Thread.VolatileRead(ref location);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static IntPtr VolatileRead(ref IntPtr location)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
-#else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static UIntPtr VolatileRead(ref UIntPtr location)
+        public static uint VolatileRead(ref uint address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static float VolatileRead(ref float location)
+        public static long VolatileRead(ref long address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static double VolatileRead(ref double location)
+        [CLSCompliant(false)]
+        public static ulong VolatileRead(ref ulong address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static T VolatileRead<T>(ref T location)
+        public static IntPtr VolatileRead(ref IntPtr address)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            return Volatile.Read(ref address);
+#else
+            return Thread.VolatileRead(ref address);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        [CLSCompliant(false)]
+        public static UIntPtr VolatileRead(ref UIntPtr address)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            return Volatile.Read(ref address);
+#else
+            return Thread.VolatileRead(ref address);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static float VolatileRead(ref float address)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            return Volatile.Read(ref address);
+#else
+            return Thread.VolatileRead(ref address);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static double VolatileRead(ref double address)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            return Volatile.Read(ref address);
+#else
+            return Thread.VolatileRead(ref address);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static T VolatileRead<T>(ref T address)
             where T : class?
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Interlocked.CompareExchange(ref location, location, location);
+            return Interlocked.CompareExchange(ref address, address, address);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        [return: NotNullIfNotNull("location")]
-        public static object? VolatileRead(ref object? location)
+        [return: NotNullIfNotNull("address")]
+        public static object? VolatileRead(ref object? address)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            return Volatile.Read(ref location);
+            return Volatile.Read(ref address);
 #else
-            return Thread.VolatileRead(ref location);
+            return Thread.VolatileRead(ref address);
 #endif
         }
 
@@ -187,160 +187,160 @@ namespace System.Threading
 
         [MethodImpl(MethodImplOptionsEx.NoInlining)]
 #endif
-        public static void VolatileWrite(ref bool location, bool value)
+        public static void VolatileWrite(ref bool address, bool value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            GC.KeepAlive(location);
+            GC.KeepAlive(address);
             Thread.MemoryBarrier();
-            location = value;
+            address = value;
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static void VolatileWrite(ref sbyte location, sbyte value)
+        public static void VolatileWrite(ref sbyte address, sbyte value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref byte location, byte value)
+        public static void VolatileWrite(ref byte address, byte value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref short location, short value)
+        public static void VolatileWrite(ref short address, short value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        [CLSCompliant(false)]
-        public static void VolatileWrite(ref ushort location, ushort value)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
-#else
-            Thread.VolatileWrite(ref location, value);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref int location, int value)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
-#else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static void VolatileWrite(ref uint location, uint value)
+        public static void VolatileWrite(ref ushort address, ushort value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref long location, long value)
+        public static void VolatileWrite(ref int address, int value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        [CLSCompliant(false)]
-        public static void VolatileWrite(ref ulong location, ulong value)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
-#else
-            Thread.VolatileWrite(ref location, value);
-#endif
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref IntPtr location, IntPtr value)
-        {
-#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
-#else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
         [CLSCompliant(false)]
-        public static void VolatileWrite(ref UIntPtr location, UIntPtr value)
+        public static void VolatileWrite(ref uint address, uint value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref float location, float value)
+        public static void VolatileWrite(ref long address, long value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref double location, double value)
+        [CLSCompliant(false)]
+        public static void VolatileWrite(ref ulong address, ulong value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite<T>(ref T location, T value)
+        public static void VolatileWrite(ref IntPtr address, IntPtr value)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            Volatile.Write(ref address, value);
+#else
+            Thread.VolatileWrite(ref address, value);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        [CLSCompliant(false)]
+        public static void VolatileWrite(ref UIntPtr address, UIntPtr value)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            Volatile.Write(ref address, value);
+#else
+            Thread.VolatileWrite(ref address, value);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void VolatileWrite(ref float address, float value)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            Volatile.Write(ref address, value);
+#else
+            Thread.VolatileWrite(ref address, value);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void VolatileWrite(ref double address, double value)
+        {
+#if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
+            Volatile.Write(ref address, value);
+#else
+            Thread.VolatileWrite(ref address, value);
+#endif
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void VolatileWrite<T>(ref T address, T value)
             where T : class?
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Interlocked.Exchange(ref location, value);
+            Interlocked.Exchange(ref address, value);
 #endif
         }
 
         [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        public static void VolatileWrite(ref object? location, object? value)
+        public static void VolatileWrite(ref object? address, object? value)
         {
 #if LESSTHAN_NETSTANDARD20 || LESSTHAN_NETCOREAPP20
-            Volatile.Write(ref location, value);
+            Volatile.Write(ref address, value);
 #else
-            Thread.VolatileWrite(ref location, value);
+            Thread.VolatileWrite(ref address, value);
 #endif
         }
     }
