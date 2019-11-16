253e3dabec3d9959a6476f59bfd656a39c24d6b1
Theraot <Theraot@gmail.com>
2019-11-16 02:49:44 -0500

    ThreadPoolEx (added reference to ThreadPool nuget)

---
+++ Framework.Core/System/Threading/ThreadPoolEx.cs
@@ -0,0 +1,44 @@
+﻿namespace System.Threading
+{
+    public static class ThreadPoolEx
+    {
+        public static bool QueueUserWorkItem(WaitCallback callBack)
+        {
+            return ThreadPool.QueueUserWorkItem(callBack);
+        }
+
+        public static bool QueueUserWorkItem(WaitCallback callBack, object? state)
+        {
+#if LESSTHAN_NETSTANDARD13
+            return ThreadPool.QueueUserWorkItem(_ => callBack(state));
+#else
+            return ThreadPool.QueueUserWorkItem(callBack, state);
+#endif
+        }
+
+        public static bool QueueUserWorkItem(Action<object?> callBack, object? state, bool preferLocal)
+        {
+#if LESSTHAN_NETSTANDARD13
+            _ = preferLocal;
+            return ThreadPool.QueueUserWorkItem(_ => callBack(state));
+#elif TARGETS_NET || TARGETS_NETCORE || LESSTHAN_NETSTANDARD21
+            _ = preferLocal;
+            return ThreadPool.QueueUserWorkItem(obj => callBack(obj), state);
+#else
+            return ThreadPool.QueueUserWorkItem(callBack, state, preferLocal);
+#endif
+        }
+
+        [System.Security.SecurityCritical]
+        public static bool UnsafeQueueUserWorkItem(WaitCallback callBack, object state)
+        {
+#if TARGETS_NET || LESSTHAN_NETCORE12 || GREATERTHAN_NETSTANDARD16
+            return ThreadPool.UnsafeQueueUserWorkItem(callBack, state);
+#elif LESSTHAN_NETSTANDARD13
+            return ThreadPool.QueueUserWorkItem(_ => callBack(state));
+#else
+            return ThreadPool.QueueUserWorkItem(callBack, state);
+#endif
+        }
+    }
+}
---
+++ TestRunner/ThreadPoolAvailabilityTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using Theraot;
+
+namespace TestRunner
+{
+    public static class ThreadPoolAvailabilityTests
+    {
+        public static void MethodAvailability()
+        {
+            No.Op<Func<WaitCallback, bool>>(ThreadPool.QueueUserWorkItem);
+            No.Op<Func<WaitCallback, object, bool>>(ThreadPoolEx.QueueUserWorkItem);
+            No.Op<Func<Action<object>, object, bool, bool>>(ThreadPoolEx.QueueUserWorkItem);
+            No.Op<Func<WaitCallback, object, bool>>(ThreadPoolEx.UnsafeQueueUserWorkItem);
+        }
+
+        public static void TypeAvailability()
+        {
+            No.Op(typeof(ThreadPool));
+            No.Op(typeof(WaitCallback));
+        }
+    }
+}
