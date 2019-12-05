e64608602e9fc9fd13af7609f2202b3be882dcb3
Theraot <Theraot@gmail.com>
2019-12-05 00:49:17 -0500

    IHasComparer can be contravariant

--- Framework.Core/Theraot/Collections/Specialized/IHasComparer.cs
+++ Framework.Core/Theraot/Collections/Specialized/IHasComparer.cs
@@ -4,7 +4,12 @@ using System.Collections.Generic;
 
 namespace Theraot.Collections.Specialized
 {
+#if LESSTHAN_NET40
+
     public interface IHasComparer<TKey>
+#else
+    public interface IHasComparer<in TKey>
+#endif
     {
         IEqualityComparer<TKey> Comparer { get; }
     }
