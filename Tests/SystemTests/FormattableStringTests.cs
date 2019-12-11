afa032ef79e10b581f489b8bd5612a156cd2eb44
Theraot <Theraot@gmail.com>
2019-12-11 13:11:01 -0500

    FormattableString test
    #117

---
+++ Tests/SystemTests/FormattableStringTests.cs
@@ -0,0 +1,22 @@
+﻿#pragma warning disable CA1305 // Specify IFormatProvider
+
+using System;
+using NUnit.Framework;
+
+namespace Tests.SystemTests
+{
+    [TestFixture]
+    public static class FormattableStringTests
+    {
+        [Test]
+        public static void ReadAsyncReads()
+        {
+            TestMe($"{123.34}");
+        }
+
+        private static void TestMe(FormattableString s)
+        {
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
