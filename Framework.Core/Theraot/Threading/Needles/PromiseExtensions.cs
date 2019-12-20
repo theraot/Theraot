ca23addace86d5257462a01d005d3ace4dfff6df
Theraot <Theraot@gmail.com>
2019-12-20 07:48:53 -0500

    Removed PromiseAwaiter

---
+++ Framework.Core/Theraot/Threading/Needles/PromiseExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Theraot.Threading.Needles
+{
+    public static class PromiseExtensions
+    {
+        public static IWaitablePromise GetAwaiter(this IWaitablePromise promise)
+        {
+            return promise;
+        }
+
+        public static IWaitablePromise<T> GetAwaiter<T>(this IWaitablePromise<T> promise)
+            where T : class
+        {
+            return promise;
+        }
+    }
+}
