85884ed01c5e151e5b49854401960dc7e86fb2bd
Theraot <Theraot@gmail.com>
2019-12-20 00:46:44 -0500

    VoidStruct Equality

--- Framework.Core/Theraot/VoidStruct.cs
+++ Framework.Core/Theraot/VoidStruct.cs
@@ -1,9 +1,38 @@
 ﻿// Needed for NET35 (TASK)
 
+using System;
+
 namespace Theraot
 {
-    public struct VoidStruct
+    public struct VoidStruct : IEquatable<VoidStruct>
     {
-        // Empty
+        public static bool operator !=(VoidStruct left, VoidStruct right)
+        {
+            _ = left;
+            _ = right;
+            return false;
+        }
+
+        public static bool operator ==(VoidStruct left, VoidStruct right)
+        {
+            _ = left;
+            _ = right;
+            return true;
+        }
+
+        public bool Equals(VoidStruct other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is VoidStruct;
+        }
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
     }
 }
