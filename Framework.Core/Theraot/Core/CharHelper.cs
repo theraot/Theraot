d726873dcf19645e2d5544a2b5ac0f718a7935d6
Theraot <Theraot@gmail.com>
2019-12-29 17:14:09 -0500

    Added NewLine to CharHelper

--- Framework.Core/Theraot/Core/CharHelper.cs
+++ Framework.Core/Theraot/Core/CharHelper.cs
@@ -16,9 +16,30 @@ namespace Theraot.Core
             '\u0020' /*Space*/
         };
 
+        private static readonly char[] _newLine =
+        {
+            '\u000A' /*NewLine*/,
+            '\u000D' /*Carriage return*/,
+        };
+
+        public static char[] GetClassicWhitespace()
+        {
+            return _classicWhitespace;
+        }
+
+        public static char[] GetNewLine()
+        {
+            return _newLine;
+        }
+
         public static bool IsClassicWhitespace(char character)
         {
             return Array.IndexOf(_classicWhitespace, character) >= 0;
+        }
+
+        public static bool IsNewLine(char character)
+        {
+            return Array.IndexOf(_newLine, character) >= 0;
         }
     }
 }
