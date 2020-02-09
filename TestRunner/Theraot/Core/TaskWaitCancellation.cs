248c40f78a0b8acb779e5b0fbccfc88f07a32ea6
Theraot <Theraot@gmail.com>
2020-02-09 01:43:24 -0500

    New test for #120

---
+++ TestRunner/Theraot/Core/TaskWaitCancellation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestRunner.Theraot.Core
+{
+    [TestFixture]
+    public static class TaskWaitCancellation
+    {
+        [Test]
+        public static void TestWaitForCancellation()
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                var task = CreateTask(cts);
+                cts.CancelAfter(10);
+                task.Wait(); // never completes
+            }
+        }
+
+        private static Task CreateTask(CancellationTokenSource cts)
+        {
+            return TaskEx.Run
+            (
+                async () =>
+                {
+                    try
+                    {
+                        await TaskEx.Delay(100, cts.Token).ConfigureAwait(true);
+                    }
+                    catch (OperationCanceledException e)
+                    {
+                        _ = e;
+                    }
+                },
+                cts.Token
+            );
+        }
+    }
+}
