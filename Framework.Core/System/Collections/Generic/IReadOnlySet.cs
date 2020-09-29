5413a7e81e0778ecd3b438a15b3db230a21fecc0
Theraot <Theraot@gmail.com>
2020-09-29 04:13:44 -0500

    Initial implementation of IReadOnlySet
    #125

---
+++ Framework.Core/System/Collections/Generic/IReadOnlySet.cs
@@ -0,0 +1,19 @@
+﻿namespace System.Collections.Generic
+{
+    public interface IReadOnlySet<T> : IReadOnlyCollection<T>
+    {
+        public bool Contains(T item);
+
+        public bool IsProperSubsetOf(IEnumerable<T> other);
+
+        public bool IsProperSupersetOf(IEnumerable<T> other);
+
+        public bool IsSubsetOf(IEnumerable<T> other);
+
+        public bool IsSupersetOf(IEnumerable<T> other);
+
+        public bool Overlaps(IEnumerable<T> other);
+
+        public bool SetEquals(IEnumerable<T> other);
+    }
+}
