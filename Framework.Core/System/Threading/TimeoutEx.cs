e151f2769eb82372869ee530ac9e1d72c94e4c6c
Theraot <Theraot@gmail.com>
2020-10-14 15:47:10 -0500

    TimeoutEx.InfiniteTimeSpan
    #130

---
+++ Framework.Core/System/Threading/TimeoutEx.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Threading
+{
+    public static class TimeoutEx
+    {
+        [Runtime.InteropServices.ComVisible(false)]
+        public static readonly TimeSpan InfiniteTimeSpan =
+#if LESSTHAN_NET45
+            new TimeSpan(0, 0, 0, 0, Timeout.Infinite);
+
+#else
+            Timeout.InfiniteTimeSpan;
+
+#endif
+    }
+}
