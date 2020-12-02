e2c419ad297856c2d49731b3478cfd43518ea95b
Konstantin Sharon <konstantins@sentinelone.com>
2020-12-02 13:07:48 +0200

    Provide KeyValuePair and DictionaryEntry methods. Fixes #134

---
+++ Framework.Core/System/Collections/Generic/KeyValuePairTheraotExtensions.cs
@@ -0,0 +1,20 @@
+﻿#if TARGETS_NET || LESSTHAN_NETCORE20 || LESSTHAN_NETSTANDARD21
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace System.Collections.Generic
+{
+    public static class KeyValuePairTheraotExtensions
+    {
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> keyValuePair, out TKey key, out TValue value)
+        {
+            key = keyValuePair.Key;
+            value = keyValuePair.Value;
+        }
+    }
+}
+
+#endif
---
+++ Tests/SystemTests/CollectionsTests/DictionaryEntryTest.cs
@@ -0,0 +1,26 @@
+﻿#if TARGETS_NET || LESSTHAN_NETCORE20 || LESSTHAN_NETSTANDARD21
+
+#if LESSTHAN_NET35
+extern alias nunitlinq;
+#endif
+
+using NUnit.Framework;
+using System.Collections;
+
+namespace Tests.SystemTests.CollectionsTests
+{
+    [TestFixture]
+    class DictionaryEntryTest
+    {
+        [Test]
+        public void Deconstruct()
+        {
+            DictionaryEntry dictionaryEntry = new DictionaryEntry(1, "2");
+            (object key, object value) = dictionaryEntry;
+            Assert.AreEqual(1, key);
+            Assert.AreEqual("2", value);
+        }
+    }
+}
+
+#endif
