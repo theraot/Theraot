cfd2fe32c64087b9666cc03576f4707a757f3c70
Theraot <Theraot@gmail.com>
2021-01-28 12:07:11 -0500

    Parenthesis for readability

--- Framework.Core/System/Collections/Concurrent/ConcurrentDictionary.cs
+++ Framework.Core/System/Collections/Concurrent/ConcurrentDictionary.cs
@@ -1602,7 +1602,7 @@ namespace System.Collections.Concurrent
                     checked
                     {
                         // Double the size of the buckets table and add one, so that we have an odd integer.
-                        newLength = tables._buckets.Length * 2 + 1;
+                        newLength = (tables._buckets.Length * 2) + 1;
 
                         // Now, we only need to check odd integers, and find the first that is not divisible
                         // by 3, 5 or 7.
