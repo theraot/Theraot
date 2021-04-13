6cc1842c9b10aff545b31a0823ebe1d46eb8f688
Theraot <Theraot@gmail.com>
2021-04-13 16:24:45 -0500

    Fixed naming

--- Framework.Core/System/Collections/HashHelpers.cs
+++ Framework.Core/System/Collections/HashHelpers.cs
@@ -1,4 +1,4 @@
-﻿#if LESSTHAN_NET40 || NETSTANDARD1_0
+#if LESSTHAN_NET40 || NETSTANDARD1_0
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
@@ -26,10 +26,10 @@ namespace System.Collections
 
             // This is equivalent of (uint)Math.BigMul(multiplier * value, divisor, out _). This version
             // is faster than BigMul currently because we only need the high bits.
-            uint highbits = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
+            uint highBits = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
 
-            Debug.Assert(highbits == value % divisor);
-            return highbits;
+            Debug.Assert(highBits == value % divisor);
+            return highBits;
         }
     }
 }
