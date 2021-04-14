efcf39c24d19baab08d2438333b485b568632fb8
Theraot <Theraot@gmail.com>
2021-04-14 00:24:57 -0500

    This using is only needed on some platforms

--- Framework.Core/System/Runtime/CompilerServices/SwitchExpressionException.cs
+++ Framework.Core/System/Runtime/CompilerServices/SwitchExpressionException.cs
@@ -3,7 +3,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#if TARGETS_NET || GREATERTHAN_NETCOREAPP11 || GREATERTHAN_NETSTANDARD16
+
 using System.Runtime.Serialization;
+
+#endif
 
 namespace System.Runtime.CompilerServices
 {
