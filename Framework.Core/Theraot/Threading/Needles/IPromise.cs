6f30019b591a74e5c0415ed9d50264d0f2613503
Theraot <Theraot@gmail.com>
2021-04-14 22:20:59 -0500

    Extracted IWaitablePromise to another file

--- Framework.Core/Theraot/Threading/Needles/IPromise.cs
+++ Framework.Core/Theraot/Threading/Needles/IPromise.cs
@@ -1,7 +1,5 @@
 ﻿// Needed for NET40
 
-using System.Runtime.CompilerServices;
-
 namespace Theraot.Threading.Needles
 {
     public interface IPromise
@@ -10,16 +8,6 @@ namespace Theraot.Threading.Needles
     }
 
     public interface IPromise<out T> : IPromise, IReadOnlyNeedle<T>
-    {
-        // Empty
-    }
-
-    public interface IWaitablePromise : IPromise, INotifyCompletion
-    {
-        void Wait();
-    }
-
-    public interface IWaitablePromise<out T> : IPromise<T>, IWaitablePromise
     {
         // Empty
     }
---
+++ Framework.Core/Theraot/Threading/Needles/IWaitablePromise.cs
@@ -0,0 +1,16 @@
+﻿// Needed for NET40
+
+using System.Runtime.CompilerServices;
+
+namespace Theraot.Threading.Needles
+{
+    public interface IWaitablePromise : IPromise, INotifyCompletion
+    {
+        void Wait();
+    }
+
+    public interface IWaitablePromise<out T> : IPromise<T>, IWaitablePromise
+    {
+        // Empty
+    }
+}
