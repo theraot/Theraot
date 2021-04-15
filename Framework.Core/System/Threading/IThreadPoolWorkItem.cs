a3ee30266fcb10400d4c90e6165f2f7b5490fea0
Theraot <Theraot@gmail.com>
2021-04-14 21:57:56 -0500

    Extracted ThreadPoolAdapter to another file

--- Framework.Core/System/Threading/IThreadPoolWorkItem.cs
+++ Framework.Core/System/Threading/IThreadPoolWorkItem.cs
@@ -12,27 +12,6 @@ namespace System.Threading
         [SecurityCritical]
         void MarkAborted(ThreadAbortException exception);
     }
-
-    internal static class ThreadPoolAdapter
-    {
-        internal static void QueueWorkItem(IThreadPoolWorkItem item)
-        {
-            ThreadPool.QueueUserWorkItem
-            (
-                _ =>
-                {
-                    try
-                    {
-                        item.ExecuteWorkItem();
-                    }
-                    catch (ThreadAbortException exception)
-                    {
-                        item.MarkAborted(exception);
-                    }
-                }
-            );
-        }
-    }
 }
 
 #endif
---
+++ Framework.Core/System/Threading/ThreadPoolAdapter.cs
@@ -0,0 +1,27 @@
+﻿#if LESSTHAN_NET40
+
+namespace System.Threading
+{
+    internal static class ThreadPoolAdapter
+    {
+        internal static void QueueWorkItem(IThreadPoolWorkItem item)
+        {
+            ThreadPool.QueueUserWorkItem
+            (
+                _ =>
+                {
+                    try
+                    {
+                        item.ExecuteWorkItem();
+                    }
+                    catch (ThreadAbortException exception)
+                    {
+                        item.MarkAborted(exception);
+                    }
+                }
+            );
+        }
+    }
+}
+
+#endif
