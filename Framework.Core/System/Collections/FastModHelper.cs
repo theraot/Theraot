c1479b6727894351c73eb3d42a2dcbf0faebc639
Theraot <Theraot@gmail.com>
2021-04-17 23:05:08 -0500

    Use var

--- Framework.Core/System/Collections/FastModHelper.cs
+++ Framework.Core/System/Collections/FastModHelper.cs
@@ -1,4 +1,4 @@
-#if LESSTHAN_NET40 || NETSTANDARD1_0
+﻿#if LESSTHAN_NET40 || NETSTANDARD1_0
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
@@ -19,7 +19,7 @@ namespace System.Collections
 
             // This is equivalent of (uint)Math.BigMul(multiplier * value, divisor, out _). This version
             // is faster than BigMul currently because we only need the high bits.
-            uint highBits = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
+            var highBits = (uint)(((((multiplier * value) >> 32) + 1) * divisor) >> 32);
 
             Debug.Assert(highBits == value % divisor);
             return highBits;
