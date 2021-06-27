7fc449349dce5c38a31b3cb5d45e3f9b219c519c
NN <NN---@users.noreply.github.com>
2021-06-27 11:01:14 +0300

    Update HashCode.

--- Framework.Core/System/HashCode.cs
+++ Framework.Core/System/HashCode.cs
@@ -8,7 +8,6 @@
 
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 /*
 
@@ -54,6 +53,9 @@ using System.ComponentModel;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 
+// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+
 namespace System
 {
     // xxHash32 is used for the hash code.
@@ -61,15 +63,25 @@ namespace System
 
     public struct HashCode
     {
-        private const uint _prime1 = 2654435761U;
-        private const uint _prime2 = 2246822519U;
-        private const uint _prime3 = 3266489917U;
-        private const uint _prime4 = 668265263U;
-        private const uint _prime5 = 374761393U;
-        private static readonly uint _seed = GenerateGlobalSeed();
-        private uint _length;
-        private uint _queue1, _queue2, _queue3;
+        private static readonly uint s_seed = GenerateGlobalSeed();
+
+        private const uint Prime1 = 2654435761U;
+        private const uint Prime2 = 2246822519U;
+        private const uint Prime3 = 3266489917U;
+        private const uint Prime4 = 668265263U;
+        private const uint Prime5 = 374761393U;
+
         private uint _v1, _v2, _v3, _v4;
+        private uint _queue1, _queue2, _queue3;
+        private uint _length;
+
+        private static uint GenerateGlobalSeed()
+        {
+            var random = new Random();
+            var bytes = new byte[4];
+            random.NextBytes(bytes);
+            return BitConverter.ToUInt32(bytes, 0);
+        }
 
         public static int Combine<T1>(T1 value1)
         {
@@ -252,6 +264,49 @@ namespace System
             return (int)hash;
         }
 
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        private static void Initialize(out uint v1, out uint v2, out uint v3, out uint v4)
+        {
+            v1 = s_seed + Prime1 + Prime2;
+            v2 = s_seed + Prime2;
+            v3 = s_seed;
+            v4 = s_seed - Prime1;
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        private static uint Round(uint hash, uint input)
+        {
+            return BitOperations.RotateLeft(hash + input * Prime2, 13) * Prime1;
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        private static uint QueueRound(uint hash, uint queuedValue)
+        {
+            return BitOperations.RotateLeft(hash + queuedValue * Prime3, 17) * Prime4;
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        private static uint MixState(uint v1, uint v2, uint v3, uint v4)
+        {
+            return BitOperations.RotateLeft(v1, 1) + BitOperations.RotateLeft(v2, 7) + BitOperations.RotateLeft(v3, 12) + BitOperations.RotateLeft(v4, 18);
+        }
+
+        private static uint MixEmptyState()
+        {
+            return s_seed + Prime5;
+        }
+
+        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
+        private static uint MixFinal(uint hash)
+        {
+            hash ^= hash >> 15;
+            hash *= Prime2;
+            hash ^= hash >> 13;
+            hash *= Prime3;
+            hash ^= hash >> 16;
+            return hash;
+        }
+
         public void Add<T>(T value)
         {
             Add(value?.GetHashCode() ?? 0);
@@ -259,18 +314,60 @@ namespace System
 
         public void Add<T>(T value, IEqualityComparer<T>? comparer)
         {
-            Add(comparer?.GetHashCode(value) ?? (value?.GetHashCode() ?? 0));
+            Add(value is null ? 0 : (comparer?.GetHashCode(value) ?? value.GetHashCode()));
         }
 
-        [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes.", true)]
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public override readonly bool Equals(object? obj) => throw new NotSupportedException("HashCode is a mutable struct and should not be compared with other HashCodes.");
+        private void Add(int value)
+        {
+            // The original xxHash works as follows:
+            // 0. Initialize immediately. We can't do this in a struct (no
+            //    default ctor).
+            // 1. Accumulate blocks of length 16 (4 uints) into 4 accumulators.
+            // 2. Accumulate remaining blocks of length 4 (1 uint) into the
+            //    hash.
+            // 3. Accumulate remaining blocks of length 1 into the hash.
 
-        [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes. Use ToHashCode to retrieve the computed hash code.", true)]
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public override readonly int GetHashCode() => throw new NotSupportedException("HashCode is a mutable struct and should not be compared with other HashCodes. Use ToHashCode to retrieve the computed hash code.");
+            // There is no need for #3 as this type only accepts ints. _queue1,
+            // _queue2 and _queue3 are basically a buffer so that when
+            // ToHashCode is called we can execute #2 correctly.
 
-        public readonly int ToHashCode()
+            // We need to initialize the xxHash32 state (_v1 to _v4) lazily (see
+            // #0) nd the last place that can be done if you look at the
+            // original code is just before the first block of 16 bytes is mixed
+            // in. The xxHash32 state is never used for streams containing fewer
+            // than 16 bytes.
+
+            // To see what's really going on here, have a look at the Combine
+            // methods.
+
+            var val = (uint)value;
+
+            // Storing the value of _length locally shaves of quite a few bytes
+            // in the resulting machine code.
+            var previousLength = _length++;
+            var position = previousLength % 4;
+
+            // Switch can't be inlined.
+
+            if (position == 0)
+                _queue1 = val;
+            else if (position == 1)
+                _queue2 = val;
+            else if (position == 2)
+                _queue3 = val;
+            else // position == 3
+            {
+                if (previousLength == 3)
+                    Initialize(out _v1, out _v2, out _v3, out _v4);
+
+                _v1 = Round(_v1, _queue1);
+                _v2 = Round(_v2, _queue2);
+                _v3 = Round(_v3, _queue3);
+                _v4 = Round(_v4, val);
+            }
+        }
+
+        public int ToHashCode()
         {
             // Storing the value of _length locally shaves of quite a few bytes
             // in the resulting machine code.
@@ -313,115 +410,28 @@ namespace System
             return (int)hash;
         }
 
-        private static uint GenerateGlobalSeed()
-        {
-            var random = new Random();
-            var bytes = new byte[4];
-            random.NextBytes(bytes);
-            return BitConverter.ToUInt32(bytes, 0);
-        }
+#pragma warning disable 0809
+        // Obsolete member 'memberA' overrides non-obsolete member 'memberB'.
+        // Disallowing GetHashCode and Equals is by design
 
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private static void Initialize(out uint v1, out uint v2, out uint v3, out uint v4)
-        {
-            v1 = _seed + _prime1 + _prime2;
-            v2 = _seed + _prime2;
-            v3 = _seed;
-            v4 = _seed - _prime1;
-        }
+        // * We decided to not override GetHashCode() to produce the hash code
+        //   as this would be weird, both naming-wise as well as from a
+        //   behavioral standpoint (GetHashCode() should return the object's
+        //   hash code, not the one being computed).
 
-        private static uint MixEmptyState()
-        {
-            return _seed + _prime5;
-        }
+        // * Even though ToHashCode() can be called safely multiple times on
+        //   this implementation, it is not part of the contract. If the
+        //   implementation has to change in the future we don't want to worry
+        //   about people who might have incorrectly used this type.
 
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private static uint MixFinal(uint hash)
-        {
-            hash ^= hash >> 15;
-            hash *= _prime2;
-            hash ^= hash >> 13;
-            hash *= _prime3;
-            hash ^= hash >> 16;
-            return hash;
-        }
+        [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes. Use ToHashCode to retrieve the computed hash code.", error: true)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override int GetHashCode() => throw new NotSupportedException("HashCode HashCode not supported");
 
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private static uint MixState(uint v1, uint v2, uint v3, uint v4)
-        {
-            return BitOperations.RotateLeft(v1, 1) + BitOperations.RotateLeft(v2, 7) + BitOperations.RotateLeft(v3, 12) + BitOperations.RotateLeft(v4, 18);
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private static uint QueueRound(uint hash, uint queuedValue)
-        {
-            return BitOperations.RotateLeft(hash + (queuedValue * _prime3), 17) * _prime4;
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private static uint Round(uint hash, uint input)
-        {
-            return BitOperations.RotateLeft(hash + (input * _prime2), 13) * _prime1;
-        }
-
-        [MethodImpl(MethodImplOptionsEx.AggressiveInlining)]
-        private void Add(int value)
-        {
-            // The original xxHash works as follows:
-            // 0. Initialize immediately. We can't do this in a struct (no
-            //    default ctor).
-            // 1. Accumulate blocks of length 16 (4 uints) into 4 accumulators.
-            // 2. Accumulate remaining blocks of length 4 (1 uint) into the
-            //    hash.
-            // 3. Accumulate remaining blocks of length 1 into the hash.
-
-            // There is no need for #3 as this type only accepts ints. _queue1,
-            // _queue2 and _queue3 are basically a buffer so that when
-            // ToHashCode is called we can execute #2 correctly.
-
-            // We need to initialize the xxHash32 state (_v1 to _v4) lazily (see
-            // #0) nd the last place that can be done if you look at the
-            // original code is just before the first block of 16 bytes is mixed
-            // in. The xxHash32 state is never used for streams containing fewer
-            // than 16 bytes.
-
-            // To see what's really going on here, have a look at the Combine
-            // methods.
-
-            var val = (uint)value;
-
-            // Storing the value of _length locally shaves of quite a few bytes
-            // in the resulting machine code.
-            var previousLength = _length++;
-            var position = previousLength % 4;
-
-            // Switch can't be inlined.
-
-            if (position == 0)
-            {
-                _queue1 = val;
-            }
-            else if (position == 1)
-            {
-                _queue2 = val;
-            }
-            else if (position == 2)
-            {
-                _queue3 = val;
-            }
-            else
-            {
-                if (previousLength == 3)
-                {
-                    Initialize(out _v1, out _v2, out _v3, out _v4);
-                }
-
-                _v1 = Round(_v1, _queue1);
-                _v2 = Round(_v2, _queue2);
-                _v3 = Round(_v3, _queue3);
-                _v4 = Round(_v4, val);
-            }
-        }
+        [Obsolete("HashCode is a mutable struct and should not be compared with other HashCodes.", error: true)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object? obj) => throw new NotSupportedException("HashCode Equality not supported");
+#pragma warning restore 0809
     }
 }
 
