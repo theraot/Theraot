6a5270a1505db48f15307bf27c63a80b94d6a9ac
NN <580536+NN---@users.noreply.github.com>
2021-10-27 11:05:31 +0300

    Implement Enumerable.TryGetNonEnumeratedCount

---
+++ Framework.Core/System/Linq/Enumerable.count.cs
@@ -0,0 +1,35 @@
+#if TARGETS_NET || TARGETS_NETSTANDARD || LESSTHAN_NET60
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static class EnumerableEx
+    {
+        public static bool TryGetNonEnumeratedCount<TSource>(this IEnumerable<TSource> source, out int count)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (source is ICollection<TSource> collectionoft)
+            {
+                count = collectionoft.Count;
+                return true;
+            }
+
+            if (source is ICollection collection)
+            {
+                count = collection.Count;
+                return true;
+            }
+
+            count = 0;
+            return false;
+        }
+    }
+}
+
+#endif
