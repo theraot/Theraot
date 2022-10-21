d88cd2a0d8588b8a2541a43d8db33b8def954efa
Giuseppe Lippolis <workgroupengineering@outlook.com>
2022-10-21 16:59:59 +0200

    feat: Add ArrayPool and MemoryPool

---
+++ Framework.Core/System/Buffers/IMemoryOwner.cs
@@ -0,0 +1,20 @@
+﻿#if LESSTHAN_NET40
+//BASEDON: https://github.com/dotnet/runtime/blob/v5.0.0/src/libraries/System.Private.CoreLib/src/System/Buffers/IMemoryOwner.cs
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Buffers
+{
+    /// <summary>
+    /// Owner of Memory<typeparamref name="T"/> that is responsible for disposing the underlying memory appropriately.
+    /// </summary>
+    public interface IMemoryOwner<T> : IDisposable
+    {
+        /// <summary>
+        /// Returns a Memory<typeparamref name="T"/>.
+        /// </summary>
+        Memory<T> Memory { get; }
+    }
+}
+#endif
