2ca5b56504062fa6c960622725122093a6ba8fc4
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2023-11-08 23:14:14 +0300

    Add compiler well-known attributes CallerArgumentExpressionAttribute; CompilerFeatureRequiredAttribute; RequiredMemberAttribute; SetsRequiredMembersAttribute; SkipLocalsInitAttribute; UnmanagedCallersOnlyAttribute

---
+++ Framework.Core/System/Runtime/CompilerServices/CallerArgumentExpressionAttribute.cs
@@ -0,0 +1,20 @@
+﻿// https://github.com/dotnet/runtime/blob/v7.0.13/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/CallerArgumentExpressionAttribute.cs
+#if !NETCOREAPP3_0_OR_GREATER
+
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public sealed class CallerArgumentExpressionAttribute : Attribute
+    {
+        public CallerArgumentExpressionAttribute(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+
+        public string ParameterName { get; }
+    }
+}
+#endif
