cf5b87f949464484b0c2b2650911dab42a7ba21a
OwnageIsMagic <OwnageIsMagic@users.noreply.github.com>
2023-11-09 01:29:57 +0300

    backport Type.IsAssignableTo(Type); backport Type.GenericTypeArguments

---
+++ Framework.Core/System/Reflection/TypeTheraotExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace System.Reflection;
+
+public static class TypeTheraotExtensions
+{
+#if NETFRAMEWORK && !NET45_OR_GREATER
+    public static Type[] GenericTypeArguments(this Type @this) => @this.GetGenericArguments();
+#endif
+#if !NET5_0_OR_GREATER
+    // [Intrinsic]
+    public static bool IsAssignableTo(this Type @this, [NotNullWhen(true)]Type? targetType)
+        => targetType?.IsAssignableFrom(@this) ?? false;
+#endif
+}
