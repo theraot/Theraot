2d53d485393b03229d39a68633f0f4e6794ac191
Theraot <Theraot@gmail.com>
2024-02-25 00:50:02 -0500

    Fixed build, again

--- Framework.Core/System/Reflection/TypeTheraotExtensions.cs
+++ Framework.Core/System/Reflection/TypeTheraotExtensions.cs
@@ -1,15 +1,16 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-namespace System.Reflection;
-
-public static class TypeTheraotExtensions
+namespace System.Reflection
 {
+    public static class TypeTheraotExtensions
+    {
 #if NETFRAMEWORK && !NET45_OR_GREATER
-    public static Type[] GenericTypeArguments(this Type @this) => @this.GetGenericArguments();
+        public static Type[] GenericTypeArguments(this Type @this) => @this.GetGenericArguments();
 #endif
 #if !NET5_0_OR_GREATER
-    // [Intrinsic]
-    public static bool IsAssignableTo(this Type @this, [NotNullWhen(true)]Type? targetType)
-        => targetType?.IsAssignableFrom(@this) ?? false;
+        // [Intrinsic]
+        public static bool IsAssignableTo(this Type @this, [NotNullWhen(true)]Type? targetType)
+            => targetType?.IsAssignableFrom(@this) ?? false;
 #endif
+    }
 }
