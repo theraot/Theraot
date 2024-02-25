d473ec840322fa4764bb5988785d187e1e0f9831
Alfonso J. Ramos <theraot@gmail.com>
2024-02-24 23:33:28 -0500

    Added ParameterModifier

---
+++ Framework.Core/System/Reflection/ParameterModifier.cs
@@ -0,0 +1,40 @@
+﻿#if LESSTHAN_NETSTANDARD15
+
+#pragma warning disable CA1815 // Override equals and operator equals on value types
+
+using System.Runtime.InteropServices;
+
+namespace System.Reflection
+{
+    [ComVisible(true)]
+    [Serializable]
+    public struct ParameterModifier
+    {
+        private readonly bool[] _internal;
+
+        public ParameterModifier(int parameterCount)
+        {
+            if (parameterCount <= 0)
+            {
+                throw new ArgumentException("Must specify one or more parameters.");
+            }
+
+            _internal = new bool[parameterCount];
+        }
+
+
+        public bool this[int index]
+        {
+            get
+            {
+                return _internal[index];
+            }
+            set
+            {
+                _internal[index] = value;
+            }
+        }
+    }
+}
+
+#endif
