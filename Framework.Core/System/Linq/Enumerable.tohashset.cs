27ce380367042772b3a0dec2ef3abc5dc3c6e22f
Alfonso J. Ramos <theraot@gmail.com>
2024-02-24 23:55:51 -0500

    Added missing braces

--- Framework.Core/System/Linq/Enumerable.tohashset.cs
+++ Framework.Core/System/Linq/Enumerable.tohashset.cs
@@ -18,7 +18,9 @@ namespace System.Linq
         public static HashSet<TSource> ToHashSet<TSource>(this IEnumerable<TSource> source, IEqualityComparer<TSource>? comparer)
         {
             if (source == null)
+            {
                 throw new ArgumentNullException(nameof(source));
+            }
 
             // Don't pre-allocate based on knowledge of size, as potentially many elements will be dropped.
             return new HashSet<TSource>(source, comparer);
